9d41075f90858305c2ee0130f619f9b5605bc5c2
kipp <kipp>
2008-08-14 13:19:32 +0000

    Allow fakesrc to be used to simulate an h(t) stream without the Fr overhead.

--- examples/inspiral.sh
+++ examples/inspiral.sh
@@ -7,17 +7,28 @@ GPSSTOP="874000320"
 INSTRUMENT="H1"
 CHANNEL="LSC-STRAIN"
 
-VISUALIZE_SNR="lal_multiscope ! ffmpegcolorspace ! ximagesink"
-#VISUALIZE_SNR="fakesink"
+SRC="lal_framesrc \
+	blocksize=$((16384*8*16)) \
+	location=${LALCACHE} \
+	instrument=${INSTRUMENT} \
+	channel-name=${CHANNEL} \
+	start-time-gps=${GPSSTART} \
+	stop-time-gps=${GPSSTOP}"
+
+#SRC="fakesrc \
+#	blocksize=$((16384*8*16)) \
+#	num-buffers=$((320/16)) \
+#	sizetype=2 \
+#	sizemax=$((16384*8*16)) \
+#	filltype=2 \
+#	datarate=$((16384*8)) \
+#! audio/x-raw-float, width=64, channels=1, rate=16384, endianness=1234"
+
+SINK="lal_multiscope ! ffmpegcolorspace ! ximagesink"
+#SINK="fakesink"
 
 gst-launch \
-	lal_framesrc \
-		blocksize=$((16384*8*16)) \
-		location="${LALCACHE}" \
-		instrument="${INSTRUMENT}" \
-		channel-name="${CHANNEL}" \
-		start-time-gps="${GPSSTART}" \
-		stop-time-gps="${GPSSTOP}" \
+	$SRC \
 	! audioconvert \
 	! audio/x-raw-float, width=64 \
 	! audiochebband \
@@ -31,4 +42,4 @@ gst-launch \
 		t-start=0 \
 		t-end=1 \
 		snr-length=$((2048*16)) \
-	templatebank0.orthogonal_snr ! $VISUALIZE_SNR
+	templatebank0.orthogonal_snr ! $SINK
