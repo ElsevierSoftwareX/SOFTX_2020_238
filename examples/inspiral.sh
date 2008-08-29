13b6359b48856f62968887c67a076d4f3c81bb79
kipp <kipp>
2008-08-29 17:24:18 +0000

    Update for changes to pad names, and set the multiscope average interval to 10 seconds.

--- examples/inspiral.sh
+++ examples/inspiral.sh
@@ -23,17 +23,17 @@ SRC="lal_framesrc \
 #	sizemax=$((16384*8*16)) \
 #	filltype=2 \
 #	datarate=$((16384*8)) \
-#! audio/x-raw-float, width=64, channels=1, rate=16384, endianness=1234"
+#! audio/x-raw-float, width=64, channels=1, rate=16384, endianness=1234, instrument=${INSTRUMENT}, channel=${CHANNEL}"
 
-SINK="queue ! lal_multiscope trace-duration=0.25 ! ffmpegcolorspace ! timeoverlay ! ffmpegcolorspace ! ximagesink"
+SINK="queue ! lal_multiscope trace-duration=0.25 average-interval=10.0 ! ffmpegcolorspace ! timeoverlay ! ffmpegcolorspace ! ximagesink"
 #SINK="queue ! fakesink"
 
 gst-launch --gst-debug-level=1 \
 	$SRC \
-	! audioconvert \
-	! audio/x-raw-float, width=64 \
+	! lal_simulation \
+		xml-location="bns_injections.xml" \
 	! audiochebband \
-		lower-frequency=20 \
+		lower-frequency=40 \
 		upper-frequency=1000 \
 		poles=8 \
 	! audioresample \
@@ -45,32 +45,33 @@ gst-launch --gst-debug-level=1 \
 	! audioresample \
 	! audio/x-raw-float, rate=512 \
 	! tee name=hoft_512 \
+	hoft_2048. ! queue ! queue ! queue ! $SINK \
 	hoft_2048. ! queue ! lal_templatebank \
 		name=templatebank0 \
 		t-start=0 \
 		t-end=1 \
 		snr-length=$((2048*16)) \
 	templatebank0.orthogonal_snr ! $SINK \
-	templatebank0.bank_magnitude ! $SINK \
+	templatebank0.orthogonal_snr_sum_squares ! $SINK \
 	hoft_2048. ! queue ! lal_templatebank \
 		name=templatebank1 \
 		t-start=1 \
 		t-end=2 \
 		snr-length=$((2048*16)) \
 	templatebank1.orthogonal_snr ! $SINK \
-	templatebank1.bank_magnitude ! $SINK \
+	templatebank1.orthogonal_snr_sum_squares ! $SINK \
 	hoft_1024. ! queue ! lal_templatebank \
 		name=templatebank2 \
 		t-start=2 \
 		t-end=4 \
 		snr-length=$((2048*16)) \
 	templatebank2.orthogonal_snr ! $SINK \
-	templatebank2.bank_magnitude ! $SINK \
+	templatebank2.orthogonal_snr_sum_squares ! $SINK \
 	hoft_512. ! queue ! lal_templatebank \
 		name=templatebank3 \
 		t-start=4 \
 		t-end=8 \
 		snr-length=$((2048*16)) \
 	templatebank3.orthogonal_snr ! $SINK \
-	templatebank3.bank_magnitude ! $SINK
+	templatebank3.orthogonal_snr_sum_squares ! $SINK
 
