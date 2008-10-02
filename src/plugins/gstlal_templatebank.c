c754c46a1be20062889fe7a7a390b15fb3fc5d83
kipp <kipp>
2008-10-02 17:03:20 +0000

    Change CHIRPMASS_START.

--- src/plugins/gstlal_templatebank.c
+++ src/plugins/gstlal_templatebank.c
@@ -84,7 +84,7 @@
 #define DEFAULT_T_END G_MAXUINT
 #define DEFAULT_SNR_LENGTH 2048	/* samples */
 #define TEMPLATE_DURATION 64	/* seconds */
-#define CHIRPMASS_START 1.15	/* M_sun */
+#define CHIRPMASS_START 1.21	/* M_sun */
 #define TEMPLATE_SAMPLE_RATE 2048	/* Hertz */
 #define NUM_TEMPLATES 200
 #define TOLERANCE 0.97
