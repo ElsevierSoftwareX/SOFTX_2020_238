9b81a8b7f12a8ae78884501af91b9551d5a0bd73
channa <channa>
2008-11-18 02:50:39 +0000

    cleaned up

--- src/utilities/test_db.c
+++ src/utilities/test_db.c
@@ -9,19 +9,11 @@
 int main()
   {
   int i;
-  /*lalDebugLevel = LALINFO | LALWARNING | LALERROR | LALNMEMDBG | LALNMEMPAD | LALNMEMTRK;*/
   PGconn *connection = connect_to_postgres_database("port=3333 dbname=inspiral user=postgres host=localhost");
+
   coinc_on_insert_from_inspiral_xml(connection, "H1-INSPIRAL_FIRST_PLAYGROUND-815751380-2048.xml");
   coinc_on_insert_from_inspiral_xml(connection, "H2-INSPIRAL_FIRST_PLAYGROUND-815751084-2048.xml");
 
-  /*insert_inspiral_xml(connection, "test.xml");*/
-
-  /*SnglInspiralTable *tab;
-  tab = calloc(1,sizeof(SnglInspiralTable));
-  
-  insert_from_sngl_inspiral_table(connection, tab, 1);
-
-  free(tab);*/
   return 0;
   }
 
