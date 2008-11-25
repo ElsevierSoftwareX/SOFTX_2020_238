b4847087a23aef12014a482a7c638fe19e295fb1
kipp <kipp>
2008-11-25 18:24:00 +0000

    Fix off-by-one channel counting bug.

--- examples/extract_channel.py
+++ examples/extract_channel.py
@@ -1,9 +1,6 @@
 import sys
 
-n = map(int, sys.argv[1:])
-if 0 not in n:
-	n = [0] + n
-n = tuple(n)
+n = tuple(map(int, sys.argv[1:]))
 for line in sys.stdin:
 	line = line.strip().split()
-	print " ".join(map(line.__getitem__, n))
+	print line[0], " ".join(map(line[1:].__getitem__, n))
