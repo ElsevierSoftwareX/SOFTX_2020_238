effd57ae21438e69dee50101efe7f0befb631c0a
kipp <kipp>
2009-01-20 23:51:48 +0000

    - update for conversion of start and stop time parameters from seconds to   nanoseconds - adjust multiscope parameters

--- examples/play_hoft.sh
+++ examples/play_hoft.sh
@@ -1,8 +1,8 @@
 # A gst-launch pipeline to play h(t) through a sound card.
 
 LALCACHE="/home/kipp/scratch_local/874100000-20000/cache/874000000-20000.cache"
-GPSSTART="874000000"
-GPSSTOP="874020000"
+GPSSTART="874000000000000000"
+GPSSTOP="874020000000000000"
 INSTRUMENT="H1"
 CHANNEL="LSC-STRAIN"
 
@@ -12,8 +12,8 @@ gst-launch --gst-debug-level=1 \
 		location="${LALCACHE}" \
 		instrument="${INSTRUMENT}" \
 		channel-name="${CHANNEL}" \
-		start-time-gps="${GPSSTART}" \
-		stop-time-gps="${GPSSTOP}" \
+		start-time-gps-ns="${GPSSTART}" \
+		stop-time-gps-ns="${GPSSTOP}" \
 	! audiochebband \
 		lower-frequency=45 \
 		upper-frequency=2500 \
@@ -25,4 +25,4 @@ gst-launch --gst-debug-level=1 \
 	! queue \
 	! audioconvert \
 	! alsasink  \
-	tee. ! queue ! audioconvert ! lal_multiscope trace-duration=0.25 average-interval=10.0 ! ffmpegcolorspace ! timeoverlay ! xvimagesink
+	tee. ! queue ! audioconvert ! lal_multiscope trace-duration=1.0 frame-interval=0.03125 average-interval=10.0 ! ffmpegcolorspace ! timeoverlay ! xvimagesink sync=false
