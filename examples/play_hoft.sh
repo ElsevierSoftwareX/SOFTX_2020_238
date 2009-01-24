12990af81d4a915e9b10788478ae9f12fc97e3dd
kipp <kipp>
2009-01-24 01:39:44 +0000

    Add adder element to unjam the alsasink.

--- examples/play_hoft.sh
+++ examples/play_hoft.sh
@@ -6,6 +6,15 @@ GPSSTOP="874020000000000000"
 INSTRUMENT="H1"
 CHANNEL="LSC-STRAIN"
 
+# the adder element preceding the alsasink element is a hack to strip the
+# time stamps from the buffers.  the framesrc element doesn't send the
+# proper "new segment" event down the pipeline so the alsasink element
+# doesn't know to expect a data stream starting at some huge time stamp,
+# instead it waits until it sees a buffer with t=0 (it waits for the clock
+# to wrap-around, it shouldn't do that but I think an integer overflow
+# makes it think that 874000000000000000 precedes 0).  one day the framesrc
+# element will get fixed.
+
 gst-launch --gst-debug-level=1 \
 	lal_framesrc \
 		blocksize=65536 \
@@ -24,5 +33,6 @@ gst-launch --gst-debug-level=1 \
 	! tee name=tee \
 	! queue \
 	! audioconvert \
-	! alsasink  \
+	! adder \
+	! alsasink \
 	tee. ! queue ! audioconvert ! lal_multiscope trace-duration=1.0 frame-interval=0.03125 average-interval=10.0 ! ffmpegcolorspace ! timeoverlay ! xvimagesink sync=false
