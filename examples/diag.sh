8d42b08488f5620d69a5ea775757fd29f8c04ae5
kipp <kipp>
2009-01-28 23:03:31 +0000

    Add diagnostic script.

---
+++ examples/diag.sh
@@ -0,0 +1,34 @@
+function test_adder() {
+	gst-launch \
+		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 timestamp-offset=1000000000 \
+		! lal_adder name=adder sync=true \
+		! audio/x-raw-float, width=64, rate=16384 \
+		! lal_nxydump start-time=0 stop-time=2250000000 \
+		! filesink location=dump.txt \
+		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 timestamp-offset=1250000000 \
+		! adder.
+}
+
+function test_gate() {
+	gst-launch \
+		lal_gate name=gate threshold=0.7 \
+		! audio/x-raw-float, width=64, rate=16384 \
+		! lal_nxydump start-time=0 stop-time=1000000000 \
+		! filesink location=dump.txt \
+		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 \
+		! audio/x-raw-float, rate=1024 \
+		! queue ! gate.control \
+		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 \
+		! queue ! gate.sink
+}
+
+function test_resampler() {
+	gst-launch \
+		audiotestsrc \
+		! audio/x-raw-float, width=64, rate=16384 \
+		! gstlal-audioresample \
+		! audio/x-raw-float, rate=4096 \
+		! fakesink
+}
+
+test_resampler
