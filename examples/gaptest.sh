b055680d60e4dff88a46eb37b481d90614f3bd18
channa <channa>
2009-01-28 21:34:08 +0000

    testing new audio resampler

--- examples/gaptest.sh
+++ examples/gaptest.sh
@@ -1,25 +1,29 @@
-gst-launch \
+STOP_TIME=40000000000
+IN_SAMPLE=64
+OUT_SAMPLE=2048
+QUALITY=10
+gst-launch-0.10 \
 	lal_gate \
 		name=gate \
-		threshold=0.5 \
-	! audioresample \
-	! audio/x-raw-float, width=64, rate=128 \
+		threshold=0.75 \
+	! gstlal-audioresample \
+	! audio/x-raw-float, width=64, rate=${OUT_SAMPLE}, quality=${QUALITY} \
 	! lal_nxydump \
 		start-time=0 \
-		stop-time=1000000000 \
+		stop-time=${STOP_TIME} \
 	! filesink \
 		location=output.txt \
 		buffer-mode=2 \
 	audiotestsrc \
 		wave=0 \
-		freq=30.5 \
-	! audio/x-raw-float, width=64, rate=512 \
+		freq=20.0 \
+	! audio/x-raw-float, width=64, rate=${IN_SAMPLE} \
 	! tee \
 		name=signal \
 	! queue \
 	! lal_nxydump \
 		start-time=0 \
-		stop-time=1000000000 \
+		stop-time=${STOP_TIME} \
 	! filesink \
 		location=input.txt \
 		buffer-mode=2 \
@@ -28,14 +32,14 @@ gst-launch \
 	! gate.sink \
 	audiotestsrc \
 		wave=0 \
-		freq=3 \
-	! audio/x-raw-float, width=64, rate=512 \
+		freq=0.125 \
+	! audio/x-raw-float, width=64, rate=${IN_SAMPLE} \
 	! tee \
 		name=control \
 	! queue \
 	! lal_nxydump \
 		start-time=0 \
-		stop-time=1000000000 \
+		stop-time=${STOP_TIME} \
 	! filesink \
 		location=control.txt \
 		buffer-mode=2 \
