d564571b7a288bb93a801479687e7bfe4020743d
kipp <kipp>
2009-02-12 00:02:08 +0000

    - be sure to include gnuscripts in the macro search path - fix bashism in 00init.sh

--- 00init.sh
+++ 00init.sh
@@ -2,11 +2,11 @@
 
 # Check with the user
 echo "This script (re-)builds the source tree's configuration and build system."
-read -p "Press CTRL-C to abort, or RETURN to continue... "
+read -p "Press CTRL-C to abort, or RETURN to continue... " INPUT
 
 # Get it done
 {
-	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal ; } &&
+	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal -I gnuscripts ; } &&
 	#{ echo "running autoheader..." ; autoheader ; } &&
 	{ echo "running automake..." ; automake -a -c ; } &&
 	{ echo "running autoconf..." ; autoconf ; } &&
