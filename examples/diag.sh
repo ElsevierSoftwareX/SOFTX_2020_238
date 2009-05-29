45635e3294950ef17aa3ce7778c79c837d0c9be6
kipp <kipp>
2009-05-29 16:11:24 +0000

    Add test_whiten()

--- examples/diag.sh
+++ examples/diag.sh
@@ -4,7 +4,7 @@ function test_adder() {
 		! lal_adder name=adder sync=true \
 		! audio/x-raw-float, width=64, rate=16384 \
 		! lal_nxydump start-time=0 stop-time=2250000000 \
-		! filesink location=dump.txt \
+		! filesink location="dump.txt" \
 		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 timestamp-offset=1250000000 \
 		! adder.
 }
@@ -14,7 +14,7 @@ function test_gate() {
 		lal_gate name=gate threshold=0.7 \
 		! audio/x-raw-float, width=64, rate=16384 \
 		! lal_nxydump start-time=0 stop-time=1000000000 \
-		! filesink location=dump.txt \
+		! filesink location="dump.txt" \
 		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 \
 		! audio/x-raw-float, rate=1024 \
 		! queue ! gate.control \
@@ -31,4 +31,14 @@ function test_resampler() {
 		! fakesink
 }
 
-test_resampler
+function test_whiten() {
+	gst-launch \
+		audiotestsrc wave=5 volume=1e-5 \
+		! audio/x-raw-float, width=64, rate=1024 \
+		! lal_whiten psd-mode=1 zero-pad=0 fft-length=8 average-samples=2 compensation-psd="unit_psd.txt" \
+		! lal_nxydump start-time=0 stop-time=300000000000 \
+		! progressreport \
+		! filesink location="dump.txt" buffer-mode=2
+}
+
+test_whiten
