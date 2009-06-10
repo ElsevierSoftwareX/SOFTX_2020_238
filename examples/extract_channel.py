743b9dadcb0584d08d268f920850b89e3ef4b64a
kipp <kipp>
2009-06-10 07:17:33 +0000

    Add some verbosity

--- examples/extract_channel.py
+++ examples/extract_channel.py
@@ -1,6 +1,10 @@
 import sys
 
 n = tuple(map(int, sys.argv[1:]))
-for line in sys.stdin:
+for i, line in enumerate(sys.stdin):
+	i += 1	# call the first line #1
+	if not i % 1000:
+		print >>sys.stderr, "line %d\r" % i,
 	line = line.strip().split()
 	print line[0], " ".join(map(line[1:].__getitem__, n))
+print >>sys.stderr, "line %d" % i
