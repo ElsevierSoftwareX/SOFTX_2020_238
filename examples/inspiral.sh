ee8d766856d19213e4e50bd44fc1414b26349ee3
kipp <kipp>
2009-06-15 02:07:35 +0000

    - take a stab at adding some command line options to lloid_gui - add an example script showing its use
    Hopefully this will make it easier to do bulk testing.

---
+++ examples/inspiral.sh
@@ -0,0 +1,7 @@
+./lloid_gui \
+	--frame-cache "/home/channa/scratch/frames/S5/strain-L2/LLO/L-L1_RDS_C03_L2-8741/L.cache" \
+	--gps-start-time 874106000.000000000 \
+	--gps-stop-time 874120000.000000000 \
+	--output "output.xml" \
+	--template-bank "/home/channa/cvs/lsware/gstlal/examples/H1-TMPLTBANK_09_1.207-874000000-2048.xml" \
+	--verbose
