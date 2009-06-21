a1c3ba3b8cefedd3f87b08dde838b6cb54d76f00
kipp <kipp>
2009-06-21 21:26:08 +0000

    - add before and after dumps to the gate test - change the ratio of the control signal frequency in the gate and resampler tests to something that is not commensurate with the buffer frequency

--- examples/diag.sh
+++ examples/diag.sh
@@ -14,12 +14,16 @@ function test_gate() {
 		lal_gate name=gate threshold=0.7 \
 		! audio/x-raw-float, width=64, rate=16384 \
 		! lal_nxydump start-time=0 stop-time=1000000000 \
-		! filesink location="dump.txt" \
-		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 \
+		! queue ! filesink location="dump_out.txt" \
+		audiotestsrc freq=13 samplesperbuffer=1024 num-buffers=8 \
 		! audio/x-raw-float, rate=1024 \
-		! queue ! gate.control \
+		! gate.control \
 		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 \
-		! queue ! gate.sink
+		! tee name=orig \
+		! gate.sink
+		orig. \
+		! lal_nxydump start-time=0 stop-time=1000000000 \
+		! queue ! filesink location="dump_in.txt"
 }
 
 function test_resampler() {
@@ -38,12 +42,11 @@ function test_resampler_gaps() {
 		! audio/x-raw-float, width=64, rate=2048 \
 		! lal_nxydump start-time=0 stop-time=1000000000 \
 		! queue ! filesink location="dump_out.txt" \
-		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 \
+		audiotestsrc freq=13 samplesperbuffer=1024 num-buffers=8 \
 		! audio/x-raw-float, rate=1024 \
 		! gate.control \
 		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 \
 		! audio/x-raw-float, width=64, rate=16384 \
-		! progressreport \
 		! tee name=orig \
 		! gate.sink \
 		orig. \
