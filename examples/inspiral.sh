28f46e59500b4f9344566c32dae7b836c8512f60
kipp <kipp>
2009-07-05 21:24:26 +0000

    Rename the --output-prefix option of lloid_gui to --output and use it to specify the file name

--- examples/inspiral.sh
+++ examples/inspiral.sh
@@ -3,6 +3,6 @@
 	--gps-start-time 873247860.000000000 \
 	--gps-stop-time 873424754.000000000 \
 	--instrument "H1" \
-	--output-prefix "output_" \
+	--output "output_H1.xml" \
 	--template-bank "/home/cromain/lloid_inspiral_comparison/tmpltbanksplit/chirpmass-1.126557_H1-TMPLTBANK_02-873250008-2048.xml.gz" \
 	--verbose
