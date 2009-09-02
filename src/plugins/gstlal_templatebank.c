8b2c87a1b421593959cf237627a9a92fb1a51b0d
kipp <kipp>
2009-09-02 06:35:06 +0000

    Fix typo.

--- src/plugins/gstlal_templatebank.c
+++ src/plugins/gstlal_templatebank.c
@@ -340,7 +340,7 @@ static int svd_create(GSTLALTemplateBank *element, int sample_rate)
 	 * generate orthonormal template bank
 	 */
 
-	generate_bank_and_svdnk_and_svd(&element->U, &element->S, &element->V, &element->chifacs, &A, element->template_bank_filename, element->reference_psd_filename, TEMPLATE_SAMPLE_RATE, TEMPLATE_SAMPLE_RATE / sample_rate, element->t_start, element->t_end, element->t_total_duration, TOLERANCE, verbose);
+	generate_bank_and_svd(&element->U, &element->S, &element->V, &element->chifacs, &A, element->template_bank_filename, element->reference_psd_filename, TEMPLATE_SAMPLE_RATE, TEMPLATE_SAMPLE_RATE / sample_rate, element->t_start, element->t_end, element->t_total_duration, TOLERANCE, verbose);
 	gsl_matrix_free(A);
 
 	/* 
