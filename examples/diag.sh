e64908c0b429593e67324efc78545b744cd4b6df
kipp <kipp>
2009-09-15 06:14:04 +0000

    My current version.

--- examples/diag.sh
+++ examples/diag.sh
@@ -2,7 +2,7 @@ function test_adder() {
 	gst-launch \
 		audiotestsrc freq=16 samplesperbuffer=1024 num-buffers=8 timestamp-offset=1000000000 \
 		! lal_adder name=adder sync=true \
-		! audio/x-raw-float, width=64, rate=16384 \
+		! audio/x-raw-float, channels=1, width=64, rate=16384 \
 		! lal_nxydump start-time=0 stop-time=2250000000 \
 		! filesink location="dump.txt" \
 		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=8 timestamp-offset=1250000000 \
@@ -12,7 +12,7 @@ function test_adder() {
 function test_gate() {
 	gst-launch \
 		lal_gate name=gate threshold=0.7 \
-		! audio/x-raw-float, width=64, rate=16384 \
+		! audio/x-raw-float, channels=1, width=64, rate=16384 \
 		! lal_nxydump start-time=0 stop-time=10000000000 \
 		! queue ! filesink location="dump_out.txt" \
 		audiotestsrc freq=13 samplesperbuffer=1024 num-buffers=8 \
@@ -28,40 +28,43 @@ function test_gate() {
 
 function test_resampler() {
 	gst-launch \
-		audiotestsrc \
-		! audio/x-raw-float, width=64, rate=16384 \
-		! gstlal-audioresample \
+		audiotestsrc wave=0 freq=1024 samplesperbuffer=10 num-buffers=1000000 \
+		! audio/x-raw-float, channels=1, width=64, rate=16384 \
+		! tee name=input \
+		! audioresample \
 		! audio/x-raw-float, rate=4096 \
-		! fakesink
+		! queue ! lal_nxydump ! filesink buffer-mode=2 location="dump_in.txt" \
+		input. \
+		! queue ! lal_nxydump ! filesink buffer-mode=2 location="dump_out.txt"
 }
 
 function test_resampler_gaps() {
 	gst-launch \
 		lal_gate name=gate threshold=0.7 \
 		! tee name=orig \
-		! audioresample \
+		! audioresample gap-aware=true \
 		! audio/x-raw-float, width=64, rate=2048 \
-		! lal_nxydump start-time=0 stop-time=10000000000 \
+		! lal_nxydump \
 		! queue ! filesink buffer-mode=2 location="dump_out.txt" \
-		audiotestsrc freq=13 samplesperbuffer=1024 num-buffers=80 \
+		audiotestsrc freq=15.8 samplesperbuffer=1024 num-buffers=8 \
 		! audio/x-raw-float, width=64, rate=1024 \
 		! tee name=control \
 		! gate.control \
-		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=2000 \
-		! audio/x-raw-float, width=64, rate=16384 \
+		audiotestsrc freq=256 samplesperbuffer=1024 num-buffers=128 \
+		! audio/x-raw-float, channels=1, width=64, rate=16384 \
 		! gate.sink \
 		control. \
-		! lal_nxydump start-time=0 stop-time=1000000000 \
+		! lal_nxydump \
 		! queue ! filesink buffer-mode=2 location="dump_control.txt" \
 		orig. \
-		! lal_nxydump start-time=0 stop-time=10000000000 \
+		! lal_nxydump \
 		! queue ! filesink buffer-mode=2 location="dump_in.txt"
 }
 
 function test_whiten() {
 	gst-launch \
 		audiotestsrc wave=9 volume=1e-2 \
-		! audio/x-raw-float, width=64, rate=1024, units="strain" \
+		! audio/x-raw-float, channels=1, width=64, rate=2048 \
 		! lal_whiten psd-mode=1 zero-pad=0 fft-length=8 median-samples=7 average-samples=128 \
 		! lal_nxydump start-time=0 stop-time=1200000000000 \
 		! progressreport \
@@ -71,7 +74,8 @@ function test_whiten() {
 function test_simulation() {
 	gst-launch \
 		audiotestsrc wave=9 volume=1e-21 timestamp-offset=873247800000000000 \
-		! audio/x-raw-float, width=64, rate=1024, units="strain" \
+		! audio/x-raw-float, channels=1, width=64, rate=2048 \
+		! taginject tags="instrument=H1,channel-name=LSC-STRAIN,units=strain" \
 		! lal_simulation xml-location="/home/dkeppel/lloid/HL-INJECTIONS_1_BNS_INJ-873247900-10.xml" \
 		! audioamplify clipping-method=3 amplification=1e20 \
 		! lal_nxydump start-time=873247900000000000 stop-time=873247910000000000 \
@@ -82,7 +86,8 @@ function test_simulation() {
 function test_simulation2wav() {
 	gst-launch \
 		audiotestsrc wave=9 volume=1e-21 timestamp-offset=873247900000000000 num-buffers=20 \
-		! audio/x-raw-float, width=64, rate=1024, units="strain" \
+		! audio/x-raw-float, channels=1, width=64, rate=2048 \
+		! taginject tags="instrument=H1,channel-name=LSC-STRAIN,units=strain" \
 		! lal_simulation xml-location="/home/dkeppel/lloid/HL-INJECTIONS_1_BNS_INJ-873247900-10.xml" \
 		! progressreport \
 		! audioamplify clipping-method=3 amplification=1e20 \
@@ -100,15 +105,6 @@ function test_framesrc() {
                         num-buffers=1000 \
                         name=framesrc \
                 ! lal_simulation xml-location="/home/dkeppel/lloid/HL-INJECTIONS_1_BNS_INJ-873247900-10.xml" \
-                ! audioresample ! audio/x-raw-float, rate=2048 \
-                ! progressreport \
-                ! fakesink
-}
-
-function test_pipeline() {
-        gst-launch \
-                audiotestsrc wave=9 volume=1e-21 timestamp-offset=873247900000000000 num-buffers=10 \
-                ! audio/x-raw-float, width=64, rate=16384 \
                 ! audioresample ! audio/x-raw-float, rate=2048 \
                 ! progressreport \
                 ! fakesink
@@ -134,5 +130,16 @@ function test_autochisq() {
 		! fakesink
 }
 
-test_autochisq
+function test_nto1() {
+	gst-launch \
+		input-selector name=nto1 select-all=true \
+		! fakesink sync=false \
+		audiotestsrc wave=9 volume=1e-2 \
+		! audio/x-raw-float, channels=1, width=64, rate=2048 \
+		! queue ! nto1. \
+		audiotestsrc wave=9 volume=1e-2 \
+		! audio/x-raw-float, channels=1, width=64, rate=2048 \
+		! queue ! nto1.
+}
 
+test_nto1
