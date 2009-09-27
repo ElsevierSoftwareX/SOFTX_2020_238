75289a0141331d2f931e8e4101da8bd2c664a363
kipp <kipp>
2009-09-27 00:06:56 +0000

    Remove fprintf()s

--- src/plugins/gstlal_whiten.c
+++ src/plugins/gstlal_whiten.c
@@ -978,9 +978,7 @@ static void class_init(gpointer class, gpointer class_data)
 	g_object_class_install_property(gobject_class, ARG_AVERAGE_SAMPLES, g_param_spec_uint("average-samples", "Average samples", "Number of FFTs used in PSD average", 1, G_MAXUINT, DEFAULT_AVERAGE_SAMPLES, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 	g_object_class_install_property(gobject_class, ARG_MEDIAN_SAMPLES, g_param_spec_uint("median-samples", "Median samples", "Number of FFTs used in PSD median history", 1, G_MAXUINT, DEFAULT_MEDIAN_SAMPLES, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 	g_object_class_install_property(gobject_class, ARG_DELTA_F, g_param_spec_double("delta-f", "Delta f", "PSD frequency resolution in Hz", 0, G_MAXDOUBLE, 0, G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
-fprintf(stderr, "a\n");
 	g_object_class_install_property(gobject_class, ARG_PSD, g_param_spec_value_array("psd", "PSD", "Power spectral density (first bin is at 0 Hz, bin spacing is delta-f)", g_param_spec_double("bin", "Bin", "Power spectral density bin", -G_MAXDOUBLE, G_MAXDOUBLE, 1.0, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS), G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
-fprintf(stderr, "b\n");
 }
 
 
