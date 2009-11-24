9473aed98b9154e25a6c91f6d6b5101b633184ce
kipp <kipp>
2009-11-24 00:01:39 +0000

    my working copy

--- examples/inspiral.sh
+++ examples/inspiral.sh
@@ -1,8 +1,13 @@
 ./lloid_gui \
 	--frame-cache "/home/kipp/gwf/cache" \
-	--gps-start-time 873247860.000000000 \
-	--gps-stop-time 873424754.000000000 \
+	--gps-start-time 873247860.0 \
+	--gps-stop-time 873424754.0 \
 	--instrument "H1" \
 	--output "output_H1.xml" \
-	--template-bank "/home/cromain/lloid_inspiral_comparison/tmpltbanksplit/chirpmass-1.126557_H1-TMPLTBANK_02-873250008-2048.xml.gz" \
+	--template-bank=/home/kipp/Development/gstlal/examples/H1-TMPLTBANK_02-873250008-2048.xml.gz \
 	--verbose
+
+exit
+
+	--injections /home/kipp/Development/gstlal/examples/HL-INJECTIONS_1_BNS_INJ-873247860-176894.xml \
+	--template-bank=/home/kipp/Development/gstlal/examples/H1-TMPLTBANK_{00,01,02,03,04,05,06,07,08,09}-873250008-2048.xml.gz \
