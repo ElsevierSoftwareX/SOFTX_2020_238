979e022ec83f6d97644f441041b65c9b9383f4a6
Kipp Cannon <kipp.cannon@ligo.org>
2009-11-27 02:14:14 -0800

    current version

--- tests/firbank_test_01.py
+++ tests/firbank_test_01.py
@@ -34,8 +34,8 @@ def firbank_test_01a(pipeline):
 	#
 
 	rate = 2048	# Hz
-	zero_pad = 0.0		# seconds
-	fft_length = 2.0	# seconds
+	gap_frequency = 13.0	# Hz
+	gap_threshold = 0.8	# of 1
 	buffer_length = 1.0	# seconds
 	test_duration = 10.0	# seconds
 	fir_length = 21	# samples
@@ -44,14 +44,14 @@ def firbank_test_01a(pipeline):
 	# build pipeline
 	#
 
-	head = test_common.gapped_test_src(pipeline, buffer_length = buffer_length, rate = rate, test_duration = test_duration, gap_frequency = 200, gap_threshold = .8)
+	head = test_common.gapped_test_src(pipeline, buffer_length = buffer_length, rate = rate, test_duration = test_duration, gap_frequency = gap_frequency, gap_threshold = gap_threshold, control_dump_filename = "firbank_test_01a_control.dump")
 	head = tee = pipeparts.mktee(pipeline, head)
 
 	fir_matrix = numpy.zeros((1, fir_length), dtype = "double")
-	latency = (fir_length - 1) / 2
-	fir_matrix[0, latency] = 1.0
+	middle = (fir_length - 1) / 2
+	fir_matrix[0, middle] = 1.0
 
-	head = pipeparts.mkfirbank(pipeline, head, fir_matrix = fir_matrix, latency = -latency)
+	head = pipeparts.mkfirbank(pipeline, head, fir_matrix = fir_matrix, latency = -middle)
 	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "firbank_test_01a_out.dump")
 	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, tee), "firbank_test_01a_in.dump")
 
