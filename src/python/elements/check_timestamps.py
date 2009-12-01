0db2b8e3431df7e54e2df828a769725e08406444
Kipp Cannon <kipp.cannon@ligo.org>
2009-12-01 10:28:41 -0800

    make better

--- src/python/elements/check_timestamps.py
+++ src/python/elements/check_timestamps.py
@@ -62,6 +62,11 @@ class CheckTimeStamps(gst.BaseTransform):
 				"channels = (int) [1, MAX], " +
 				"endianness = (int) BYTE_ORDER, " +
 				"width = (int) {32, 64};" +
+				"audio/x-raw-complex, " +
+				"rate = (int) [1, MAX], " +
+				"channels = (int) [1, MAX], " +
+				"endianness = (int) BYTE_ORDER, " +
+				"width = (int) {64, 128};" +
 				"audio/x-raw-int, " +
 				"rate = (int) [1, MAX], " +
 				"channels = (int) [1, MAX], " +
@@ -87,6 +92,11 @@ class CheckTimeStamps(gst.BaseTransform):
 				"channels = (int) [1, MAX], " +
 				"endianness = (int) BYTE_ORDER, " +
 				"width = (int) {32, 64};" +
+				"audio/x-raw-complex, " +
+				"rate = (int) [1, MAX], " +
+				"channels = (int) [1, MAX], " +
+				"endianness = (int) BYTE_ORDER, " +
+				"width = (int) {64, 128};" +
 				"audio/x-raw-int, " +
 				"rate = (int) [1, MAX], " +
 				"channels = (int) [1, MAX], " +
@@ -112,9 +122,8 @@ class CheckTimeStamps(gst.BaseTransform):
 
 
 	def do_set_caps(self, incaps, outcaps):
-		self.channels = incaps[0]["channels"]
-		self.width = incaps[0]["width"]
-		self.rate = incaps[0]["rate"]
+		self.unit_size = incaps[0]["width"] // 8 * incaps[0]["channels"]
+		self.units_per_second = incaps[0]["rate"]
 		return True
 
 
@@ -131,7 +140,11 @@ class CheckTimeStamps(gst.BaseTransform):
 		# initialize timestamp book-keeping
 		#
 
-		if self.t0 is None:
+		if self.t0 is None or bool(buf.flags & gst.BUFFER_FLAG_DISCONT):
+			if self.t0 is None:
+				print >>sys.stderr, "%s: initial timestamp = %s, offset = %d" % (self.get_property("name"), printable_timestamp(buf.timestamp), buf.offset)
+			elif bool(buf.flags & gst.BUFFER_FLAG_DISCONT):
+				print >>sys.stderr, "%s: discontinuity:  timestamp = %s, offset = %d" % (self.get_property("name"), printable_timestamp(buf.timestamp), buf.offset)
 			self.next_timestamp = self.t0 = buf.timestamp
 			self.next_offset = self.offset0 = buf.offset
 
@@ -144,13 +157,13 @@ class CheckTimeStamps(gst.BaseTransform):
 		if buf.offset != self.next_offset:
 			print >>sys.stderr, "%s: got offset %d expected %d" % (self.get_property("name"), buf.offset, self.next_offset)
 
-		expected_offset = self.offset0 + int(round((buf.timestamp - self.t0) * float(self.rate) / gst.SECOND))
+		expected_offset = self.offset0 + int(round((buf.timestamp - self.t0) * float(self.units_per_second) / gst.SECOND))
 		if buf.offset != expected_offset:
-			print >>sys.stderr, "%s: timestamp/offset mismatch:  at a rate of %d samples/s buffer's timestamp %s corresponds to offset %d, got %d" % (self.get_property("name"), self.rate, printable_timestamp(buf.timestamp), expected_offset, buf.offset)
+			print >>sys.stderr, "%s: timestamp/offset mismatch:  at a rate of %d samples/s buffer's timestamp %s corresponds to offset %d, got %d" % (self.get_property("name"), self.units_per_second, printable_timestamp(buf.timestamp), expected_offset, buf.offset)
 
 		length = buf.offset_end - buf.offset
-		if buf.size != length * (self.width // 8 * self.channels):
-			print >>sys.stderr, "%s: buffer length %d corresponds to size %d, got %d" % (self.get_property("name"), length, length * (self.width // 8 * self.channels), buf.size)
+		if buf.size != length * self.unit_size:
+			print >>sys.stderr, "%s: buffer length %d corresponds to size %d, got %d" % (self.get_property("name"), length, length * self.unit_size, buf.size)
 
 		#
 		# reset for next buffer
@@ -158,6 +171,10 @@ class CheckTimeStamps(gst.BaseTransform):
 
 		self.next_offset = buf.offset_end
 		self.next_timestamp = buf.timestamp + buf.duration
+
+		#
+		# done
+		#
 
 		return gst.FLOW_OK
 
