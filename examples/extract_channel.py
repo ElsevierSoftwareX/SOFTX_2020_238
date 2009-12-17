da77b42e2e52ad5fa24b94264eaea34f813ff344
Kipp Cannon <kipp.cannon@ligo.org>
2009-12-17 12:56:57 -0800

    try to improve speed

--- examples/extract_channel.py
+++ examples/extract_channel.py
@@ -1,10 +1,10 @@
 import sys
 
-n = tuple(map(int, sys.argv[1:]))
+n = tuple(int(a) + 1 for a in sys.argv[1:])
 for i, line in enumerate(sys.stdin):
 	i += 1	# call the first line #1
 	if not i % 1000:
 		print >>sys.stderr, "line %d\r" % i,
 	line = line.strip().split()
-	print line[0], " ".join(map(line[1:].__getitem__, n))
+	print line[0], " ".join(line[c] for c in n)
 print >>sys.stderr, "line %d" % i
