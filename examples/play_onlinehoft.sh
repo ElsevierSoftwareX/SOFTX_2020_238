b2fecb5c0ec7c2505c47148239649ab9b03c57ad
Leo Singer <leo.singer@ligo.org>
2010-04-27 18:20:54 -0700

    Added demo for onlinehoft element

---
+++ examples/play_onlinehoft.sh
@@ -0,0 +1,19 @@
+# A gst-launch pipeline to encode h(t) from NDS2 into a OGG/Vorbis file
+# The 'adder' element is included as a hack to throw away GPS time stamps; otherwise
+# the autoaudiosink won't play
+
+gst-launch --seek=`lalapps_tconvert now -12 hours`000000000 \
+	lal_onlinehoftsrc \
+        instrument=H1 \
+	! queue min-threshold-buffers=2 \
+	! audiochebband \
+		lower-frequency=45 \
+		upper-frequency=2500 \
+		poles=8 \
+	! audioamplify \
+		clipping-method=3 \
+		amplification=2e+17 \
+	! adder \
+	! audiorate \
+	! audioconvert \
+	! autoaudiosink
