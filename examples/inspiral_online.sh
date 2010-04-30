2dd7f96310f6050f0a53cbe12e9c69287df3051d
Leo Singer <leo.singer@ligo.org>
2010-04-30 14:58:35 -0700

    Changed inspiral_online to sqlite output

--- examples/inspiral_online.sh
+++ examples/inspiral_online.sh
@@ -1,9 +1,9 @@
-rm -f output_H1_bank*.xml
+rm -f output_H1.sqlite
 
 GST_DEBUG=3 gstlal_inspiral \
 	--online-data \
 	--instrument "H1" \
-	--output "output_H1.xml" \
+	--output "output_H1.sqlite" \
 	--gps-start-time `lalapps_tconvert now` \
 	--template-bank=banks/1-split_bank-H1-TMPLTBANK_DATAFIND-871157768-2048.xml.gz \
 	--reference-psd "reference_psd.xml.gz" \
