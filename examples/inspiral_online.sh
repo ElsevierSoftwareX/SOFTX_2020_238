2d0e234875e768874ab67fdea994cc58051c5c5a
Leo Singer <leo.singer@ligo.org>
2010-04-30 15:05:51 -0700

    Resolved PR 3217

--- examples/inspiral_online.sh
+++ examples/inspiral_online.sh
@@ -7,5 +7,6 @@ GST_DEBUG=3 gstlal_inspiral \
 	--gps-start-time `lalapps_tconvert now` \
 	--template-bank=banks/1-split_bank-H1-TMPLTBANK_DATAFIND-871157768-2048.xml.gz \
 	--reference-psd "reference_psd.xml.gz" \
+	--write-pipeline "inspiral_online" \
 	--comment "lloid rrocks" \
 	--verbose
