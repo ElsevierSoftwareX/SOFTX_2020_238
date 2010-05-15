ad21008659635bed922eba14814bd6244e140fc6
Leo Singer <leo.singer@ligo.org>
2010-05-15 12:30:07 -0700

    Abbreviated sandbox script

--- examples/gst-sandbox.py
+++ examples/gst-sandbox.py
@@ -3,44 +3,25 @@
 Example gst-python applicaiton for trying out new ideas
 """
 
-# The following snippet is taken from http://gstreamer.freedesktop.org/wiki/FAQ#Mypygstprogramismysteriouslycoredumping.2Chowtofixthis.3F
-import pygtk
-pygtk.require ("2.0")
-import gobject
-gobject.threads_init ()
-import pygst
-pygst.require ("0.10")
-import gst
+from gstlal.pipeutil import *
 
-# Shouldn't need pygtk or pygst
-del pygtk
-del pygst
+gps_start_time = 956858656
 
-gps_start_time = 956858656+1
-
-# Create a new source element
-src = gst.element_factory_make('lal_onlinehoftsrc')
-src.set_property('instrument', 'H1')
-
-# Create a new sink element
-sink = gst.element_factory_make('fakesink')
-
-# Construct pipeline
 pipeline = gst.Pipeline()
-pipeline.add_many(src, sink)
-gst.element_link_many(src, sink)
+src = mkelem('lal_onlinehoftsrc', {'instrument':'H1'})
+pipeline.add(src)
 
-# Play pipeline
-print "Setting state to PAUSED:", pipeline.set_state(gst.STATE_PAUSED)
-print pipeline.get_state()
-
-# Seek the source
-print "Seeking:", src.seek(1.0, gst.FORMAT_TIME, gst.SEEK_FLAG_FLUSH | gst.SEEK_FLAG_KEY_UNIT,
+src.send_event(gst.event_new_seek(
+	1.0, gst.FORMAT_TIME, gst.SEEK_FLAG_KEY_UNIT,
 	gst.SEEK_TYPE_SET, gps_start_time * gst.SECOND,
-	gst.SEEK_TYPE_NONE, -1)
+	gst.SEEK_TYPE_NONE, -1))
+
+
+snk = mkelem('fakesink')
+pipeline.add(snk)
+src.link(snk)
 
 print "Setting state to PLAYING:", pipeline.set_state(gst.STATE_PLAYING)
-print pipeline.get_state()
 
 # Start runloop
 mainloop = gobject.MainLoop()
