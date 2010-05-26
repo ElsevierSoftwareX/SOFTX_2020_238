2b012fb36c2ae8c789251796ea32f9b16b256ae4
Leo Singer <leo.singer@ligo.org>
2010-05-26 12:58:50 -0700

    plotlatency.py now plots diagonal rules showing 'realtime' contours

--- examples/plotlatency.py
+++ examples/plotlatency.py
@@ -18,10 +18,12 @@ from optparse import OptionParser
 # If a positional argument is given, try to open the file with that name.
 # Otherwise, read from stdin.
 if len(args) > 0:
-	file = open(args[0], 'r')
+	filename = args[0]
+	file = open(filename, 'r')
 else:
 	import sys
 	file = sys.stdin
+	filename = '/dev/stdin'
 
 
 # Regex matching "progress_(??:??:??) ????????? seconds"
@@ -41,15 +43,30 @@ for line in file:
 file.close()
 
 
+gps_start_time = min(trend[0][1] for trend in trends.values())
+gps_end_time = max(trend[-1][1] for trend in trends.values())
+gps_duration = gps_end_time - gps_start_time
+running_duration = max(trend[-1][0] for trend in trends.values())
+
 # Plot
 import pylab
+
+# Plot diagonal grid
+t0 = max(gps_duration, running_duration)
+for t in range(-t0, t0, 900):
+	pylab.plot((0, t0), (t, t+t0), color='#cccccc')
+
 for k, v in trends.iteritems():
 	data = pylab.array(v)
-	pylab.plot(data[:,0], data[:,1], label=k)
+	pylab.plot(data[:,0], data[:,1] - gps_start_time, label=k)
 
-pylab.grid()
+pylab.xlim((0, running_duration))
+pylab.ylim((0, gps_duration))
+pylab.gca().set_aspect('equal')
+
 pylab.legend(loc='lower right')
 pylab.xlabel('running time (seconds)')
-pylab.ylabel('stream time (seconds)')
+pylab.ylabel('stream time - %d (seconds)' % gps_start_time)
+pylab.title('Progress report for %s' % filename)
 pylab.show()
 
