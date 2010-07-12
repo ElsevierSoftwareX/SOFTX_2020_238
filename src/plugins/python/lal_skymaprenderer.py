f7f536d17eae30e9235e8e0f11efaed6872bb9ec
Erin Kara <erin.kara@ligo.org>
2010-07-12 16:12:00 -0700

    ready to push

--- src/plugins/python/lal_skymaprenderer.py
+++ src/plugins/python/lal_skymaprenderer.py
@@ -35,32 +35,15 @@ __date__ = "FIXME"
 # =============================================================================
 #
 
-
-import sys
-from gstlal.pipeutil import *
-
-import matplotlib
-matplotlib.rcParams.update({
-	"font.size": 8.0,
-	"axes.titlesize": 10.0,
-	"axes.labelsize": 10.0,
-	"xtick.labelsize": 8.0,
-	"ytick.labelsize": 8.0,
-	"legend.fontsize": 8.0,
-	"figure.dpi": 100,
-	"savefig.dpi": 100,
-	"text.usetex": True,
-	"path.simplify": True
-})
-from matplotlib import figure
-from matplotlib import cm
-from matplotlib import colors
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import numpy
 
+from gstlal.pipeutil import *
 from gstlal import pipeio
-from gstlal.elements import matplotlibcaps
-from gst.extend.pygobject import gproperty, with_construct_properties
+from gstlal import matplotlibhelper
+
+import matplotlib
+from matplotlib import cm
+from matplotlib import colors
 
 
 #
@@ -72,8 +55,7 @@ from gst.extend.pygobject import gproperty, with_construct_properties
 #
 
 
-
-class lal_skymaprenderer(gst.BaseTransform):
+class lal_skymaprenderer(matplotlibhelper.BaseMatplotlibTransform):
 	__gstdetails__ = (
 		'Skymap Plotting Element',
 		'Transform',
@@ -81,23 +63,9 @@ class lal_skymaprenderer(gst.BaseTransform):
 		__author__
 	)
 
-	gproperty(
-		gobject.TYPE_BOOLEAN,
-		"entire-sky",
-		"Set to TRUE to view a grid spanning the entire sky",
-		True,
-		construct=True
-	)
-
-	gproperty(
-		gobject.TYPE_BOOLEAN,
-		"colormap",
-		'Set to TRUE for colored map, FALSE for grayscale)',
-		True,
-		construct=True
-	)
 
 	__gsttemplates__ = (
+		matplotlibhelper.BaseMatplotlibTransform.__gsttemplates__,
 		gst.PadTemplate("sink",
 			gst.PAD_SINK,
 			gst.PAD_ALWAYS,
@@ -107,28 +75,13 @@ class lal_skymaprenderer(gst.BaseTransform):
 				"endianness = (int) BYTE_ORDER, " +
 				"width = (int) 64"
 			)
-		),
-		gst.PadTemplate("src",
-			gst.PAD_SRC,
-			gst.PAD_ALWAYS,
-			gst.caps_from_string(
-				matplotlibcaps + ", " +
-				"width = (int) [1, MAX], " +
-				"height = (int) [1, MAX]"
-			)
 		)
 	)
 
 
-	def __init__(self):
-		super(lal_skymaprenderer, self).__init__()
-		self.out_height = 300   # default, pixels
-		self.out_width = 600    # default, pixels
-		self.set_property("entire-sky", True)
-		self.set_property("colormap", True)
-
 	def do_get_unit_size(self, caps):
 		return pipeio.get_unit_size(caps)
+
 
 	def do_set_caps(self, incaps, outcaps):
 		channels = incaps[0]["channels"]
@@ -144,16 +97,9 @@ class lal_skymaprenderer(gst.BaseTransform):
 		# geographic longitude, span, log(probability)
 		#
 
-		# Set buffer metadata
-		outbuf.offset = inbuf.offset
-		outbuf.offset_end = inbuf.offset_end
-		outbuf.duration = inbuf.duration
-		outbuf.timestamp = inbuf.timestamp
-
 
 		# Convert raw buffer into numpy array
 		skymap_array = pipeio.array_from_audio_buffer(inbuf)
-
 
 
 		# Assign column of skymap_array as individual arrays
@@ -163,13 +109,9 @@ class lal_skymaprenderer(gst.BaseTransform):
 		logp = skymap_array[:,3]
 
 
+		# Build plot
+		cax, kw = matplotlib.colorbar.make_axes(self.axes)
 
-		# Generate plot
-		fig = figure.Figure()
-		FigureCanvas(fig)
-		fig.set_size_inches(self.out_width / float(fig.get_dpi()), self.out_height / float(fig.get_dpi()))
-		axes = fig.gca()
-		cax, kw = matplotlib.colorbar.make_axes(axes)
 
 		# Specify vertices of polygons
 		phi_vertices = numpy.array([phi-span/2, phi-span/2, phi+span/2, phi+span/2])
@@ -177,45 +119,32 @@ class lal_skymaprenderer(gst.BaseTransform):
 		theta_vertices = numpy.array([theta-span/2, theta+span/2, theta+span/2, theta-span/2])
 		theta_vertices = theta_vertices.T
 
-		# Allow for jet or gray colormap
-		if (self.get_property("colormap")):
-			colormap = matplotlib.colorbar.ColorbarBase(cax, norm=colors.Normalize(vmin=logp.min(), vmax=logp.max()), cmap=cm.jet)
-		else:
-			colormap = matplotlib.colorbar.ColorbarBase(cax, norm=colors.Normalize(vmin=logp.min(), vmax=logp.max()), cmap=cm.gray)
+
+		# Make ColorbarBase object
+		colormap = matplotlib.colorbar.ColorbarBase(cax, norm=colors.Normalize(vmin=logp.min(), vmax=logp.max()), cmap=cm.jet)
+
 
 		# Fill polygons with logp
 		for i in range(len(logp)):
-			axes.fill(phi_vertices[i], theta_vertices[i], facecolor=colormap.to_rgba(logp[i]), edgecolor='none')
-		axes.set_title(r"Signal Candidate Probability Distribution")
-		axes.set_xlabel(r"Geographic Latitude (radians)")
-		axes.set_ylabel(r"Geographic Longitude (radians)")
+			self.axes.fill(phi_vertices[i], theta_vertices[i], facecolor=colormap.to_rgba(logp[i]), edgecolor='none')
+		self.axes.set_title(r"Signal Candidate Probability Distribution")
+		self.axes.set_xlabel(r"Geographic Latitude (radians)")
+		self.axes.set_ylabel(r"Geographic Longitude (radians)")
 
 
-
-		if self.get_property("entire-sky"):
-			axes.set_xlim((0, 2*numpy.pi))
-			axes.set_ylim((0, numpy.pi))
-			axes.grid(True)
-			axes.invert_yaxis()
+		# Render to output buffer
+		matplotlibhelper.render(self.figure, outbuf)
 
 
-
-		# Extract pixel data
-		fig.canvas.draw()
-		rgba_buffer = fig.canvas.buffer_rgba(0,0)
-		rgba_buffer_size = len(rgba_buffer)
-
-
-
-		# Copy pixel data
-		outbuf[0:rgba_buffer_size] = rgba_buffer
-		outbuf.datasize = rgba_buffer_size
-
+		# Set buffer metadata
+		outbuf.offset = inbuf.offset
+		outbuf.offset_end = inbuf.offset_end
+		outbuf.duration = inbuf.duration
+		outbuf.timestamp = inbuf.timestamp		
 
 
 		# Done
 		return gst.FLOW_OK
-
 
 
 	def do_transform_caps(self, direction, caps):
@@ -235,17 +164,6 @@ class lal_skymaprenderer(gst.BaseTransform):
 
 		raise ValueError
 
-	def do_transform_size(self, direction, caps, size, othercaps):
-
-
-		if direction == gst.PAD_SINK:
-
-
-			# FIXME:  why is othercaps not the *other* caps?
-			return self.out_width * self.out_height * 4
-                        return othercaps[0]["width"] * othercaps[0]["height"] * othercaps[0]["bpp"] / 8
-
-		raise ValueError, direction
 
 # Register element class
 gstlal_element_register(lal_skymaprenderer)
