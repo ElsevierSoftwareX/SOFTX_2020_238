6fb7161cf03fe6ef93f0728f52f1d426877defa8
Leo Singer <leo.singer@ligo.org>
2010-07-27 11:32:48 -0700

    Added table of contents generation script

---
+++ examples/configurations/8hourly/buildToc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import glob
+import os.path
+
+print >>open('index.html', 'w'), """
+<html>
+<title>gstlal Online 8-Hourly Results Index</title>
+</head>
+<frameset cols="20%,80%">
+<frame src="index_list.html" id="listframe"/>
+<frame id="contentframe" />
+</frameset>
+</html>
+"""
+
+print >>open('index_list.html', 'w'), "<html><body>" + "".join([
+	'<a target="contentframe" href="%(dir)s/index_correlations.html">%(dir)s</a><br />' % {'dir':subdirname} for subdirname in glob.glob('????-??-??T??')]
+) + "</body><html>"
+
