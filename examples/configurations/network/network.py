14259699fbf6e5dab4639f79f05af8fd17b85b60
Chad Hanna <chad.hanna@ligo.org>
2010-08-12 16:31:56 -0700

    add output, comment out skymap

--- examples/configurations/network/network.py
+++ examples/configurations/network/network.py
@@ -10,6 +10,7 @@ opts, args = OptionParser(
 		Option("--gps-start-time", "-s", metavar="INT", type="int", help="GPS time at which to start analysis."),
 		Option("--gps-end-time", "-e", metavar="INT", type="int", help="GPS time at which to end analysis."),
 		Option("--template-bank", metavar="FILE", help="Name of template bank file."),
+		Option("--output", metavar="FILE", help="Name of output file. (.xml .xml.gz or .sqlite)"),
 	]
 ).parse_args()
 
@@ -22,6 +23,9 @@ from gstlal.gstlal_reference_psd import read_psd
 from gstlal import lloidparts
 from gstlal.pipeutil import gst, gobject
 from gstlal.lloidparts import mkelems_fast
+from gstlal import ligolw_output
+from glue import segments
+from pylal.datatypes import LIGOTimeGPS
 import sys
 
 
@@ -60,10 +64,12 @@ seekevent = gst.event_new_seek(
 
 
 coinc = mkelems_fast(pipeline, "lal_coinc")[-1]
-skymap = mkelems_fast(pipeline, coinc, "lal_skymap", {"bank-filename": opts.template_bank})[-1]
-mkelems_fast(pipeline, skymap, "fakesink")
+#skymap = mkelems_fast(pipeline, coinc, "lal_skymap", {"bank-filename": opts.template_bank})[-1]
+#mkelems_fast(pipeline, skymap, "fakesink")
+#mkelems_fast(pipeline, coinc, "fakesink")
 
-
+seg = segments.segment(LIGOTimeGPS(opts.gps_start_time), LIGOTimeGPS(opts.gps_end_time))
+data = {}
 for ifo in opts.instrument:
 	bank = read_bank("bank.%s.pickle" % ifo)
 	bank.logname = ifo # FIXME This is only need to give elements names, that should be automatic.
@@ -73,11 +79,29 @@ for ifo in opts.instrument:
 	basicsrc = lloidparts.mkLLOIDbasicsrc(pipeline, seekevent, ifo, None, online_data=True)
 	hoftdict = lloidparts.mkLLOIDsrc(pipeline, basicsrc, rates, psd=psd, psd_fft_length=opts.psd_fft_length)
 	snr_tee = lloidparts.mkLLOIDhoftToSnr(pipeline, hoftdict, ifo, bank, lloidparts.mkcontrolsnksrc(pipeline, max(rates)))
-	triggers = lloidparts.mkLLOIDsnrToTriggers(pipeline, snr_tee, bank)
-	triggers.link_pads("src", coinc, "sink%d")
-	mkelems_fast(pipeline, snr_tee, "queue", skymap)
+	triggers = lloidparts.mkLLOIDsnrToTriggers(pipeline, snr_tee, bank, lal_triggergen_algorithm=1, lal_triggergen_max_gap=0.1)
+	triggers_tee = mkelems_fast(pipeline, triggers, "tee")[-1]
+	# output a database for each detector
+	data[ifo] = ligolw_output.Data([ifo], tmp_space=None, output=ifo+"-"+opts.output, seg=seg, out_seg=seg, injections=None, comment="", verbose=True)
+	data[ifo].prepare_output_file(ligolw_output.make_process_params(opts))
+	mkelems_fast(pipeline, triggers_tee, "appsink", {"caps": gst.Caps("application/x-lal-snglinspiral"), "sync": False, "async": False, "emit-signals": True, "max-buffers": 1, "drop": True})[-1].connect_after("new-buffer", ligolw_output.appsink_new_buffer, data[ifo])
+	triggers_tee.link_pads("src%d", coinc, "sink%d")
+	#mkelems_fast(pipeline, snr_tee, "queue", skymap)
 
+#
+# output file
+#
+
+# FIXME make some of these kw args options
+data['all'] = ligolw_output.Data(opts.instrument, tmp_space=None, output="".join(opts.instrument)+"-"+opts.output, seg=seg, out_seg=seg, injections=None, comment="", verbose=True)
+data['all'].prepare_output_file(ligolw_output.make_process_params(opts))
+mkelems_fast(pipeline, coinc, "appsink", {"caps": gst.Caps("application/x-lal-snglinspiral"), "sync": False, "async": False, "emit-signals": True, "max-buffers": 1, "drop": True})[-1].connect_after("new-buffer", ligolw_output.appsink_new_buffer, data['all'])
+
+#
+# Ready set go!
+#
 
 pipeline.set_state(gst.STATE_PLAYING)
 gst.DEBUG_BIN_TO_DOT_FILE(pipeline, gst.DEBUG_GRAPH_SHOW_NON_DEFAULT_PARAMS, "network")
 mainloop.run()
+data.write_output_file()
