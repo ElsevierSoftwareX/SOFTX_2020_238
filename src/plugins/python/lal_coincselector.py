493b05e963c31dac6d0cadc4906fa43e2fcd2874
Leo Singer <leo.singer@ligo.org>
2010-08-16 12:17:26 -0700

    Just a convenient place to stop

--- src/plugins/python/lal_coincselector.py
+++ src/plugins/python/lal_coincselector.py
@@ -18,8 +18,11 @@ Select interesting coincidences based on estimated false alarm rates.
 """
 __author__ = "Leo Singer <leo.singer@ligo.org>"
 
+# FIXME: Pick which sngl_inspiral field to hijack.
+# Currently I am using rsqveto_duration to store per-detector IFAR.
 
 from gstlal.pipeutil import *
+import pylal.xlal.datatypes.snglinspiraltable as sngl
 
 
 class lal_coincselector(gst.BaseTransform):
@@ -29,7 +32,22 @@ class lal_coincselector(gst.BaseTransform):
 		__doc__,
 		__author__
 	)
-
+	__gproperties__ = {
+		'min-ifar': (
+			gobject.TYPE_UINT64,
+			'min-ifar',
+			'Minimum network IFAR (inverse false alarm rate, also known as extrapolated waiting time) in nanoseconds.  Coincidences with an estimated IFAR less than this value will be dropped.',
+			0, gobject.G_MAXULONG, 3600 * gst.SECOND,
+			gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT
+		),
+		'dt': ( # FIXME: could the coinc and coincselector elements share this piece of information?
+			gobject.TYPE_UINT64,
+			'dt',
+			'Coincidence window in nanoseconds.',
+			0, gobject.G_MAXULONG, 3600 * gst.SECOND,
+			gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT
+		),
+	}
 	__gsttemplates__ = (
 		gst.PadTemplate("sink",
 			gst.PAD_SINK, gst.PAD_ALWAYS,
@@ -47,6 +65,18 @@ class lal_coincselector(gst.BaseTransform):
 		)
 	)
 
+
+	def do_transform(self, inbuf, outbuf):
+		min_ifar = self.get_property('min-ifar')
+		dt = float(self.get_property('dt'))
+		rows = sngl.from_buffer(outbuf.data)
+		stride = outbuf.caps[0]['channels']
+		for sngl_group in [rows[i*stride:i*stride+stride] for i in range(len(rows) / stride)]:
+			ifar = dt
+			for row in sngl_group:
+				if any(buffer(row)):
+					ifar *= row.rsqveto_duration / dt
+			
 
 
 # Register element class
