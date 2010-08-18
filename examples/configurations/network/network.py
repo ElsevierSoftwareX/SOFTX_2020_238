e0668682540e63cf0dbf8649e9aed6c37fb382e5
Leo Singer <leo.singer@ligo.org>
2010-08-18 15:52:20 -0700

    Took out LLOIDHandler in favor of class that now lives in lloidparts

--- examples/configurations/network/network.py
+++ examples/configurations/network/network.py
@@ -29,31 +29,9 @@ from pylal.datatypes import LIGOTimeGPS
 import sys
 
 
-# FIXME: This class, or something like it, occurs in many of our pipelines.
-# Maybe we could put a generalized version of it in a library?
-class LLOIDHandler(object):
-	def __init__(self, mainloop, pipeline):
-		self.mainloop = mainloop
-		self.pipeline = pipeline
-
-		bus = pipeline.get_bus()
-		bus.add_signal_watch()
-		bus.connect("message", self.on_message)
-
-	def on_message(self, bus, message):
-		if message.type == gst.MESSAGE_EOS:
-			self.pipeline.set_state(gst.STATE_NULL)
-			self.mainloop.quit()
-		elif message.type == gst.MESSAGE_ERROR:
-			gerr, dbgmsg = message.parse_error()
-			self.pipeline.set_state(gst.STATE_NULL)
-			self.mainloop.quit()
-			sys.exit("error (%s:%d '%s'): %s" % (gerr.domain, gerr.code, gerr.message, dbgmsg))
-
-
 pipeline = gst.Pipeline()
 mainloop = gobject.MainLoop()
-handler = LLOIDHandler(mainloop, pipeline)
+handler = lloidparts.LLOIDHandler(mainloop, pipeline)
 
 
 seekevent = gst.event_new_seek(
