cb2d9c07eae36c99cfd3464d03343b02fc659ca6
Leo Singer <leo.singer@ligo.org>
2010-08-17 17:15:08 -0700

    ligolw_output: index only created if it doesn't exist yet

--- src/python/ligolw_output.py
+++ src/python/ligolw_output.py
@@ -226,7 +226,7 @@ class Data(object):
 				self.time_slide_table = lsctables.table.get_table(self.xmldoc, lsctables.TimeSlideTable.tableName)
 				self.coinc_definer = lsctables.table.get_table(self.xmldoc, lsctables.CoincDefTable.tableName)
 				# FIXME add snr index for quick loudest event tables
-				self.connection.cursor().execute("CREATE INDEX snrix ON coinc_inspiral(snr)")
+				self.connection.cursor().execute("CREATE INDEX IF NOT EXISTS snrix ON coinc_inspiral(snr)")
 		else:
 			self.xmldoc = xmldoc
 			self.sngl_inspiral_table = sngl_inspiral_table
