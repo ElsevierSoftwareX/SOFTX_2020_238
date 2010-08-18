9d3aca975250cf7d12bf36250006546e065270a0
Leo Singer <leo.singer@ligo.org>
2010-08-17 22:38:58 -0700

    coincselector: caught exceptions

--- src/plugins/python/lal_coincselector.py
+++ src/plugins/python/lal_coincselector.py
@@ -21,6 +21,7 @@ __author__ = "Leo Singer <leo.singer@ligo.org>"
 
 from gstlal.pipeutil import *
 from gstlal.pipeio import net_ifar, sngl_inspiral_groups_from_buffer, sngl_inspiral_groups_to_buffer
+import traceback
 
 
 class lal_coincselector(gst.Element):
@@ -89,18 +90,22 @@ class lal_coincselector(gst.Element):
 
 
 	def chain(self, pad, inbuf):
-		# FIXME: Pick which sngl_inspiral field to hijack.
-		# Currently I am using alpha to store per-detector IFAR.
-		buf = sngl_inspiral_groups_to_buffer(
-			(group for group in sngl_inspiral_groups_from_buffer(inbuf) if net_ifar((row.alpha for row in group), float(self.__dt)) >= float(self.__min_ifar)),
-			inbuf.caps[0]['channels'])
-		buf.offset = inbuf.offset
-		buf.offset_end = inbuf.offset_end
-		buf.timestamp = inbuf.timestamp
-		buf.duration = inbuf.duration
-		buf.caps = inbuf.caps
-		# FIXME: copy flags too
-		return self.__srcpad.push(inbuf)
+		try: # FIXME: apparently the gst.Pad wrapper silences exceptions from chain() routines.
+			# FIXME: Pick which sngl_inspiral field to hijack.
+			# Currently I am using alpha to store per-detector IFAR.
+			buf = sngl_inspiral_groups_to_buffer(
+				(group for group in sngl_inspiral_groups_from_buffer(inbuf) if net_ifar((row.alpha for row in group), float(self.__dt)) >= float(self.__min_ifar)),
+				inbuf.caps[0]['channels'])
+			buf.offset = inbuf.offset
+			buf.offset_end = inbuf.offset_end
+			buf.timestamp = inbuf.timestamp
+			buf.duration = inbuf.duration
+			buf.caps = inbuf.caps
+			# FIXME: copy flags too
+			return self.__srcpad.push(inbuf)
+		except:
+			self.error(traceback.format_exc())
+			return gst.FLOW_ERROR
 
 
 # Register element class
