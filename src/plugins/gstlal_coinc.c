c10252dcc51f6d30c7d2ccc75bac861f4cf5a5cf
Leo Singer <leo.singer@ligo.org>
2010-09-15 14:08:18 -0700

    lal_coinc: added debug category

--- src/plugins/gstlal_coinc.c
+++ src/plugins/gstlal_coinc.c
@@ -23,6 +23,10 @@
 #include <lal/LIGOMetadataTables.h>
 
 
+#define GST_CAT_DEFAULT gstlal_coinc_debug
+GST_DEBUG_CATEGORY_STATIC(GST_CAT_DEFAULT);
+
+
 static GstElementClass *parent_class = NULL;
 
 
@@ -661,6 +665,7 @@ GType gstlal_coinc_get_type(void)
 			.instance_init = instance_init,
 		};
 		type = g_type_register_static(GST_TYPE_ELEMENT, "lal_coinc", &info, 0);
+		GST_DEBUG_CATEGORY_INIT(GST_CAT_DEFAULT, "coinc", 0, "coinc element");
 	}
 
 	return type;
