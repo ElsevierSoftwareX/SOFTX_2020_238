9e88fa5f01ece39939f34c4301aab28ba00c404d
Nickolas Fotopoulos <nvf@gravity.phys.uwm.edu>
2010-09-27 12:57:33 -0700

    autoupdate.js: now both thumbnails and lightbox link update correctly

--- src/data/www/assets/autoupdate.js
+++ src/data/www/assets/autoupdate.js
@@ -48,17 +48,21 @@
 		this.each(function() {
 			var obj = this;
 			var old_src = $(this).attr('src');
+			var parent = $(obj).parent();
+			var old_href = undefined;
+			if (parent.is('a'))
+				old_href=parent.attr('href');
 			var func = function() {
 				window.setTimeout(function() {
 					// FIXME: we could load the image into a separate img object,
 					// then swap URLs; this might help to avoid flicker.
-					var new_src = old_src + "#e" + Math.random();
+					var new_id = "#e" + Math.random();
 					if ($(obj).is(':visible'))
-						obj.src = new_src;
+						obj.src = old_src + new_id;
 					// This is a hack to get fancybox to work right.
 					var parent = $(obj).parent();
-					if (parent.is('a[href^="'+old_src+'"]'))
-						parent.attr('href', new_src);
+					if (parent.is('a'))
+						parent.attr('href', old_href + new_id);
 					func();
 				}, interval);
 			};
