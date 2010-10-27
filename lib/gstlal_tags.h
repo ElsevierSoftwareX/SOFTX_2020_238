9f712954068cc13caf6f5c677830da31fd0b163b
Kipp Cannon <kipp.cannon@ligo.org>
2010-10-27 10:23:08 +0200

    Leo has contributed to this as well

--- lib/gstlal_tags.h
+++ lib/gstlal_tags.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2000,2001,2008  Kipp C. Cannon
+ * Copyrigth (C) 2010  Leo Singer
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
