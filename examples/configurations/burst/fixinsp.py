034d8e6c11f89332f33840ebf9c43cfbe013b73c
Chad Hanna <chad.hanna@ligo.org>
2010-11-11 20:44:13 -0500

    to fix inspiral injs

---
+++ examples/configurations/burst/fixinsp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+from glue.ligolw import lsctables
+from glue.ligolw import utils
+
+fname = sys.argv[1]
+
+xmldoc = utils.load_filename(fname)
+tbl = lsctables.table.get_table(xmldoc, "sim_inspiral")
+start = tbl[0].geocent_end_time - 100
+for row in tbl: row.geocent_end_time -= start
+utils.write_filename(xmldoc, fname)
