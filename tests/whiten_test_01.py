858c6c65991cbb30a57d0593b173b0b2d9387954
Kipp Cannon <kipp.cannon@ligo.org>
2010-12-07 09:56:06 -0800

    unit test:  renormalize whiten test 01a

--- tests/whiten_test_01.py
+++ tests/whiten_test_01.py
@@ -39,8 +39,9 @@ def whiten_test_01a(pipeline):
 
 	def psd_resolution_changed(elem, pspec, ignored):
 		delta_f = elem.get_property("delta-f")
-		n = int(round(elem.get_property("f-nyquist") / delta_f) + 1)
-		elem.set_property("mean-psd", numpy.zeros((n,), dtype="double") + 2.0 * delta_f)
+		f_nyquist = elem.get_property("f-nyquist")
+		n = int(round(f_nyquist / delta_f) + 1)
+		elem.set_property("mean-psd", numpy.zeros((n,), dtype="double") + 2.0 * delta_f * (4 / f_nyquist))
 
 	#
 	# try changing these.  test should still work!
@@ -56,7 +57,7 @@ def whiten_test_01a(pipeline):
 	# build pipeline
 	#
 
-	head = test_common.test_src(pipeline, buffer_length = buffer_length, rate = rate, test_duration = test_duration)
+	head = test_common.test_src(pipeline, buffer_length = buffer_length, rate = rate, test_duration = test_duration, wave = 9)
 	head = tee = pipeparts.mktee(pipeline, head)
 	head = pipeparts.mkwhiten(pipeline, head, psd_mode = 1, zero_pad = zero_pad, fft_length = fft_length)
 	head.connect_after("notify::f-nyquist", psd_resolution_changed, None)
