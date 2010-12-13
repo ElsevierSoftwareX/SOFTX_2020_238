704a6771eee2ebf8e71fca49187320769529ecfb
Chad Hanna <chad.hanna@ligo.org>
2010-12-13 15:03:41 -0500

    fixed warnings

--- gst/lal/gstlal_mean.c
+++ gst/lal/gstlal_mean.c
@@ -69,28 +69,29 @@ static int mean_process(GSTLALMean *element, guint64 available_length, guint64 o
 {
 	/* FIXME assumed to be stable over size of input buffer, probably fine */
 	guint64 i,j,k;
-	if (!element->sum1) element->sum1 = (double *) calloc(element->channels, sizeof(double));
-	if (!element->sum2) element->sum2 = (double *) calloc(element->channels, sizeof(double));
+	guint64 channels = (guint64) element->channels;
+	if (!element->sum1) element->sum1 = (double *) calloc(channels, sizeof(double));
+	if (!element->sum2) element->sum2 = (double *) calloc(channels, sizeof(double));
 
-	memset((void *) element->sum1, 0, sizeof(double) * element->channels);
-	memset((void *) element->sum2, 0, sizeof(double) * element->channels);
+	memset((void *) element->sum1, 0, sizeof(double) * channels);
+	memset((void *) element->sum2, 0, sizeof(double) * channels);
 
 	/* pre compute the sum for the first sample */
 	guint64 offset = available_length - output_length;
-	for (j = 0; j < element->channels; j++) {
+	for (j = 0; j < channels; j++) {
 		for(k = 0; k < element->n-1; k++) {
 			if(k > offset) break;
-			element->sum2[j] += pow(in[(offset - k) * element->channels + j], element->moment);
+			element->sum2[j] += pow(in[(offset - k) * channels + j], element->moment);
 		}
 	}
 	
 	for(i = 0; i < output_length; i++) {
 		/* How far to look back in the input */
 		offset = available_length - output_length + i;
-		for (j = 0; j < element->channels; j++) {
-			element->sum2[j] += pow(in[offset * element->channels + j], element->moment);
-			element->sum1[j] += pow(in[(offset - element->n + 1) * element->channels + j], element->moment);
-			out[i*element->channels +j] = (element->sum2[j] - element->sum1[j]) / element->n;
+		for (j = 0; j < channels; j++) {
+			element->sum2[j] += pow(in[offset * channels + j], element->moment);
+			element->sum1[j] += pow(in[(offset - element->n + 1) * channels + j], element->moment);
+			out[i*channels +j] = (element->sum2[j] - element->sum1[j]) / element->n;
 		}
 	}
 	return 0;
@@ -100,27 +101,28 @@ static int thresh_process(GSTLALMean *element, guint64 available_length, guint64
 {
 	guint64 i,j,k;
 	double thresh = element->thresh;
+	guint64 channels = (guint64) element->channels;
 
-	if (!element->lastcross) element->lastcross = (guint64 *) calloc(element->channels, sizeof(guint64));
+	if (!element->lastcross) element->lastcross = (guint64 *) calloc(channels, sizeof(guint64));
 
-	memset((void *) element->lastcross, 0, sizeof(guint64) * element->channels);
+	memset((void *) element->lastcross, 0, sizeof(guint64) * channels);
 
 	/* pre compute the threshold crossing for the first sample */
 	guint64 offset = available_length - output_length;
-	for (j = 0; j < element->channels; j++) {
+	for (j = 0; j < channels; j++) {
 		for(k = 0; k < element->n-1; k++) {
 			if(k > offset) break;
-			if (fabs(in[(offset - k) * element->channels + j]) >= thresh) element->lastcross[j] = offset - k;
+			if (fabs(in[(offset - k) * channels + j]) >= thresh) element->lastcross[j] = offset - k;
 		}
 	}
 	
 	for(i = 0; i < output_length; i++) {
 		/* How far to look back in the input */
 		offset = available_length - output_length + i;
-		for (j = 0; j < element->channels; j++) {
-			if (fabs(in[offset * element->channels + j]) >= thresh) element->lastcross[j] = offset - k;
-			if (offset - element->lastcross[j] > element->n) out[i*element->channels +j] = 0.0;
-			else out[i*element->channels +j] = in[offset * element->channels + j];
+		for (j = 0; j < channels; j++) {
+			if (fabs(in[offset * channels + j]) >= thresh) element->lastcross[j] = offset - k;
+			if (offset - element->lastcross[j] > element->n) out[i*channels +j] = 0.0;
+			else out[i*channels +j] = in[offset * channels + j];
 		}
 	}
 	return 0;
@@ -130,18 +132,19 @@ static int max_over_n_process(GSTLALMean *element, guint64 available_length, gui
 {
 	guint64 i,j,k;
 	double currentvalue = 0.;
+	guint64 channels = (guint64) element->channels;
 
-	if (!element->max) element->max = (double *) calloc(element->channels, sizeof(double));
-	if (!element->lastmax) element->lastmax = (guint64 *) calloc(element->channels, sizeof(guint64));
+	if (!element->max) element->max = (double *) calloc(channels, sizeof(double));
+	if (!element->lastmax) element->lastmax = (guint64 *) calloc(channels, sizeof(guint64));
 
 	/* pre compute the max for the first sample */
 	guint64 offset = available_length - output_length;
-	for (j = 0; j < element->channels; j++) {
-		element->max[j] = pow(in[(offset - element->n + 1) * element->channels + j], element->moment);
+	for (j = 0; j < channels; j++) {
+		element->max[j] = pow(in[(offset - element->n + 1) * channels + j], element->moment);
 		element->lastmax[j] = offset - element->n;
 		for(k = 0; k < element->n-1; k++) {
 			if(k > offset) break;
-			currentvalue = pow(in[(offset - k) * element->channels + j], element->moment);
+			currentvalue = pow(in[(offset - k) * channels + j], element->moment);
 			if (currentvalue >= element->max[j] ) {
 				element->max[j] = currentvalue;
 				element->lastmax[j] = offset-k;
@@ -152,30 +155,30 @@ static int max_over_n_process(GSTLALMean *element, guint64 available_length, gui
 	for(i = 0; i < output_length; i++) {
 		/* How far to look back in the input */
 		offset = available_length - output_length + i;
-		for (j = 0; j < element->channels; j++) {
+		for (j = 0; j < channels; j++) {
 
 			/* Check to see if the current value exceeds the previous maximum */
-			currentvalue = pow(in[(offset) * element->channels + j], element->moment);
+			currentvalue = pow(in[(offset) * channels + j], element->moment);
 			if (currentvalue >= element->max[j]) {
 				element->max[j] = currentvalue;
 				element->lastmax[j] = offset;
 			}
 
 			/* Check to see if the last maximum was recent, and use it if so */
-			if ( (offset - element->lastmax[j]) < element->n ) out[i*element->channels +j] = element->max[j];
+			if ( (offset - element->lastmax[j]) < element->n ) out[i*channels +j] = element->max[j];
 			/* Otherwise recompute the maximum over the last n samples */
 			else {
-				element->max[j] = pow(in[(offset - element->n + 1) * element->channels + j], element->moment);
+				element->max[j] = pow(in[(offset - element->n + 1) * channels + j], element->moment);
 				element->lastmax[j] = offset - element->n;
 				for(k = 0; k < element->n; k++) {
 					if(k > offset) break;
-					currentvalue = pow(in[(offset - k) * element->channels + j], element->moment);
+					currentvalue = pow(in[(offset - k) * channels + j], element->moment);
 					if (currentvalue >= element->max[j] ) {
 						element->max[j] = currentvalue;
 						element->lastmax[j] = offset - k;
 					}
 				}
-			out[i*element->channels +j] = element->max[j];
+			out[i*channels +j] = element->max[j];
 			}
 		}
 	}
@@ -187,27 +190,28 @@ static int max_every_n_process(GSTLALMean *element, guint64 available_length, gu
 	guint64 i,j,k;
 	double currentvalue = 0.;
 	guint64 offset = 0;
+	guint64 channels = (guint64) element->channels;
 
-	if (!element->max) element->max = (double *) calloc(element->channels, sizeof(double));
-	if (!element->lastmax) element->lastmax = (guint64 *) calloc(element->channels, sizeof(guint64));
+	if (!element->max) element->max = (double *) calloc(channels, sizeof(double));
+	if (!element->lastmax) element->lastmax = (guint64 *) calloc(channels, sizeof(guint64));
 
 	for(i = 0; i < output_length; i++) {
 		/* How far to look back in the input */
 		offset = available_length - output_length + i;
-		for (j = 0; j < element->channels; j++) {
+		for (j = 0; j < channels; j++) {
 			if (!(i % element->n)) {
-				element->max[j] = pow(in[(offset) * element->channels + j], element->moment);
+				element->max[j] = pow(in[(offset) * channels + j], element->moment);
 				element->lastmax[j] = offset;
 				for(k = 0; k < element->n; k++) {
 					if(k > offset) break;
-					currentvalue = pow(in[(offset - k) * element->channels + j], element->moment);
+					currentvalue = pow(in[(offset - k) * channels + j], element->moment);
 					if (currentvalue >= element->max[j] ) {
 						element->max[j] = currentvalue;
 						element->lastmax[j] = offset - k;
 					}
 				}
 			}
-			out[i*element->channels +j] = element->max[j];
+			out[i*channels +j] = element->max[j];
 		}
 	}
 	return 0;
@@ -395,7 +399,7 @@ enum property {
 #define TYPE_INTEGRAL 2
 #define TYPE_MAX_OVER_N 3
 #define TYPE_MAX_EVERY_N 4
-
+#define TYPE_THRESH 5
 
 /*
  * ============================================================================
@@ -465,7 +469,7 @@ static gboolean set_caps(GstBaseTransform *trans, GstCaps *incaps, GstCaps *outc
 	if (element->type == TYPE_INTEGRAL) element->process = GST_DEBUG_FUNCPTR(mean_process);
 	if (element->type == TYPE_MAX_OVER_N) element->process = GST_DEBUG_FUNCPTR(max_over_n_process);
 	if (element->type == TYPE_MAX_EVERY_N) element->process = GST_DEBUG_FUNCPTR(max_every_n_process);
-
+	if (element->type == TYPE_THRESH) element->process = GST_DEBUG_FUNCPTR(thresh_process);
 	return success;
 }
 
