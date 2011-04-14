c3ae48107aeb222af4c6d1a81065277d1ab2b682
Shaun Hooper <hoopes01@student.uwa.edu.au>
2011-04-14 14:22:53 +0800

    Changed test pipeline alot. I don't actually know why it won't work anymore

--- tests/iir_pipeline_test.py
+++ tests/iir_pipeline_test.py
@@ -45,14 +45,14 @@ def iirbank_test_01a(pipeline):
         gap_threshold = 0.0     # of 1
         buffer_length = 1.0     # seconds
         test_duration = 5.0     # seconds
-        wave = 10               # ticks
-        volume = 1.0;
-        freq = 0.01#12.0
+        wave = 0               # ticks
+        volume = 1.0
+        freq = 50#0.01
 
         #
         # build pipeline
         #
-        print "Before Test Src"
+
         head = test_common.gapped_test_src(pipeline,
                                            wave = wave,
                                            freq = freq,
@@ -62,48 +62,53 @@ def iirbank_test_01a(pipeline):
                                            gap_frequency = gap_frequency,
                                            gap_threshold = gap_threshold,
                                            control_dump_filename = "iirbank_test_01a_control.dump")
-        print "After Test Src"
-        head = tee = pipeparts.mktee(pipeline, head)
 
+        ##head = tee = tee2 = pipeparts.mktee(pipeline, head)
+	##pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, tee), "iirbank_test_01a_in.dump")
         #
         # Create IIR bank
         #
 
-	#a1, b0, delay = cbc_template_iir.makeiirbank()
-#       the = 1.0
-
-        #ip = spawaveform.iirinnerproduct(a1, b0, delay, psd)
-
-        #M = numpy.loadtxt('test.out')
-        #amp = M[:,0]
-        #phase = M[:,1]
 	amp, phase, f = cbc_template_iir.waveform(1.4, 1.4, 40, 1500, 4096)
-	#for x in zip(amp, phase): print "%f %f\n" %x 
-        a1, b0, delay = spawaveform.iir(amp, phase, 0.04, 0.9, 0.25)
-        psd = numpy.ones(amp.shape[0]/2)
-	a1 =numpy.array([a1])
-	b0 =numpy.array([b0])
-	delay =numpy.array([delay])
-	print a1.shape, b0.shape, delay.shape
+	#numpy.savetxt('amp.txt',numpy.vstack((amp, phase)))
+	##f = open("ampnew.txt", "w")
+	##for n in range(len(amp)):
+	##	print >>f, "%f, %f" % (amp[n], phase[n])
+	##f.close()
+
+        ##a1, b0, delay = spawaveform.iir(amp, phase, 0.04, 0.9, 0.25)
+	##out = spawaveform.iirresponse(test_duration * rate, a1, b0, delay)
+	#numpy.savetxt('respnew.txt',(numpy.vstack(out.real), numpy.vstack(out.imag)))
+	##f = open("response.txt","w")
+	##for n in range(len(out)):
+	##	print >>f, "%f, %f" % (out[n].real, out[n].imag)
+	##f.close()
+        ##psd = numpy.ones(amp.shape[0]/2)
+	##a1 =numpy.array([a1])
+	##b0 =numpy.array([b0])
+	##delay =numpy.array([delay])
+	##print a1.shape, b0.shape, delay.shape
         #
         # Build rest of pipeline
         #
 
-        head = pipeparts.mkiirbank(pipeline, head, a1 = a1, b0 = b0, delay = delay)
+        ##head = pipeparts.mkiirbank(pipeline, head, a1 = a1, b0 = b0, delay = delay)
+	##head = outtee = pipeparts.mktee(pipeline, head)
+	##pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "iirbank_test_01a_out.dump")
+	##print >>sys.stderr, "finished making iir bank"
 
-        head = outtee = pipeparts.mktee(pipeline, head)
         #outtee = tee = pipeparts.mktee(pipeline, tee)
-        pipeparts.mkplaybacksink(pipeline, pipeparts.mkqueue(pipeline,outtee))
-
-        pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "iirbank_test_01a_out.dump")
-        pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, tee), "iirbank_test_01a_in.dump")
+        pipeparts.mkplaybacksink(pipeline, pipeparts.mkqueue(pipeline, head))
+	##print >>sys.stderr, "finished playback"
+        
+        
 
         #
         # done
         #
-        # gst.DEBUG_BIN_TO_DOT_FILE(pipeline, gst.DEBUG_GRAPH_SHOW_NON_DEFAULT_PARAMS, "iir_single_waveform_pipeline")
-	gst.DEBUG_BIN_TO_DOT_FILE(pipeline, gst.DEBUG_GRAPH_SHOW_NON_DEFAULT_PARAMS, "iir_single_waveform_pipeline")
 
+	gst.DEBUG_BIN_TO_DOT_FILE(pipeline, gst.DEBUG_GRAPH_SHOW_NON_DEFAULT_PARAMS, "iir_single_waveform_pipeline_new")
+	print >>sys.stderr, "finished pipeline"
         return pipeline
 
 
@@ -118,7 +123,7 @@ def iirbank_test_01a(pipeline):
 #name = "iirbank_test_01a"
 #mainloop = gobject.MainLoop()
 #pipeline = gst.Pipeline(name)
-#handler = Handler(mainloop, iirbank_test_01a(pipeline))
+#handler = test_common.Handler(mainloop, iirbank_test_01a(pipeline))
 
 #pipeline = gst.Pipeline()
 #pipeline = iirbank_test_01a(pipeline)
@@ -129,5 +134,5 @@ def iirbank_test_01a(pipeline):
 
 test_common.build_and_run(iirbank_test_01a, "iirbank_test_01a")
 
-
+print >>sys.stderr, "exit"
 #numpy.zeros((1, ), dtype = "cdouble")
