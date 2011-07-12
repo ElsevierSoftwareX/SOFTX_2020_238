9e32c5d01f6256dd346cf94bf28966f0ca48bf10
Kipp Cannon <kipp.cannon@ligo.org>
2011-07-12 15:17:36 -0500

    gstlal.lloidparts:  three adjustments
    - edit the definition of effective SNR
    - require triple coincidence
    - add the maximum magnitude of the time slide offsets to the coincidence back-off time

--- python/lloidparts.py
+++ python/lloidparts.py
@@ -805,7 +805,7 @@ class StreamThinca(object):
 		# a \Delta t only coincidence test it's the \Delta t window
 		# not including the light travel time
 		self.coincidence_threshold = coincidence_threshold
-		self.coincidence_back_off = coincidence_back_off
+		self.coincidence_back_off = coincidence_back_off + max(abs(offset) for offset in dataobj.time_slide_table.getColumnByName("offset"))
 		self.thinca_interval = thinca_interval
 		self.ids = set()
 
@@ -830,9 +830,9 @@ class StreamThinca(object):
 		def event_comparefunc(event_a, offset_a, event_b, offset_b, light_travel_time, delta_t):
 			return (event_a.mass1 != event_b.mass1) or (event_a.mass2 != event_b.mass2) or (abs(event_a.get_end() + offset_a - event_b.get_end() - offset_b) > light_travel_time + delta_t) 
 		def ntuple_comparefunc(events, offset_vector, seg = segments.segment(self.last_boundary, boundary)):
-			return ligolw_thinca.coinc_inspiral_end_time(events, offset_vector) not in seg
+			return set(event.ifo for event in events) != set(["H1", "H2", "L1"]) or ligolw_thinca.coinc_inspiral_end_time(events, offset_vector) not in seg
 		def get_effective_snr(self, fac):
-			return self.snr / self.chisq
+			return self.snr / (self.chisq / self.chisq_dof)**.5
 		orig_get_effective_snr, ligolw_thinca.SnglInspiral.get_effective_snr = ligolw_thinca.SnglInspiral.get_effective_snr, get_effective_snr
 		ligolw_thinca.ligolw_thinca(
 			self.dataobj.xmldoc,
