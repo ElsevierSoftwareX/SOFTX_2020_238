013b01c90fa1b78c4d0474ed249c4246ec46fde3
chad hanna <chad.hanna@ligo.org>
2011-07-14 13:25:23 -0500

    changed clustering back to SNR

--- examples/configurations/PBH/bank/cluster.sql
+++ examples/configurations/PBH/bank/cluster.sql
@@ -14,14 +14,15 @@ CREATE TEMPORARY TABLE _cluster_info_ AS
 		coinc_event.coinc_event_id AS coinc_event_id,
 		(coinc_event.time_slide_id || ";" || coinc_event.instruments) AS category,
 		(coinc_inspiral.end_time - (SELECT MIN(end_time) FROM coinc_inspiral)) + 1e-9 * coinc_inspiral.end_time_ns AS end_time,
-		CASE WHEN coinc_inspiral.combined_far != 0 THEN 1.0 / coinc_inspiral.combined_far ELSE coinc_inspiral.snr / (SELECT MIN(coinc_inspiral.combined_far) FROM coinc_inspiral WHERE coinc_inspiral.combined_far != 0) END AS ifar
+		coinc_inspiral.snr AS snr
+		--CASE WHEN coinc_inspiral.combined_far != 0 THEN 1.0 / coinc_inspiral.combined_far ELSE coinc_inspiral.snr / (SELECT MIN(coinc_inspiral.combined_far) FROM coinc_inspiral WHERE coinc_inspiral.combined_far != 0) END AS ifar
 	FROM
 		coinc_event
 		JOIN coinc_inspiral ON (
 			coinc_inspiral.coinc_event_id == coinc_event.coinc_event_id
 		);
 CREATE INDEX tmpindex1 ON _cluster_info_ (coinc_event_id);
-CREATE INDEX tmpindex2 ON _cluster_info_ (category, end_time, ifar);
+CREATE INDEX tmpindex2 ON _cluster_info_ (category, end_time, snr);
 
 --
 -- delete coincs that are within 10 s of coincs with higher SNR in the same
@@ -39,7 +40,7 @@ WHERE
 			JOIN _cluster_info_ AS _cluster_info_b_ ON (
 				_cluster_info_b_.category == _cluster_info_a_.category
 				AND (_cluster_info_b_.end_time BETWEEN _cluster_info_a_.end_time - 10.0 AND _cluster_info_a_.end_time + 10.0)
-				AND _cluster_info_b_.ifar > _cluster_info_a_.ifar
+				AND _cluster_info_b_.snr > _cluster_info_a_.snr
 			)
 		WHERE
 			_cluster_info_a_.coinc_event_id == coinc_event.coinc_event_id
