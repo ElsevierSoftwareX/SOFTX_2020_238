b5620543df79be6af34664b1fef339f339f81707
Adam Mercer <adam.mercer@ligo.org>
2011-08-08 15:42:16 -0500

    use autoreconf for building build system

--- 00init.sh
+++ 00init.sh
@@ -6,9 +6,6 @@ read -p "Press CTRL-C to abort, or RETURN to continue... " INPUT
 
 # Get it done
 {
-	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal -I gnuscripts ; } &&
-	#{ echo "running autoheader..." ; autoheader ; } &&
-	{ echo "running automake..." ; automake -a -c ; } &&
-	{ echo "running autoconf..." ; autoconf ; } &&
+	{ echo "running autoreconf" ; autoreconf ; } &&
 	echo "$0 complete." ;
 } || { echo "$0 failed." ; false ; }
