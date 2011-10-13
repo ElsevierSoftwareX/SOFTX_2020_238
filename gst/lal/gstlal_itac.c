9aefec457d9e4c1642866de0a66d1e82a3305042
chad hanna <chad.hanna@ligo.org>
2011-10-13 16:54:49 -0500

    new element to eventually find triggers

---
+++ gst/lal/gstlal_itac.c
@@ -0,0 +1,519 @@
+/*
+ * An inspiral trigger and autocorrelation chisq (itac) element
+ *
+ * Copyright (C) 2011  Chad Hanna, Kipp Cannon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+/*
+ * ========================================================================
+ *
+ *                                  Preamble
+ *
+ * ========================================================================
+ */
+
+
+/*
+ * struff from the C library
+ */
+
+
+/*
+ * stuff from glib/gstreamer
+ */
+
+
+#include <glib.h>
+#include <gst/gst.h>
+#include <gst/base/gstadapter.h>
+#include <math.h>
+#include <string.h>
+
+/*
+ * our own stuff
+ */
+
+#include <gstlal.h>
+#include <gstlal_itac.h>
+#include <gstlal_peakfinder.h>
+#include <gstaudioadapter.h>
+
+static guint64 output_num_samps(GSTLALItac *element)
+{
+	return (guint64) element->n;
+}
+
+
+static guint64 output_num_bytes(GSTLALItac *element)
+{
+	return (guint64) output_num_samps(element) * element->adapter->unit_size;
+}
+
+
+static int reset_time_and_offset(GSTLALItac *element)
+{
+	element->next_output_offset = 0;
+	element->next_output_timestamp = GST_CLOCK_TIME_NONE;
+	return 0;
+}
+
+static guint gst_audioadapter_available_samples(GstAudioAdapter *adapter)
+{
+	guint size;
+	g_object_get(adapter, "size", &size, NULL);
+	return size;
+}
+
+
+/*
+ * ============================================================================
+ *
+ *                                 Properties
+ *
+ * ============================================================================
+ */
+
+
+enum property {
+	ARG_N = 1
+};
+
+
+static void set_property(GObject *object, enum property id, const GValue *value, GParamSpec *pspec)
+{
+	GSTLALItac *element = GSTLAL_ITAC(object);
+
+	GST_OBJECT_LOCK(element);
+
+	switch(id) {
+	case ARG_N:
+		element->n = g_value_get_uint(value);
+		break;
+
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, id, pspec);
+		break;
+	}
+
+	GST_OBJECT_UNLOCK(element);
+}
+
+
+static void get_property(GObject *object, enum property id, GValue *value, GParamSpec *pspec)
+{
+	GSTLALItac *element = GSTLAL_ITAC(object);
+
+	GST_OBJECT_LOCK(element);
+
+	switch(id) {
+	case ARG_N:
+		g_value_set_uint(value, element->n);
+		break;
+
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, id, pspec);
+		break;
+	}
+
+	GST_OBJECT_UNLOCK(element);
+}
+
+
+/*
+ * ============================================================================
+ *
+ *                                  Sink Pad
+ *
+ * ============================================================================
+ */
+
+
+/*
+ * getcaps()
+ */
+
+
+static GstCaps *getcaps(GstPad * pad)
+{
+	GSTLALItac *element = GSTLAL_ITAC(gst_pad_get_parent(pad));
+	GstCaps *peercaps, *caps;
+
+	/*
+	 * get our own allowed caps.  use the fixed caps function to avoid
+	 * recursing back into this function.
+	 */
+
+	caps = gst_pad_get_fixed_caps_func(pad);
+
+	/*
+	 * get the allowed caps from the downstream peer if the peer has
+	 * caps, intersect without our own.
+	 */
+
+	peercaps = gst_pad_peer_get_caps(element->srcpad);
+	if(peercaps) {
+		GstCaps *result = gst_caps_intersect(peercaps, caps);
+		gst_caps_unref(peercaps);
+		gst_caps_unref(caps);
+		caps = result;
+	}
+
+	/*
+	 * done
+	 */
+
+	gst_object_unref(element);
+	return caps;
+}
+
+
+/*
+ * setcaps()
+ */
+
+
+static gboolean setcaps(GstPad *pad, GstCaps *caps)
+{
+	GSTLALItac *element = GSTLAL_ITAC(gst_pad_get_parent(pad));
+	GstStructure *structure;
+	gint rate, width, channels;
+	gboolean success = TRUE;
+
+	/*
+	 * parse caps
+	 */
+
+	structure = gst_caps_get_structure(caps, 0);
+	if(!gst_structure_get_int(structure, "rate", &rate))
+		success = FALSE;
+	if(!gst_structure_get_int(structure, "width", &width))
+		success = FALSE;
+	if(!gst_structure_get_int(structure, "channels", &channels))
+		success = FALSE;
+
+	/*
+	 * try setting caps on downstream element
+	 */
+
+	if(success)
+		success = gst_pad_set_caps(element->srcpad, caps);
+
+	/*
+	 * update the element metadata
+	 */
+
+	if(success) {
+		element->channels = channels;
+		element->rate = rate;
+		g_object_set(element->adapter, "unit-size", width / 8 * channels, NULL);
+		element->maxdata = gstlal_double_peak_samples_and_values_new(channels);
+	}
+
+	/*
+	 * done
+	 */
+
+	gst_object_unref(element);
+	return success;
+}
+
+
+/*
+ * chain()
+ */
+
+static void update_state(GSTLALItac *element, GstBuffer *srcbuf)
+{
+	element->next_output_offset = GST_BUFFER_OFFSET_END(srcbuf);
+	element->next_output_timestamp = GST_BUFFER_TIMESTAMP(srcbuf) + GST_BUFFER_DURATION(srcbuf);
+}
+
+static GstFlowReturn push_buffer(GSTLALItac *element, GstBuffer *srcbuf)
+{
+	GST_DEBUG_OBJECT(element, "pushing %" GST_BUFFER_BOUNDARIES_FORMAT, GST_BUFFER_BOUNDARIES_ARGS(srcbuf));
+	return gst_pad_push(element->srcpad, srcbuf);
+}
+
+static GstBuffer *make_gap_buffer(GSTLALItac *element, guint length)
+{
+	gint size = sizeof(double) * length * element->channels;
+        GstBuffer *srcbuf = NULL;
+        GstCaps *caps = GST_PAD_CAPS(element->srcpad);
+        GstFlowReturn result = gst_pad_alloc_buffer(element->srcpad, element->next_output_offset, size, caps, &srcbuf);
+
+        if (result != GST_FLOW_OK)
+                return srcbuf;
+
+        /* set the offset */
+        GST_BUFFER_OFFSET(srcbuf) = element->next_output_offset;
+        GST_BUFFER_OFFSET_END(srcbuf) = element->next_output_offset + length;
+
+        /* set the time stamps */
+        GST_BUFFER_TIMESTAMP(srcbuf) = element->next_output_timestamp;
+        GST_BUFFER_DURATION(srcbuf) = (GstClockTime) gst_util_uint64_scale_int_round(GST_SECOND, length, element->rate);
+        
+	/* memset to zero */
+        memset(GST_BUFFER_DATA(srcbuf), 0, GST_BUFFER_SIZE(srcbuf));
+
+	return srcbuf;
+}
+
+static GstFlowReturn chain(GstPad *pad, GstBuffer *sinkbuf)
+{
+	GSTLALItac *element = GSTLAL_ITAC(gst_pad_get_parent(pad));
+	GstFlowReturn result = GST_FLOW_OK;
+	GstBuffer *srcbuf = NULL;
+	guint64 maxsize = output_num_bytes(element);
+	gboolean copied_gap, copied_nongap;
+	guint outsamps, gapsamps;
+
+	/* if we haven't allocated storage do it now, we should never try to copy from an adapter with a larger buffer than this */
+	if (!element->data)
+		element->data = (double *) malloc(maxsize);
+
+	/*
+	 * check validity of timestamp and offsets
+	 */
+
+	if(!GST_BUFFER_TIMESTAMP_IS_VALID(sinkbuf) || !GST_BUFFER_DURATION_IS_VALID(sinkbuf) || !GST_BUFFER_OFFSET_IS_VALID(sinkbuf) || !GST_BUFFER_OFFSET_END_IS_VALID(sinkbuf)) {
+		gst_buffer_unref(sinkbuf);
+		GST_ERROR_OBJECT(element, "error in input stream: buffer has invalid timestamp and/or offset");
+		result = GST_FLOW_ERROR;
+		goto done;
+	}
+
+	/* FIXME if we were more careful we wouldn't lose so much data around disconts */
+	if (GST_BUFFER_FLAG_IS_SET(sinkbuf, GST_BUFFER_FLAG_DISCONT)) {
+		reset_time_and_offset(element);
+		gst_audioadapter_clear(element->adapter);
+	}
+
+	/* if we don't have a valid first timestamp yet take this one */
+	if (element->next_output_timestamp == GST_CLOCK_TIME_NONE) {
+		element->next_output_timestamp = GST_BUFFER_TIMESTAMP(sinkbuf);// + output_duration(element);
+	}
+
+	/* put the incoming buffer into an adapter, handles gaps */
+	gst_audioadapter_push(element->adapter, sinkbuf);
+
+	/* while we have enough data to do the calculation, do it and push out buffers n samples long */
+	while(gst_audioadapter_available_samples(element->adapter) >= element->n) {
+
+		/* See if the output is a gap or not */
+		gapsamps = gst_audioadapter_head_gap_length(element->adapter);
+		if (gapsamps > 0) {
+			outsamps = gst_audioadapter_head_gap_length(element->adapter) > element->n ? element->n : gst_audioadapter_head_gap_length(element->adapter);
+			/* Clearing the max data structure causes the next buffer to be a gap */
+			gstlal_double_peak_samples_and_values_clear(element->maxdata);
+			srcbuf = gstlal_double_new_buffer_from_peak(element->maxdata, element->srcpad, element->next_output_offset, outsamps, element->next_output_timestamp, element->rate);
+			//srcbuf = make_gap_buffer(element, outsamps);
+		}
+		else {
+			outsamps = gst_audioadapter_head_nongap_length(element->adapter) > element->n ? element->n : gst_audioadapter_head_nongap_length(element->adapter);
+
+			/* call the peak finding library on a buffer from the adapter */
+			gst_audioadapter_copy(element->adapter, (void *) element->data, outsamps, &copied_gap, &copied_nongap);
+			gstlal_double_peak_over_window(element->maxdata, (const double *) element->data, outsamps);
+			srcbuf = gstlal_double_new_buffer_from_peak(element->maxdata, element->srcpad, element->next_output_offset, outsamps, element->next_output_timestamp, element->rate);
+		}	
+
+		/* set the time stamp and offset state */
+		update_state(element, srcbuf);
+
+		/* push the result */
+		result = push_buffer(element, srcbuf);
+
+		/* knock off the first buffers worth of bytes since we don't need them any more */
+		gst_audioadapter_flush(element->adapter, outsamps);
+	}
+
+done:
+	gst_object_unref(element);
+	return result;
+}
+
+
+/*
+ * ============================================================================
+ *
+ *                                Type Support
+ *
+ * ============================================================================
+ */
+
+
+/*
+ * Parent class.
+ */
+
+
+static GstElementClass *parent_class = NULL;
+
+
+/*
+ * Instance finalize function.  See ???
+ */
+
+
+static void finalize(GObject *object)
+{
+	GSTLALItac *element = GSTLAL_ITAC(object);
+
+	gst_object_unref(element->sinkpad);
+	element->sinkpad = NULL;
+	gst_object_unref(element->srcpad);
+	element->srcpad = NULL;
+	g_object_unref(element->adapter);
+	G_OBJECT_CLASS(parent_class)->finalize(object);
+}
+
+
+/*
+ * Base init function.  See
+ *
+ * http://developer.gnome.org/doc/API/2.0/gobject/gobject-Type-Information.html#GBaseInitFunc
+ */
+
+
+#define CAPS \
+	"audio/x-raw-float, " \
+	"rate = (int) [1, MAX], " \
+	"channels = (int) [1, MAX], " \
+	"endianness = (int) BYTE_ORDER, " \
+	"width = (int) {64}; "
+
+
+static void base_init(gpointer class)
+{
+	GstElementClass *element_class = GST_ELEMENT_CLASS(class);
+
+	gst_element_class_set_details_simple(
+		element_class,
+		"Itac",
+		"Filter",
+		"Find itacs in a time series every n samples",
+		"Chad Hanna <chad.hanna@ligo.org>"
+	);
+
+	gst_element_class_add_pad_template(
+		element_class,
+		gst_pad_template_new(
+			"sink",
+			GST_PAD_SINK,
+			GST_PAD_ALWAYS,
+			gst_caps_from_string(CAPS)
+		)
+	);
+	gst_element_class_add_pad_template(
+		element_class,
+		gst_pad_template_new(
+			"src",
+			GST_PAD_SRC,
+			GST_PAD_ALWAYS,
+			gst_caps_from_string(CAPS)
+		)
+	);
+}
+
+
+/*
+ * Class init function.  See
+ *
+ * http://developer.gnome.org/doc/API/2.0/gobject/gobject-Type-Information.html#GClassInitFunc
+ */
+
+
+static void class_init(gpointer class, gpointer class_data)
+{
+	GObjectClass *gobject_class = G_OBJECT_CLASS(class);
+
+	parent_class = g_type_class_ref(GST_TYPE_ELEMENT);
+
+	gobject_class->set_property = GST_DEBUG_FUNCPTR(set_property);
+	gobject_class->get_property = GST_DEBUG_FUNCPTR(get_property);
+	gobject_class->finalize = GST_DEBUG_FUNCPTR(finalize);
+
+	g_object_class_install_property(
+		gobject_class,
+		ARG_N,
+		g_param_spec_uint(
+			"n",
+			"n",
+			"number of samples over which to identify itacs",
+			0, G_MAXUINT, 0,
+			G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS | G_PARAM_CONSTRUCT
+		)
+	);
+}
+
+
+/*
+ * Instance init function.  See
+ *
+ * http://developer.gnome.org/doc/API/2.0/gobject/gobject-Type-Information.html#GInstanceInitFunc
+ */
+
+
+static void instance_init(GTypeInstance *object, gpointer class)
+{
+	GSTLALItac *element = GSTLAL_ITAC(object);
+	GstPad *pad;
+
+	gst_element_create_all_pads(GST_ELEMENT(element));
+
+	/* configure (and ref) sink pad */
+	pad = gst_element_get_static_pad(GST_ELEMENT(element), "sink");
+	gst_pad_set_getcaps_function(pad, GST_DEBUG_FUNCPTR(getcaps));
+	gst_pad_set_setcaps_function(pad, GST_DEBUG_FUNCPTR(setcaps));
+	gst_pad_set_chain_function(pad, GST_DEBUG_FUNCPTR(chain));
+	element->sinkpad = pad;
+
+	/* retrieve (and ref) src pad */
+	pad = gst_element_get_static_pad(GST_ELEMENT(element), "src");
+	element->srcpad = pad;
+
+	/* internal data */
+	element->rate = 0;
+	reset_time_and_offset(element);
+
+	element->adapter = g_object_new(GST_TYPE_AUDIOADAPTER, NULL);
+}
+
+
+/*
+ * gstlal_itac_get_type().
+ */
+
+
+GType gstlal_itac_get_type(void)
+{
+	static GType type = 0;
+
+	if(!type) {
+		static const GTypeInfo info = {
+			.class_size = sizeof(GSTLALItacClass),
+			.class_init = class_init,
+			.base_init = base_init,
+			.instance_size = sizeof(GSTLALItac),
+			.instance_init = instance_init,
+		};
+		type = g_type_register_static(GST_TYPE_ELEMENT, "lal_itac", &info, 0);
+	}
+
+	return type;
+}
