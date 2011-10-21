d944b8ef35cb03e8aa57dd9c9a50c36f34aae597
chad hanna <chad.hanna@ligo.org>
2011-10-21 10:31:27 -0500

    add complex versions

--- lib/gstlal_peakfinder.h
+++ lib/gstlal_peakfinder.h
@@ -32,12 +32,15 @@ int gstlal_double_complex_peak_samples_and_values_clear(struct gstlal_double_com
 
 /* find a peak over a fixed window */
 int gstlal_double_peak_over_window(struct gstlal_double_peak_samples_and_values *output, const double *data, guint64 length);
-int gstlal_double_fill_output_with_peak(struct gstlal_double_peak_samples_and_values *input, double *data, guint64 lenght);
-GstBuffer *gstlal_double_new_buffer_from_peak(struct gstlal_double_peak_samples_and_values *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
-
-/* clear an output buffer and populate with just the peaks */
 int gstlal_double_complex_peak_over_window(struct gstlal_double_complex_peak_samples_and_values *output, const double *data, guint64 length);
+
+/* fill the output */
+int gstlal_double_fill_output_with_peak(struct gstlal_double_peak_samples_and_values *input, double *data, guint64 length);
 int gstlal_double_complex_fill_output_with_peak(struct gstlal_double_complex_peak_samples_and_values *input, double complex *data, guint64 length);
+
+/* Convenience functions */
+GstBuffer *gstlal_double_new_buffer_from_peak(struct gstlal_double_peak_samples_and_values *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
+GstBuffer *gstlal_double_complex_new_buffer_from_peak(struct gstlal_double_complex_peak_samples_and_values *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
 
 G_END_DECLS
 #endif	/* __GSTLAL_PEAKFINDER_H__ */
