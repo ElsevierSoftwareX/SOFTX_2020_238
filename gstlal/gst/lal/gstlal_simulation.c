25324dce27154d7a096bddf8519d72aaa0718c64
chad hanna <chad.hanna@ligo.org>
2011-11-14 10:08:27 -0600

    fixed 10 digit gps bug

--- gstlal/gst/lal/gstlal_simulation.c
+++ gstlal/gst/lal/gstlal_simulation.c
@@ -800,8 +800,8 @@ static GstFlowReturn chain(GstPad *pad, GstBuffer *buf)
 	 */
 
 	if(!element->injection_document) {
-		LIGOTimeGPS start = {-999999999, 0};
-		LIGOTimeGPS end = {+999999999, 0};
+		LIGOTimeGPS start = {-2000000000, 0};
+		LIGOTimeGPS end = {+2000000000, 0};
 		/* earliest and latest possible LIGOTimeGPS */
 		/* FIXME:  hard-coded = BAD BAD BAD */
 		/*XLALINT8NSToGPS(&start, (INT8) 1 << 63);
