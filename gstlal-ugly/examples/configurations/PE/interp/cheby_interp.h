a43837ba29c525c3f7bd606c2128c48cb9d62d62
Chad Hanna <chad.hanna@ligo.org>
2012-04-11 13:52:03 -0400

    various changes

--- gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
+++ gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
@@ -1,15 +1,19 @@
-struct waveform_interpolant {
+struct 2d_waveform_interpolant {
 	gsl_vector *svd_basis;
 	/* See http://arxiv.org/pdf/1108.5618v1.pdf  This represents the C
- *  	 * matrix of formula (8) without mu.  Note that you specify a separate waveform
- *  	 	 * interpolant object for each mu 
- *  	 	 	 */
-	gsl_matrix *Ckl;
-	
+ 	 * matrix of formula (8) without mu.  Note that you specify a separate waveform
+	 * interpolant object for each mu 
+	 */
+	gsl_matrix *C_KL;
+
+	double p1_min;
+	double p1_max;
+	double p2_min;
+	double p2_max;
 		
 	}
 	
-struct waveform_interpolants {
+struct 2d_waveform_interpolant_array {
 	struct waveform_interpolant *interp;
 	int size;
 	
