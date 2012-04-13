635dd85942a5b78549cece2ec9844d2e2bc40a87
Chad Hanna <chad.hanna@ligo.org>
2012-04-13 14:49:46 -0400

    cheby_interp: now compiles

--- gstlal-ugly/examples/configurations/PE/interp/cheby_interp.c
+++ gstlal-ugly/examples/configurations/PE/interp/cheby_interp.c
@@ -4,9 +4,9 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
-#include <complex.h>
 #include <gsl/gsl_complex.h>
-#include <spa.c>
+#include <gsl/gsl_complex_math.h>
+/*#include <spa.c>*/
 /*
  * Data structure methods
  */
@@ -31,9 +31,9 @@ struct twod_waveform_interpolant_array * new_waveform_interpolant_array(int size
  * Formula (3) in http://arxiv.org/pdf/1108.5618v1.pdf
  */ 
 
-gsl_complex projection_coefficient(gsl_vector_complex *svd_basis, gsl_vector_complex *spa_waveform){
+static gsl_complex projection_coefficient(gsl_vector_complex *svd_basis, gsl_vector_complex *spa_waveform){
 	/*project svd basis onto SPA's to get coefficients*/
-	gsl_complex M; //Should equal double complex and can be safely cast
+	gsl_complex M;
 	/* Note that svd_basis should have 0 imaginary part */
 	gsl_blas_zdotu(svd_basis, spa_waveform, &M);
 	return M;
@@ -44,7 +44,7 @@ gsl_complex projection_coefficient(gsl_vector_complex *svd_basis, gsl_vector_com
  * Formula (4) in http://arxiv.org/pdf/1108.5618v1.pdf
  */
 
-double chebyshev_node(int j, int J_max) {
+static double chebyshev_node(int j, int J_max) {
 	return cos(M_PI * (2.*j + 1.) / J_max / 2.);
 }
 
@@ -52,7 +52,7 @@ double chebyshev_node(int j, int J_max) {
  * Formula (5) in http://arxiv.org/pdf/1108.5618v1.pdf
  */
 
-double onedCheby(double x, int J, int J_max) {
+static double onedCheby(double x, int J, int J_max) {
 	double w;
 
 	if (J == 0)
@@ -63,7 +63,7 @@ double onedCheby(double x, int J, int J_max) {
 	return (pow(x - sqrt(x*x - 1.0), J) + pow(x + sqrt(x*x - 1.0), J)) / w;
 }
 
-double twodCheby(double x, int K, int K_max, double y, int L, int L_max) {
+static double twodCheby(double x, int K, int K_max, double y, int L, int L_max) {
 	return onedCheby(x, K, K_max) * onedCheby(y, L, L_max);
 }
 
@@ -74,26 +74,25 @@ double twodCheby(double x, int K, int K_max, double y, int L, int L_max) {
 
 
 /* you do this for every mu */
-gsl_matrix_complex * compute_C_KL(gsl_vector *x_k, gsl_vector *y_l, gsl_matrix_complex *M) {
+static gsl_matrix_complex * compute_C_KL(gsl_vector *x_k, gsl_vector *y_l, gsl_matrix_complex *M) {
 	int K, L, k, l;
 	gsl_complex out;
 
-	double	k_max = x_k->size;
-	double  l_max = y_l->size;
+	int k_max = x_k->size;
+	int l_max = y_l->size;
 	gsl_matrix_complex *C_KL = gsl_matrix_complex_calloc(k_max, l_max);
-	gsl_complex tmp; 
-
+	gsl_complex tmp;
 
 	for (K = 0; K < k_max; K++) {
 		for (L = 0; L < l_max; L++) {
 			GSL_SET_COMPLEX(&out, 0, 0);
 			for (k = 0; k < k_max; k++) {
 				for (l = 0; l < l_max; l++) {
-					tmp =  gsl_complex_mul_real(gsl_matrix_complex_get(M, k, l),twodCheby(gsl_vector_get(x_k, k), K, k_max, gsl_vector_get(y_l, l), L, l_max) );
-					out= gsl_complex_add(out, tmp);
+					tmp = gsl_complex_mul_real(gsl_matrix_complex_get(M, k, l), twodCheby(gsl_vector_get(x_k, k), K, k_max, gsl_vector_get(y_l, l), L, l_max));
+					out = gsl_complex_add(out, tmp);
 				}
 			}
-		gsl_matrix_complex_set(C_KL, K, L, (gsl_complex) out);
+		gsl_matrix_complex_set(C_KL, K, L, out);
 		}
 	}
 
@@ -105,19 +104,20 @@ gsl_matrix_complex * compute_C_KL(gsl_vector *x_k, gsl_vector *y_l, gsl_matrix_c
  * Formula (8) in http://arxiv.org/pdf/1108.5618v1.pdf
  */
 
-gsl_complex compute_M_xy(gsl_matrix_complex *C_KL, double x, double y) {
+static gsl_complex compute_M_xy(gsl_matrix_complex *C_KL, double x, double y) {
 	int K_max, L_max;
 	int K, L;
-	K_max = C_KL->size1;
-	L_max = C_KL->size2;
 	gsl_complex M; 
 	gsl_complex tmp;
+	
+	K_max = C_KL->size1;
+	L_max = C_KL->size2;
 
 	GSL_SET_COMPLEX(&M, 0, 0);
 
 	for (K = 0; K < K_max; K++) {
 		for (L = 0; L < L_max; L++) {
-			//FIXME, is this correct??
+			/* FIXME, is this correct?? */
 			tmp =gsl_complex_mul_real(gsl_matrix_complex_get(C_KL, K, L),twodCheby(x, K, K_max, y, L, L_max));
 			M = gsl_complex_add(M, tmp);
 			
@@ -132,7 +132,7 @@ gsl_complex compute_M_xy(gsl_matrix_complex *C_KL, double x, double y) {
  * generic cheby utilities
  */
 
-double map_coordinate_to_cheby(double c_min, double c_max, double c) {
+static double map_coordinate_to_cheby(double c_min, double c_max, double c) {
 	return 2. * ( c - c_min) / (c_max - c_min) - 1.;
 }
 
@@ -141,20 +141,20 @@ double map_coordinate_to_cheby(double c_min, double c_max, double c) {
  */
 
 /* FIXME use a better name */
-gsl_vector *interpolate_waveform_from_mchirp_and_eta(struct twod_waveform_interpolant_array *interps, double mchirp, double eta) { 
+static gsl_vector_complex *interpolate_waveform_from_mchirp_and_eta(struct twod_waveform_interpolant_array *interps, double mchirp, double eta) { 
 	int i;
 	gsl_complex M;
 	double deltaF, x, y;
-	gsl_vector_complex *h_f = gsl_vector_complex_calloc(interps[0]->svd_basis->size);
 	struct twod_waveform_interpolant *interp = interps->interp;
+	gsl_vector_complex *h_f = gsl_vector_complex_calloc(interp->svd_basis->size);
 	
 	for (i = 0; i < interps->size; i++, interp++) {
 		x = map_coordinate_to_cheby(interp->p1_min, interp->p1_max, mchirp);
 		y = map_coordinate_to_cheby(interp->p2_min, interp->p2_max, eta);
 		M = compute_M_xy(interp->C_KL, x, y);
-		// don't do this, we need an add and scale function so as to not destroy svd_basis
-		//gsl_vector_scale(interp->svd_basis, M);
-		gsl_blas_zaxpy(&M, &h_f, interp->svd_basis); 
+		/* don't do this, we need an add and scale function so as to not destroy svd_basis */
+		/* gsl_vector_scale(interp->svd_basis, M); */
+		gsl_blas_zaxpy(M, h_f, interp->svd_basis); 
 		}
 	free_waveform_interp_objects(interps);
 
--- gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
+++ gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
@@ -3,26 +3,30 @@
 
 struct twod_waveform_interpolant {
 
-	gsl_vector_complex *svd_basis; //complex part needs to be set to zero
+	gsl_vector_complex *svd_basis; /* Imaginary part must be zero */
+
 	/* See http://arxiv.org/pdf/1108.5618v1.pdf  This represents the C
  	 * matrix of formula (8) without mu.  Note that you specify a separate waveform
 	 * interpolant object for each mu 
 	 */
-	gsl_matrix *C_KL;
+
+	gsl_matrix_complex *C_KL;
 
 	double p1_min;
 	double p1_max;
 	double p2_min;
 	double p2_max;
 		
-	}
+};
 	
 struct twod_waveform_interpolant_array {
 	struct twod_waveform_interpolant *interp;
 	int size;
 	
-}
+};
 
+int free_waveform_interp_objects(struct twod_waveform_interpolant_array *);
 
+struct twod_waveform_interpolant_array * new_twod_waveform_interpolant_array(int size);
 
 
