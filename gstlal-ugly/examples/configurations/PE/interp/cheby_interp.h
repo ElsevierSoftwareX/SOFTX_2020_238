b8a5be74e66f706f5806eb42e93d9c35ca1a0b3a
Rory Smith <rory.smith@ligo.org>
2012-04-13 11:04:46 -0700

    cheby_interp.h : making it compilable

--- gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
+++ gstlal-ugly/examples/configurations/PE/interp/cheby_interp.h
@@ -1,5 +1,9 @@
-struct 2d_waveform_interpolant {
-	gsl_vector_complex *svd_basis;
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+
+struct twod_waveform_interpolant {
+
+	gsl_vector_complex *svd_basis; //complex part needs to be set to zero
 	/* See http://arxiv.org/pdf/1108.5618v1.pdf  This represents the C
  	 * matrix of formula (8) without mu.  Note that you specify a separate waveform
 	 * interpolant object for each mu 
@@ -13,8 +17,8 @@ struct 2d_waveform_interpolant {
 		
 	}
 	
-struct 2d_waveform_interpolant_array {
-	struct waveform_interpolant *interp;
+struct twod_waveform_interpolant_array {
+	struct twod_waveform_interpolant *interp;
 	int size;
 	
 }
