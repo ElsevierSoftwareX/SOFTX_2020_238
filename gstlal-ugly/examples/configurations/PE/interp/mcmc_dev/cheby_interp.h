492737f8676986bb9c70fe108ea2f0dfaed01439
Rory Smith <rory.smith@ligo.org>
2012-05-02 22:44:18 -0400

    mcmc_dev/: directory for chebyshev interpolant for mcmc devel

---
+++ gstlal-ugly/examples/configurations/PE/interp/mcmc_dev/cheby_interp.h
@@ -0,0 +1,46 @@
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+
+struct twod_waveform_interpolant {
+
+	gsl_vector_view svd_basis; /* Imaginary part must be zero */
+
+	/* See http://arxiv.org/pdf/1108.5618v1.pdf  This represents the C
+ 	 * matrix of formula (8) without mu.  Note that you specify a separate waveform
+	 * interpolant object for each mu 
+	 */
+
+	gsl_matrix_complex *C_KL;
+
+		
+};
+	
+struct twod_waveform_interpolant_array {
+	struct twod_waveform_interpolant *interp;
+	int size;
+	double param1_min;
+	double param1_max;
+	double param2_min;
+	double param2_max;
+	
+};
+
+struct twod_waveform_interpolant_patches {
+	REAL8FrequencySeries *psd;
+	int number_of_patches;
+	struct twod_waveform_interpolant_array *interp_arrays;
+	double inner_param1_min;
+	double inner_param1_max;
+	double inner_param2_min;
+	double inner_param2_max;
+	double outer_param1_min;
+	double outer_param1_max;
+	double outer_param2_min;
+	double outer_param2_max;
+}
+
+int free_waveform_interp_objects(struct twod_waveform_interpolant_array *);
+
+struct twod_waveform_interpolant_array* new_waveform_interpolant_array_from_svd_bank(gsl_matrix *svd_bank, double param1_min, double param2_min, double param1_max, double param2_max);i
+
+struct twod_waveform_interpolant_patches* interpolants_over_patches(REAL8FrequencySeries *psd, int N_patches);
