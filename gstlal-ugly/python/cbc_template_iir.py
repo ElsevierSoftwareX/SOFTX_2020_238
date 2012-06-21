2adab56de49cf38e931b5d5cefeebfe043a13eda
Shaun Hooper <hoopes01@student.uwa.edu.au>
2012-06-20 20:26:47 -0500

    added GPL license

--- gstlal-ugly/python/cbc_template_iir.py
+++ gstlal-ugly/python/cbc_template_iir.py
@@ -3,8 +3,23 @@
 # This code should read in a xml file and produce three matrices, a1, b0, delay
 # that correspond to a bank of waveforms
 #
-# Copyright Shaun Hooper 2010-05-13
 #######
+#
+# Copyright (C) 2010-2012 Shaun Hooper
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from pylal import spawaveform
 import sys
