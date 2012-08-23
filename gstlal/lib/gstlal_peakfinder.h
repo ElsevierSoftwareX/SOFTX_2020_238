07f4f705a95312a04fe288049964fff04f33acf4
chad hanna <chad.hanna@ligo.org>
2012-08-23 14:51:20 -0500

    gstlal_peakfinder.h: rewrite of peak finder element to support more types

--- gstlal/lib/gstlal_peakfinder.h
+++ gstlal/lib/gstlal_peakfinder.h
@@ -8,46 +8,90 @@
 
 G_BEGIN_DECLS
 
-struct gstlal_double_peak_samples_and_values {
+typedef enum tag_gstlal_peak_type_specifier {
+	GSTLAL_PEAK_FLOAT,
+	GSTLAL_PEAK_DOUBLE,
+	GSTLAL_PEAK_COMPLEX,
+	GSTLAL_PEAK_DOUBLE_COMPLEX,
+	GSTLAL_PEAK_TYPE_COUNT
+} gstlal_peak_type_specifier;
+
+struct gstlal_peak_state {
 	guint channels;
 	guint num_events;
 	guint *samples;
-	double *values;
+	/* should sync with gstlal_peak_type */
+	union	{
+		float * as_float;
+		double * as_double;
+		float complex * as_float_complex;
+		double complex * as_double_complex;
+		} values;
+	gstlal_peak_type_specifier type;
+	guint unit;
 	guint pad;
 	double thresh;
 };
 
-struct gstlal_double_complex_peak_samples_and_values {
-	guint channels;
-	guint num_events;
-	guint *samples;
-	double complex *values;
-	guint pad;
-	double thresh;
-};
+
+/*
+ * Type agnostic declarations
+ */
+
 
 /* make new structures */
-struct gstlal_double_peak_samples_and_values *gstlal_double_peak_samples_and_values_new(guint channels);
-struct gstlal_double_complex_peak_samples_and_values *gstlal_double_complex_peak_samples_and_values_new(guint channels);
+struct gstlal_peak_state *gstlal_peak_state_new(guint channels, gstlal_peak_type_specifier type);
+
+/* free structures */
+int gstlal_peak_state_free(struct gstlal_peak_state *state);
 
 /* clear contents of structures */
-int gstlal_double_peak_samples_and_values_clear(struct gstlal_double_peak_samples_and_values *);
-int gstlal_double_complex_peak_samples_and_values_clear(struct gstlal_double_complex_peak_samples_and_values *);
-
-/* find a peak over a fixed window */
-int gstlal_double_peak_over_window(struct gstlal_double_peak_samples_and_values *output, const double *data, guint64 length);
-int gstlal_double_complex_peak_over_window(struct gstlal_double_complex_peak_samples_and_values *output, const double complex *data, guint64 length);
-
-/* Pull out peak snippets */
-int gstlal_double_complex_series_around_peak(struct gstlal_double_complex_peak_samples_and_values *input, double complex *data, double complex *outputmat, guint n);
-
-/* fill the output */
-int gstlal_double_fill_output_with_peak(struct gstlal_double_peak_samples_and_values *input, double *data, guint64 length);
-int gstlal_double_complex_fill_output_with_peak(struct gstlal_double_complex_peak_samples_and_values *input, double complex *data, guint64 length);
+int gstlal_peak_state_clear(struct gstlal_peak_state *state);
 
 /* Convenience functions */
-GstBuffer *gstlal_double_new_buffer_from_peak(struct gstlal_double_peak_samples_and_values *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
-GstBuffer *gstlal_double_complex_new_buffer_from_peak(struct gstlal_double_complex_peak_samples_and_values *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
+GstBuffer *gstlal_new_buffer_from_peak(struct gstlal_peak_state *input, GstPad *pad, guint64 offset, guint64 length, GstClockTime time, guint rate);
+
+int gstlal_peak_over_window(struct gstlal_peak_state *state, const void *data, guint64 length);
+
+int gstlal_series_around_peak(struct gstlal_peak_state *state, void *data, void *outputmat, guint n);
+
+
+/*
+ * Type specific declarations
+ */
+
+
+/* float */
+
+#define TYPE_STRING float
+#define TYPE float
+#include <gstlal/gstlal_peakfinder.ht>
+#undef TYPE
+#undef TYPE_STRING
+
+/* double */
+
+#define TYPE_STRING double
+#define TYPE double
+#include <gstlal/gstlal_peakfinder.ht>
+#undef TYPE
+#undef TYPE_STRING
+
+/* complex */
+
+#define TYPE_STRING float_complex
+#define TYPE float complex
+#include <gstlal/gstlal_peakfinder.ht>
+#undef TYPE
+#undef TYPE_STRING
+
+/* double complex */
+
+#define TYPE_STRING double_complex
+#define TYPE double complex
+#include <gstlal/gstlal_peakfinder.ht>
+#undef TYPE
+#undef TYPE_STRING
 
 G_END_DECLS
 #endif	/* __GSTLAL_PEAKFINDER_H__ */
