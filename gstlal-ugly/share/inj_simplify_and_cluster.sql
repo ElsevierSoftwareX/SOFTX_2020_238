a4c19deec3a247da0f17ea80141e684119dbc808
chad hanna <chad.hanna@ligo.org>
2012-09-26 13:56:59 -0500

    sql: 4s instead of 10s clustering

--- gstlal-ugly/share/inj_simplify_and_cluster.sql
+++ gstlal-ugly/share/inj_simplify_and_cluster.sql
@@ -60,7 +60,7 @@ CREATE TEMPORARY TABLE _cluster_info_ AS
 		coinc_event.coinc_event_id AS coinc_event_id,
 		(coinc_event.time_slide_id || ";" || coinc_event.instruments) AS category,
 		(coinc_inspiral.end_time - (SELECT MIN(end_time) FROM coinc_inspiral)) + 1e-9 * coinc_inspiral.end_time_ns AS end_time,
-		coinc_event.likelihood AS snr
+		coinc_inspiral.snr AS snr
 		--CASE WHEN coinc_inspiral.combined_far != 0 THEN 1.0 / coinc_inspiral.combined_far ELSE coinc_inspiral.snr / (SELECT MIN(coinc_inspiral.combined_far) FROM coinc_inspiral WHERE coinc_inspiral.combined_far != 0) END AS ifar
 	FROM
 		coinc_event
@@ -71,7 +71,7 @@ CREATE INDEX tmpindex1 ON _cluster_info_ (coinc_event_id);
 CREATE INDEX tmpindex2 ON _cluster_info_ (category, end_time, snr);
 
 --
--- delete coincs that are within 10 s of coincs with higher SNR in the same
+-- delete coincs that are within 4 s of coincs with higher SNR in the same
 -- category
 --
 
@@ -85,7 +85,7 @@ WHERE
 			_cluster_info_ AS _cluster_info_a_
 			JOIN _cluster_info_ AS _cluster_info_b_ ON (
 				_cluster_info_b_.category == _cluster_info_a_.category
-				AND (_cluster_info_b_.end_time BETWEEN _cluster_info_a_.end_time - 10.0 AND _cluster_info_a_.end_time + 10.0)
+				AND (_cluster_info_b_.end_time BETWEEN _cluster_info_a_.end_time - 4.0 AND _cluster_info_a_.end_time + 4.0)
 				AND _cluster_info_b_.snr > _cluster_info_a_.snr
 			)
 		WHERE
