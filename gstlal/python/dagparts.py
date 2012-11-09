c0b1989e3949fa6a053aa9dd210636056a2d32ea
chad hanna <chad.hanna@ligo.org>
2012-11-09 07:57:48 -0600

    dagparts.py: add common functions, classes from inspiral_pipe.py

--- gstlal/python/dagparts.py
+++ gstlal/python/dagparts.py
@@ -30,26 +30,137 @@ DAG construction tools.
 """
 
 
-import math
 import os
 import sys
-import time
 
-
-from glue import iterutils
 from glue import segments
-from glue import segmentsUtils
 from glue import pipeline
-from glue.lal import CacheEntry
-from pylal.datatypes import LIGOTimeGPS
-from pylal import ligolw_tisi
-from pylal import llwapp
-from lalapps import power
 
 
 __author__ = "Kipp Cannon <kipp.cannon@ligo.org>, Chad Hanna <chad.hanna@ligo.org>"
 __date__ = "$Date$" #FIXME
 __version__ = "$Revision$" #FIXME
+
+
+#
+# =============================================================================
+#
+# Environment utilities
+#
+# =============================================================================
+#
+
+
+def which(prog):
+	which = subprocess.Popen(['which',prog], stdout=subprocess.PIPE)
+	out = which.stdout.read().strip()
+	if not out: 
+		print >>sys.stderr, "ERROR: could not find %s in your path, have you built the proper software and sourced the proper environment scripts?" % (prog,prog)
+		raise ValueError 
+	return out
+
+
+def log_path():
+	host = socket.getfqdn()
+	try:
+		return os.environ['TMPDIR']
+	except KeyError:
+		print "\n\n!!!! $TMPDIR NOT SET !!!!\n\n\tPLEASE email your admin to tell them to set $TMPDIR to be the place where a users temporary files should be\n"
+		#FIXME add more hosts as you need them
+		if 'cit' in host or 'caltech.edu' in host:
+			tmp = '/usr1/' + os.environ['USER']
+			print "falling back to ", tmp
+			return tmp
+		if 'phys.uwm.edu' in host:
+			tmp = '/localscratch/' + os.environ['USER']
+			print "falling back to ", tmp
+			return tmp
+		if 'aei.uni-hannover.de' in host:
+			tmp = '/local/user/' + os.environ['USER']
+			print "falling back to ", tmp
+			return tmp
+		if 'phy.syr.edu' in host:
+			tmp = '/usr1/' + os.environ['USER']
+			print "falling back to ", tmp
+			return tmp
+
+		raise KeyError("$TMPDIR is not set and I don't recognize this environment")
+
+
+#
+# =============================================================================
+#
+# Condor DAG utilities
+#
+# =============================================================================
+#
+
+
+class CondorDAG(pipeline.CondorDAG):
+
+	def __init__(self, name, logpath = log_path()):
+		self.basename = name
+		tempfile.tempdir = logpath
+		tempfile.template = self.basename + '.dag.log.'
+		logfile = tempfile.mktemp()
+		fh = open( logfile, "w" )
+		fh.close()
+		pipeline.CondorDAG.__init__(self,logfile)
+		self.set_dag_file(self.basename)
+		self.jobsDict = {}
+		self.node_id = 0
+		self.output_cache = []
+
+	def add_node(self, node):
+		node.set_retry(3)
+		self.node_id += 1
+		node.add_macro("macroid", self.node_id)
+		node.add_macro("macronodename", node.get_name())
+		pipeline.CondorDAG.add_node(self, node)
+
+	def write_cache(self):
+		out = self.basename + ".cache"
+		f = open(out,"w")
+		for c in self.output_cache:
+			f.write(str(c)+"\n")
+		f.close()
+
+
+class CondorDAGJob(pipeline.CondorDAGJob):
+	"""
+	A generic job class for gstlal stuff
+	"""
+	def __init__(self, executable, tag_base):
+		self.__prog__ = tag_base
+		self.__executable = executable
+		self.__universe = 'vanilla'
+		pipeline.CondorDAGJob.__init__(self, self.__universe, self.__executable)
+		self.add_condor_cmd('getenv','True')
+		self.tag_base = tag_base
+		self.set_sub_file(tag_base+'.sub')
+		self.set_stdout_file('logs/'+tag_base+'-$(macroid)-$(macronodename)-$(cluster)-$(process).out')
+		self.set_stderr_file('logs/'+tag_base+'-$(macroid)-$(macronodename)-$(cluster)-$(process).err')
+		self.number = 1
+
+
+class CondorDAGNode(pipeline.CondorDAGNode):
+	"""
+	A generic node class for gstlal stuff
+	"""
+	def __init__(self, job, dag, p_node=[]):
+		pipeline.CondorDAGNode.__init__(self, job)
+		for p in p_node:
+			self.add_parent(p)
+		dag.add_node(self)
+
+
+#
+# =============================================================================
+#
+# Segment utilities
+#
+# =============================================================================
+#
 
 
 def breakupseg(seg, maxextent, overlap):
