929e736f478efd9b3136c6b44b07a3b468a62c2e
Kipp Cannon <kipp.cannon@ligo.org>
2012-11-19 16:55:15 -0500

    add GstLALGPSClock type
    a GstClock subclassed from GstSystemClock that returns the GPS time
    according to the system clock

---
+++ gstlal/lib/gstlal_gpsclock.c
@@ -0,0 +1,94 @@
+/*
+ * GstLALGPSClock
+ *
+ * Copyright (C) 2012  Kipp Cannon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+/*
+ * ============================================================================
+ *
+ *                                  Preamble
+ *
+ * ============================================================================
+ */
+
+
+#include <glib.h>
+#include <gst/gst.h>
+
+
+#include <lal/Date.h>
+
+
+#include <gstlal_gpsclock.h>
+
+
+/*
+ * ============================================================================
+ *
+ *                                Boilerplate
+ *
+ * ============================================================================
+ */
+
+
+GST_BOILERPLATE(GstLALGPSClock, gstlal_gpsclock, GstSystemClock, GST_TYPE_SYSTEM_CLOCK);
+
+
+/*
+ * ============================================================================
+ *
+ *                              GstClock Methods
+ *
+ * ============================================================================
+ */
+
+
+static GstClockTime get_internal_time(GstClock *clock)
+{
+	LIGOTimeGPS gps;
+
+	return XLALGPSToINT8NS(XLALGPSTimeNow(&gps));
+}
+
+
+/*
+ * ============================================================================
+ *
+ *                              GObject Methods
+ *
+ * ============================================================================
+ */
+
+
+static void gstlal_gpsclock_base_init(gpointer klass)
+{
+}
+
+
+static void gstlal_gpsclock_class_init(GstLALGPSClockClass *klass)
+{
+	GstClockClass *clock_class = GST_CLOCK_CLASS(klass);
+
+	clock_class->get_internal_time = GST_DEBUG_FUNCPTR(get_internal_time);
+}
+
+
+static void gstlal_gpsclock_init(GstLALGPSClock *element, GstLALGPSClockClass *klass)
+{
+}
---
+++ gstlal/lib/gstlal_gpsclock.h
@@ -0,0 +1,99 @@
+/*
+ * GstLALGPSClock
+ *
+ * Copyright (C) 2012  Kipp Cannon
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#ifndef __GSTLAL_GPS_CLOCK_H__
+#define __GSTLAL_GPS_CLOCK_H__
+
+
+/*
+ * ============================================================================
+ *
+ *                                  Preamble
+ *
+ * ============================================================================
+ */
+
+
+#include <glib.h>
+#include <gst/gst.h>
+
+
+G_BEGIN_DECLS
+
+
+/*
+ * ============================================================================
+ *
+ *                                    Type
+ *
+ * ============================================================================
+ */
+
+
+#define GSTLAL_GPS_CLOCK_TYPE \
+	(gstlal_gps_clock_get_type())
+#define GSTLAL_GPS_CLOCK(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST((obj), GSTLAL_GPS_CLOCK_TYPE, GstLALGPSClock))
+#define GSTLAL_GPS_CLOCK_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST((klass), GSTLAL_GPS_CLOCK_TYPE, GstLALGPSClockClass))
+#define GSTLAL_GPS_CLOCK_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS((obj), GSTLAL_GPS_CLOCK_TYPE, GstLALGPSClockClass))
+#define GST_IS_LAL_GPS_CLOCK(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj), GSTLAL_GPS_CLOCK_TYPE))
+#define GST_IS_LAL_GPS_CLOCK_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE((klass), GSTLAL_GPS_CLOCK_TYPE))
+
+
+typedef struct _GstLALGPSClockClass GstLALGPSClockClass;
+typedef struct _GstLALGPSClock GstLALGPSClock;
+
+
+struct _GstLALGPSClockClass {
+	GstSystemClockClass parent_class;
+};
+
+
+/**
+ * GstLALGPSClock
+ */
+
+
+struct _GstLALGPSClock {
+	GstSystemClock systemclock;
+};
+
+
+/*
+ * ============================================================================
+ *
+ *                                Exported API
+ *
+ * ============================================================================
+ */
+
+
+GType gstlal_gps_clock_get_type(void);
+
+
+G_END_DECLS
+
+
+#endif	/* __GSTLAL_GPS_CLOCK_H__ */
