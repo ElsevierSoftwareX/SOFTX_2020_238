4201f0488530d741e26053bcf68b7b9603ac0125
Chad Hanna <chad.hanna@ligo.org>
2012-12-06 05:06:58 -0800

    lloidparts: deprecate mkLLOIDsrc() and mkLLOIDbasicsrc() functions and replace their guts with data source versions. Also mv htgate function to datasource.

--- gstlal-inspiral/python/lloidparts.py
+++ gstlal-inspiral/python/lloidparts.py
@@ -27,6 +27,7 @@
 import math
 import sys
 import numpy
+import warnings
 
 
 # The following snippet is taken from http://gstreamer.freedesktop.org/wiki/FAQ#Mypygstprogramismysteriouslycoredumping.2Chowtofixthis.3F
@@ -44,6 +45,7 @@ from gstlal import bottle
 from gstlal import pipeparts
 from gstlal import reference_psd
 from gstlal import simulation
+from gstlal import datasource
 
 
 #
@@ -85,23 +87,6 @@ def mksegmentsrcgate(pipeline, src, segment_list, threshold, seekevent = None, i
 		if not segsrc.send_event(seekevent):
 			raise RuntimeError("Element %s did not handle seek event" % segsrc.get_name())
 	return pipeparts.mkgate(pipeline, src, threshold = threshold, control = pipeparts.mkqueue(pipeline, segsrc))
-
-#
-# gate controlled by h(t)
-#
-
-
-def mkhtgate(pipeline, src, control = None, threshold = 8.0, attack_length = -128, hold_length = -128, name = None):
-	# FIXME someday explore a good bandpass filter
-	# src = pipeparts.mkaudiochebband(pipeline, src, low_frequency, high_frequency)
-	src = pipeparts.mktee(pipeline, src)
-	if control is None:
-		control = pipeparts.mkqueue(pipeline, src, max_size_time = 0, max_size_bytes = 0, max_size_buffers = 0)
-	input = pipeparts.mkqueue(pipeline, src, max_size_time = gst.SECOND, max_size_bytes = 0, max_size_buffers = 0)
-	if name is not None:
-		return pipeparts.mkgate(pipeline, input, threshold = threshold, control = control, attack_length = attack_length, hold_length = hold_length, invert_control = True, name = name)
-	else:
-		return pipeparts.mkgate(pipeline, input, threshold = threshold, control = control, attack_length = attack_length, hold_length = hold_length, invert_control = True)
 
 
 def seek_event_for_gps(gps_start_time, gps_end_time, flags = 0):
@@ -203,284 +188,41 @@ class get_gate_state(object):
 
 
 def mkLLOIDbasicsrc(pipeline, seekevent, instrument, detector, data_source = "frames", injection_filename = None, frame_segments = None, state_vector_on_off_dict = {"H1" : (0x7, 0x160), "L1" : (0x7, 0x160), "V1" : (0x67, 0x100)}, verbose = False):
+
 	#
-	# data source
+	# FIXME this is deprecated, call datasource.mkbasicsrc directly.  THIS WILL BE DELETED!
 	#
-
-	# First process fake data or frame data
-	if data_source == "white":
-		# seek events have to be given to these since the element returned is a tag inject
-		src = pipeparts.mkfakesrcseeked(pipeline, instrument, detector.channel, seekevent, blocksize = detector.block_size)
-	elif data_source == "silence":
-		# seek events have to be given to these since the element returned is a tag inject
-		src = pipeparts.mkfakesrcseeked(pipeline, instrument, detector.channel, seekevent, blocksize = detector.block_size, wave = 4)
-	elif data_source == 'LIGO':
-		src = pipeparts.mkfakeLIGOsrc(pipeline, instrument = instrument, channel_name = detector.channel, blocksize = detector.block_size)
-	elif data_source == 'AdvLIGO':
-		src = pipeparts.mkfakeadvLIGOsrc(pipeline, instrument = instrument, channel_name = detector.channel, blocksize = detector.block_size)
-	elif data_source == 'AdvVirgo':
-		src = pipeparts.mkfakeadvvirgosrc(pipeline, instrument = instrument, channel_name = detector.channel, blocksize = detector.block_size)
-	elif data_source == "frames":
-		if instrument == "V1":
-			#FIXME Hack because virgo often just uses "V" in the file names rather than "V1".  We need to sieve on "V"
-			src = pipeparts.mkframesrc(pipeline, location = detector.frame_cache, instrument = instrument, cache_src_regex = "V", channel_name = detector.channel, blocksize = detector.block_size, segment_list = frame_segments)
-		else:
-			src = pipeparts.mkframesrc(pipeline, location = detector.frame_cache, instrument = instrument, cache_dsc_regex = instrument, channel_name = detector.channel, blocksize = detector.block_size, segment_list = frame_segments)
-	# Next process online data, fake data must be None for this to have gotten this far
-	elif data_source == "online":
-		# See https://wiki.ligo.org/DAC/ER2DataDistributionPlan#LIGO_Online_DQ_Channel_Specifica
-		state_vector_on_bits, state_vector_off_bits = state_vector_on_off_dict[instrument]
-
-		# FIXME:  be careful hard-coding shared-memory partition
-		# FIXME make wait_time adjustable through web interface or command line or both
-		src = pipeparts.mklvshmsrc(pipeline, shm_name = {"H1": "LHO_Data", "H2": "LHO_Data", "L1": "LLO_Data", "V1": "VIRGO_Data"}[instrument], wait_time = 120)
-		src = pipeparts.mkframecppchanneldemux(pipeline, src, do_file_checksum = True, skip_bad_files = True)
-
-		# strain
-		strain = pipeparts.mkqueue(pipeline, None, max_size_buffers = 0, max_size_bytes = 0, max_size_time = gst.SECOND * 60 * 10) # 10 minutes of buffering
-		pipeparts.src_deferred_link(src, "%s:%s" % (instrument, detector.channel), strain.get_pad("sink"))
-		strain = pipeparts.mkaudiorate(pipeline, strain, skip_to_first = True, silent = False)
-		@bottle.route("/%s/strain_add_drop.txt" % instrument)
-		def strain_add(elem = strain):
-			import time
-			from pylal.date import XLALUTCToGPS
-			t = float(XLALUTCToGPS(time.gmtime()))
-			add = elem.get_property("add")
-			drop = elem.get_property("drop")
-			# FIXME don't hard code the sample rate
-			return "%.9f %d %d" % (t, add / 16384., drop / 16384.)
-
-		# state vector
-		statevector = gst.element_factory_make("queue")
-		statevector.set_property("max_size_buffers", 0)
-		statevector.set_property("max_size_bytes", 0)
-		statevector.set_property("max_size_time", gst.SECOND * 60 * 10) # 10 minutes of buffering
-		pipeline.add(statevector)
-		# FIXME:  don't hard-code channel name
-		pipeparts.src_deferred_link(src, "%s:%s" % (instrument, "LLD-DQ_VECTOR"), statevector.get_pad("sink"))
-		# FIXME we don't add a signal handler to the statevector audiorate, I assume it should report the same missing samples?
-		statevector = pipeparts.mkaudiorate(pipeline, statevector, skip_to_first = True)
-		statevector = pipeparts.mkstatevector(pipeline, statevector, required_on = state_vector_on_bits, required_off = state_vector_off_bits)
-		@bottle.route("/%s/state_vector_on_off_gap.txt" % instrument)
-		def state_vector_state(elem = statevector):
-			import time
-			from pylal.date import XLALUTCToGPS
-			t = float(XLALUTCToGPS(time.gmtime()))
-			on = elem.get_property("on-samples")
-			off = elem.get_property("off-samples")
-			gap = elem.get_property("gap-samples")
-			return "%.9f %d %d %d" % (t, on, off, gap)
-
-		# use state vector to gate strain
-		src = pipeparts.mkgate(pipeline, strain, threshold = 1, control = statevector, name = "%s_statevector_gate" % instrument)
-		# export state vector state
-		src.set_property("emit-signals", True)
-		# FIXME:  let the state vector messages going to stderr be
-		# controled somehow
-		bottle.route("/%s/current_segment.txt" % instrument)(get_gate_state(src, msg = instrument, verbose = True).text)
 	
-	else:
-		raise ValueError("invalid data_source: %s" % data_source)
+	warnings.warn("mkLLOIDbasicsrc() is obsolete.  Please modify your code to call datasource.mkbasicsrc as this function will be deleted in a future release", DeprecationWarning)
 
-	# seek some non-live sources FIXME someday this should go away and seeks
-	# should only be done on the pipeline that is why this is separated
-	# here
-	if data_source in ("LIGO", "AdvLIGO", "AdvVirgo", "frames"):
-		#
-		# seek the data source if not live
-		#
+	class gwinfo(object):
+		def __init__(self, seekevent, instrument, detector, data_source, injection_filename, frame_segments, state_vector_on_off_dict):
+			self.channel_dict = {instrument: detector.channel}
+			self.frame_segments = frame_segments or dict([(instrument, None) for instrument in self.channel_dict])
+			self.seekevent = seekevent
+			self.state_vector_on_off_bits = state_vector_on_off_dict
+			self.data_source = data_source
+			self.frame_cache = detector.frame_cache
+			self.block_size = detector.block_size
+			self.injection_filename = injection_filename
+			self.gate_start_callback = None
+			self.gate_stop_callback = None
+			self.shm_part_dict = {"H1": "LHO_Data", "H2": "LHO_Data", "L1": "LLO_Data", "V1": "VIRGO_Data"}
+			self.dq_channel_dict = { "H1": "LLD-DQ_VECTOR", "H2": "LLD-DQ_VECTOR","L1": "LLD-DQ_VECTOR", "V1": "LLD-DQ_VECTOR" }
 
-		if src.set_state(gst.STATE_READY) != gst.STATE_CHANGE_SUCCESS:
-			raise RuntimeError("Element %s did not want to enter ready state" % src.get_name())
-		if not src.send_event(seekevent):
-			raise RuntimeError("Element %s did not handle seek event" % src.get_name())
-
-	#
-	# provide an audioconvert element to allow Virgo data (which is
-	# single-precision) to be adapted into the pipeline
-	#
-
-	src = pipeparts.mkaudioconvert(pipeline, src)
-
-
-	#
-	# progress report
-	#
-
-	if verbose:
-		src = pipeparts.mkprogressreport(pipeline, src, "progress_src_%s" % instrument)
-
-	#
-	# optional injections
-	#
-
-	if injection_filename is not None:
-		src = pipeparts.mkinjections(pipeline, src, injection_filename)
-
-	#
-	# done
-	#
-
-	return src
+	return datasource.mkbasicsrc(pipeline, gwinfo(seekevent, instrument, detector, data_source, injection_filename, frame_segments, state_vector_on_off_dict), instrument, verbose)
 
 
 def mkLLOIDsrc(pipeline, src, rates, instrument, psd = None, psd_fft_length = 8, ht_gate_threshold = None, veto_segments = None, seekevent = None, nxydump_segment = None, track_psd = False, block_duration = None, zero_pad = 0):
 	"""Build pipeline stage to whiten and downsample h(t)."""
 
 	#
-	# down-sample to highest of target sample rates.  we include a caps
-	# filter upstream of the resampler to ensure that this is, infact,
-	# *down*-sampling.  if the source time series has a lower sample
-	# rate than the highest target sample rate the resampler will
-	# become an upsampler, and the result will likely interact poorly
-	# with the whitener as it tries to ampify the non-existant
-	# high-frequency components, possibly adding significant numerical
-	# noise to its output.  if you see errors about being unable to
-	# negotiate a format from this stage in the pipeline, it is because
-	# you are asking for output sample rates that are higher than the
-	# sample rate of your data source.
+	# FIXME this function is deprecated, should be deleted
 	#
 
-	quality = 9
-	head = pipeparts.mkcapsfilter(pipeline, src, "audio/x-raw-float, rate=[%d,MAX]" % max(rates))
-	head = pipeparts.mkcapsfilter(pipeline, pipeparts.mkresample(pipeline, head, quality = quality), "audio/x-raw-float, rate=%d" % max(rates))
-	head = pipeparts.mknofakedisconts(pipeline, head)	# FIXME:  remove when resampler is patched
-	head = pipeparts.mkchecktimestamps(pipeline, head, "%s_timestamps_%d_hoft" % (instrument, max(rates)))
+	warnings.warn("mkLLOIDsrc() is deprecated.  Call datasource.mkwhitened_multirate_src() instead", DeprecationWarning)
 
-	#
-	# add a reblock element.  to reduce disk I/O gstlal_inspiral asks
-	# framesrc to provide enormous buffers, and it helps reduce the RAM
-	# pressure of the pipeline by slicing them up.  also, the
-	# whitener's gap support isn't 100% yet and giving it smaller input
-	# buffers works around the remaining weaknesses (namely that when
-	# it sees a gap buffer large enough to drain its internal history,
-	# it doesn't know enough to produce a short non-gap buffer to drain
-	# its history followed by a gap buffer, it just produces one huge
-	# non-gap buffer that's mostly zeros).
-	#
-
-	head = pipeparts.mkreblock(pipeline, head, block_duration = block_duration)
-
-	#
-	# construct whitener.
-	#
-
-	head = pipeparts.mkwhiten(pipeline, head, fft_length = psd_fft_length, zero_pad = zero_pad, average_samples = 64, median_samples = 7, expand_gaps = True, name = "lal_whiten_%s" % instrument)
-	# export PSD in ascii text format
-	# FIXME:  also make them available in XML format as a single document
-	@bottle.route("/%s/psd.txt" % instrument)
-	def get_psd_txt(elem = head):
-		delta_f = elem.get_property("delta-f")
-		yield "# frequency\tspectral density\n"
-		for i, value in enumerate(elem.get_property("mean-psd")):
-			yield "%.16g %.16g\n" % (i * delta_f, value)
-	if psd is None:
-		# use running average PSD
-		head.set_property("psd-mode", 0)
-	else:
-		# use running psd
-		if track_psd:
-			head.set_property("psd-mode", 0)
-		# use fixed PSD
-		else:
-			head.set_property("psd-mode", 1)
-
-		#
-		# install signal handler to retrieve \Delta f and
-		# f_{Nyquist} whenever they are known and/or change,
-		# resample the user-supplied PSD, and install it into the
-		# whitener.
-		#
-
-		def psd_resolution_changed(elem, pspec, psd):
-			# get frequency resolution and number of bins
-			delta_f = elem.get_property("delta-f")
-			n = int(round(elem.get_property("f-nyquist") / delta_f) + 1)
-			# interpolate and install PSD
-			psd = reference_psd.interpolate_psd(psd, delta_f)
-			elem.set_property("mean-psd", psd.data[:n])
-
-		head.connect_after("notify::f-nyquist", psd_resolution_changed, psd)
-		head.connect_after("notify::delta-f", psd_resolution_changed, psd)
-	head = pipeparts.mkchecktimestamps(pipeline, head, "%s_timestamps_%d_whitehoft" % (instrument, max(rates)))
-
-	# FIXME should this be done??
-	# The idea is to drop the whiteners first 8 psd_lengths worth of data
-	# if it is in tracking mode.
-	if psd is None or track_psd:
-		head = pipeparts.mkdrop(pipeline, head, psd_fft_length * 8 * max(rates))
-
-	#
-	# optionally add vetoes
-	#
-
-	if veto_segments is not None:
-		head = mksegmentsrcgate(pipeline, head, veto_segments, threshold=0.1, seekevent=seekevent, invert_output=True)
-
-	#
-	# tee for highest sample rate stream
-	#
-
-	head = {max(rates): pipeparts.mktee(pipeline, head)}
-
-	#
-	# down-sample whitened time series to remaining target sample rates
-	# while applying an amplitude correction to adjust for low-pass
-	# filter roll-off.  we also scale by \sqrt{original rate / new
-	# rate}.  this is done to preserve the square magnitude of the time
-	# series --- the inner product of the time series with itself.
-	# really what we want is for
-	#
-	#	\int v_{1}(t) v_{2}(t) \diff t
-	#		\approx \sum v_{1}(t) v_{2}(t) \Delta t
-	#
-	# to be preserved across different sample rates, i.e. for different
-	# \Delta t.  what we do is rescale the time series and ignore
-	# \Delta t, so we put 1/2 factor of the ratio of the \Delta t's
-	# into the h(t) time series here, and, later, another 1/2 factor
-	# into the template when it gets downsampled.
-	#
-	# by design, the output of the whitener is a unit-variance time
-	# series.  however, downsampling it reduces the variance due to the
-	# removal of some frequency components.  we require the input to
-	# the orthogonal filter banks to be unit variance, therefore a
-	# correction factor is applied via an audio amplify element to
-	# adjust for the reduction in variance due to the downsampler.
-	#
-
-	# FIXME this for loop was reworked to allow the h(t) gate to go after
-	# audioresamplers.  There is apparently a cornercase in the
-	# audioresample element that is causing a problem
-	for rate in sorted(set(rates)):
-		if rate < max(rates): # downsample
-			head[rate] = pipeparts.mkaudioamplify(pipeline, head[max(rates)], 1/math.sqrt(pipeparts.audioresample_variance_gain(quality, max(rates), rate)))
-			head[rate] = pipeparts.mkcapsfilter(pipeline, pipeparts.mkresample(pipeline, head[rate], quality = quality), caps = "audio/x-raw-float, rate=%d" % rate)
-			head[rate] = pipeparts.mknofakedisconts(pipeline, head[rate])	# FIXME:  remove when resampler is patched
-			head[rate] = pipeparts.mkchecktimestamps(pipeline, head[rate], "%s_timestamps_%d_whitehoft" % (instrument, rate))
-	
-		#
-		# optional gate on whitened h(t) amplitude
-		#
-
-		if ht_gate_threshold is not None:
-			# all h(t) gates are controlled by the same max rate control input.
-			head[rate] = mkhtgate(pipeline, head[rate], control = pipeparts.mkqueue(pipeline, head[max(rates)], max_size_time = 0, max_size_bytes = 0, max_size_buffers = 0), threshold = ht_gate_threshold, hold_length = -rate // 4, attack_length = -rate // 4, name = "%s_%d_ht_gate" % (instrument, rate))
-			# export ht gate state
-			head[rate].set_property("emit-signals", True)
-			# FIXME:  let the state messages going to stderr be
-			# controled somehow
-			bottle.route("/%s/%d/current_ht_gate_segment.txt" % (instrument, rate))(get_gate_state(head[rate], msg = "%s_%d_ht_gate" % (instrument, rate), verbose = True).text)
-
-		head[rate] = pipeparts.mktee(pipeline, head[rate])
-
-	#
-	# done.  return value is a dictionary of tee elements indexed by
-	# sample rate
-	#
-
-	#for rate, elem in head.items():
-	#	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, elem), "src_%d.dump" % rate)
-	return head
+	return datasource.mkwhitened_multirate_src(pipeline, src, rates, instrument, psd = psd, psd_fft_length = psd_fft_length, ht_gate_threshold = ht_gate_threshold, veto_segments = veto_segments, seekevent = seekevent, nxydump_segment = nxydump_segment, track_psd = track_psd, block_duration = block_duration, zero_pad = zero_pad)
 
 
 #
