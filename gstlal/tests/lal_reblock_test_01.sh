2dc2a8eecc4ebd842266d5da85d844242018807a
Kipp Cannon <kipp.cannon@ligo.org>
2013-02-01 14:25:08 -0500

    add verbosity to lal_reblock unit test

--- gstlal/tests/lal_reblock_test_01.sh
+++ gstlal/tests/lal_reblock_test_01.sh
@@ -4,5 +4,6 @@ function run_lal_reblock() {
 	gst-launch audiotestsrc blocksize=16384 num-buffers=512 wave=9 ! tee name=in ! audio/x-raw-float, width=32, channels=1, rate=300 ! lal_reblock block-duration=0312400000 ! queue ! lal_checktimestamps silent=true timestamp-fuzz=1 ! lal_nxydump ! filesink location="${name}_out.dump" sync=false async=false in. ! lal_nxydump ! filesink location="${name}_in.dump" sync=false async=false
 }
 
+echo === Running Test ${name} ===
 { ! run_lal_reblock 2>&1 | grep -q "lal_checktimestamps" ; } &&
 ./cmp_nxydumps ${name}_{in,out}.dump
