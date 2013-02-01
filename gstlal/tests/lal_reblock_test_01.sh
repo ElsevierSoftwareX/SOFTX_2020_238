82709e052a5c04152fa5c9e04cc8b72472440184
Kipp Cannon <kipp.cannon@ligo.org>
2013-02-01 14:07:24 -0500

    expand lal_reblock unit tests to be sample-for-sample

--- gstlal/tests/lal_reblock_test_01.sh
+++ gstlal/tests/lal_reblock_test_01.sh
@@ -1,5 +1,8 @@
+name=lal_reblock_test_01
+
 function run_lal_reblock() {
-	gst-launch audiotestsrc blocksize=16384 num-buffers=4096 wave=9 ! audio/x-raw-float, width=32, channels=1, rate=125 ! lal_reblock block-duration=0312400000 ! queue ! lal_checktimestamps silent=true timestamp-fuzz=0 ! fakesink
+	gst-launch audiotestsrc blocksize=16384 num-buffers=512 wave=9 ! tee name=in ! audio/x-raw-float, width=32, channels=1, rate=300 ! lal_reblock block-duration=0312400000 ! queue ! lal_checktimestamps silent=true timestamp-fuzz=1 ! lal_nxydump ! filesink location="${name}_out.dump" sync=false async=false in. ! lal_nxydump ! filesink location="${name}_in.dump" sync=false async=false
 }
 
-! run_lal_reblock 2>&1 | grep -q "lal_checktimestamps"
+{ ! run_lal_reblock 2>&1 | grep -q "lal_checktimestamps" ; } &&
+./cmp_nxydumps ${name}_{in,out}.dump
