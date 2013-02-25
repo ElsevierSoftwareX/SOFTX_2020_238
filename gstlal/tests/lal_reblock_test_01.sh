14b2478377fac36b812d2c0e8b394ef59a2b7f1d
Kipp Cannon <kipp.cannon@ligo.org>
2013-02-25 14:14:27 -0500

    tidy up lal_reblock_test_01.sh

--- gstlal/tests/lal_reblock_test_01.sh
+++ gstlal/tests/lal_reblock_test_01.sh
@@ -1,7 +1,7 @@
 name=lal_reblock_test_01
 
 function run_lal_reblock() {
-	gst-launch audiotestsrc blocksize=16384 num-buffers=512 wave=9 ! tee name=in ! audio/x-raw-float, width=32, channels=1, rate=300 ! lal_reblock block-duration=0312400000 ! queue ! lal_checktimestamps silent=true timestamp-fuzz=1 ! lal_nxydump ! filesink location="${name}_out.dump" sync=false async=false in. ! lal_nxydump ! filesink location="${name}_in.dump" sync=false async=false
+	gst-launch audiotestsrc blocksize=16384 num-buffers=512 wave=9 ! audio/x-raw-float, width=32, channels=1, rate=300 ! tee name=in ! lal_reblock block-duration=0312400000 ! queue ! lal_checktimestamps silent=true timestamp-fuzz=1 ! lal_nxydump ! filesink location="${name}_out.dump" sync=false async=false in. ! lal_nxydump ! filesink location="${name}_in.dump" sync=false async=false
 }
 
 echo === Running Test ${name} ===
