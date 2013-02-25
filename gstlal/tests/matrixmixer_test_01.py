13f92bbe5d0e0a19127de43dc794289c3e040913
Kipp Cannon <kipp.cannon@ligo.org>
2013-02-25 14:21:05 -0500

    add matrixmixer unit test

---
+++ gstlal/tests/matrixmixer_test_01.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# Copyright (C) 2013  Kipp Cannon
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+
+
+import numpy
+import sys
+from gstlal import pipeparts
+import test_common
+
+
+#
+# =============================================================================
+#
+#                                  Pipelines
+#
+# =============================================================================
+#
+
+
+#
+# is the matrixmixer element an identity transform when given an identity
+# matrix?
+#
+
+
+def matrixmixer_test_01(pipeline, name, width):
+	#
+	# try changing these.  test should still work!
+	#
+
+	rate = 2048	# Hz
+	gap_frequency = 13.0	# Hz
+	gap_threshold = 0.8	# of 1
+	buffer_length = 1.0	# seconds
+	test_duration = 10.0	# seconds
+
+	#
+	# build pipeline
+	#
+
+	head = test_common.gapped_test_src(pipeline, buffer_length = buffer_length, rate = rate, width = width, channels = 2, test_duration = test_duration, gap_frequency = gap_frequency, gap_threshold = gap_threshold, control_dump_filename = "%s_control.dump" % name)
+	head = tee = pipeparts.mktee(pipeline, head)
+
+	head = pipeparts.mkmatrixmixer(pipeline, head, numpy.identity(2, dtype = "double"))
+	head = pipeparts.mkchecktimestamps(pipeline, head)
+	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "%s_out.dump" % name)
+	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, tee), "%s_in.dump" % name)
+
+	#
+	# done
+	#
+
+	return pipeline
+
+
+#
+# =============================================================================
+#
+#                                     Main
+#
+# =============================================================================
+#
+
+
+test_common.build_and_run(matrixmixer_test_01, "matrixmixer_test_01a", width = 64)
+test_common.build_and_run(matrixmixer_test_01, "matrixmixer_test_01b", width = 32)
+
