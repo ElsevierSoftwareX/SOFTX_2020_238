e83c0ab8c9cc4e5d40a22aa573f5718b6cc90731
Kipp Cannon <kcannon@cita.utoronto.ca>
2013-05-11 20:03:11 -0400

    framecpp:  generalize mux and demux caps
    allow bit depths other than the sample width

--- gstlal-ugly/gst/framecpp/framecpp_channelmux.cc
+++ gstlal-ugly/gst/framecpp/framecpp_channelmux.cc
@@ -1051,28 +1051,28 @@ static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE(
 			"channels = (int) 1, " \
 			"endianness = (int) BYTE_ORDER, " \
 			"width = (int) 8, " \
-			"depth = (int) 8, " \
+			"depth = (int) [1, 8], " \
 			"signed = (boolean) {true, false};" \
 		"audio/x-raw-int, " \
 			"rate = (int) [1, MAX], " \
 			"channels = (int) 1, " \
 			"endianness = (int) BYTE_ORDER, " \
 			"width = (int) 16, " \
-			"depth = (int) 16, " \
+			"depth = (int) [1, 16], " \
 			"signed = (boolean) {true, false};" \
 		"audio/x-raw-int, " \
 			"rate = (int) [1, MAX], " \
 			"channels = (int) 1, " \
 			"endianness = (int) BYTE_ORDER, " \
 			"width = (int) 32, " \
-			"depth = (int) 32, " \
+			"depth = (int) [1, 32], " \
 			"signed = (boolean) {true, false};" \
 		"audio/x-raw-int, " \
 			"rate = (int) [1, MAX], " \
 			"channels = (int) 1, " \
 			"endianness = (int) BYTE_ORDER, " \
 			"width = (int) 64, " \
-			"depth = (int) 64, " \
+			"depth = (int) [1, 64], " \
 			"signed = (boolean) {true, false};" \
 	)
 );
