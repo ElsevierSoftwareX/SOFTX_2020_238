c6df5eb70b6f1e36629cfd440752bbe6f601953c
Kipp Cannon <kipp.cannon@ligo.org>
2013-05-23 23:37:43 -0400

    convert gstlal.pipeparts into a sub-package

--- gstlal/python/pipeparts/__init__.py
+++ gstlal/python/pipeparts/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009--2011  LIGO Scientific Collaboration
+# Copyright (C) 2009--2013  LIGO Scientific Collaboration
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
