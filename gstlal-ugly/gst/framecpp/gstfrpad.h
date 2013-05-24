d25c7f21efab9c7bd9f68ba70e569769f0cd229d
Kipp Cannon <kipp.cannon@ligo.org>
2013-05-23 23:42:27 -0400

    GstFrPad:  update copyright dates

--- gstlal-ugly/gst/framecpp/gstfrpad.h
+++ gstlal-ugly/gst/framecpp/gstfrpad.h
@@ -1,7 +1,7 @@
 /*
  * GstFrPad
  *
- * Copyright (C) 2012  Kipp Cannon
+ * Copyright (C) 2012,2013  Kipp Cannon
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
