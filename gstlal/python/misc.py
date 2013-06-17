71b802c0814243d47602624b39cfd01b3805e0eb
Kipp Cannon <kipp.cannon@ligo.org>
2013-06-17 16:12:49 -0500

    gstlal:  update copyright notices in python/

--- gstlal/python/misc.py
+++ gstlal/python/misc.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2010  Kipp Cannon <kipp.cannon@ligo.org>
+# Copyright (C) 2010,2011  Kipp Cannon <kipp.cannon@ligo.org>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
