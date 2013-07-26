b264398cd4137b385f2316ac0a7c05492b662772
Chris Pankow <chris.pankow@ligo.org>
2013-07-26 01:43:33 -0500

    excesspower.py: Add function to create filters from sngl_burst table.

--- gstlal-ugly/python/excesspower.py
+++ gstlal-ugly/python/excesspower.py
@@ -297,6 +297,119 @@ def build_filter(psd, rate=4096, flow=64, fhigh=2000, filter_len=0, b_wind=16.0,
 	filters.shape = ( bands, filter_len-1 )
 	return filters, freq_filters
 
+def build_filter_from_xml(psd, sb_table, corr=None):
+	"""
+	Build a set of individual channel Hann window frequency filters (with bandwidth 'band') and then transfer them into the time domain as a matrix. The nth row of the matrix contains the time-domain filter for the flow+n*band frequency channel. The overlap is the fraction of the channel which overlaps with the previous channel. If filter_len is not set, then it defaults to nominal minimum width needed for the bandwidth requested.
+	"""
+
+	# FIXME: We'll need to create one plan for each duration for filters with
+	# differing durations
+	# FFTW requires a thread lock for plans
+	gstlal.fftw.lock()
+	try:
+		# Build spectral correlation function
+		spec_corr = numpy.array( corr )
+		ifftplan = XLALCreateReverseREAL8FFTPlan( (len(psd.data)-1)*2, 1 )
+		d_len = (len(psd.data)-1)*2
+	finally:
+		# Give the lock back
+		gstlal.fftw.unlock()
+
+	# Filter length needs to be long enough to get the pertinent features in
+	# the time domain
+	rate = psd.deltaF * len(psd.data)
+	filter_len = 4*int(rate/sb_table[0].bandwidth)
+	
+	# TODO: For filters with different durations, we'll have to keep track of
+	# the intended length of each filter
+	filters = numpy.zeros((filter_len-1)*len(sb_table))
+	freq_filters = []
+	for i, row in enumerate(sb_table):
+		# cfreq + band since the filters are actually 2*band wide
+		if row.central_freq + row.bandwidth > rate/2:
+			print >> sys.stderr, "WARNING: high frequency (%f) requested is higher than Nyquist, adjusting to match." % (row.central_freq + row.bandwidth)
+			continue
+
+		if row.central_freq + row.bandwidth == rate/2:
+			print >> sys.stderr, "WARNING: high frequency (%f) is equal to Nyquist. Filters will probably be bad. Reduce the high frequency." % (row.central_freq+ row.bandwidth)
+
+		try:
+			# Create the EP filter in the FD
+			h_wind = lalburst.XLALCreateExcessPowerFilter(
+				#channel_flow =
+				# The XLAL function's flow corresponds to the left side FWHM, not the near zero point. Thus, the filter *actually* begins at f_cent - band and ends at f_cent + band, and flow = f_cent - band/2 and fhigh = f_cent + band/2
+				row.central_freq - row.bandwidth/2.0,
+				#channel_width =
+				row.bandwidth,
+				#psd =
+				psd,
+				#correlation =
+				spec_corr
+			)
+		except: # The XLAL wrapped function didn't work
+			statuserr = "Filter generation failed for band %f with %d samples.\nPossible relevant bits and pieces that went into the function call:\n" % (row.bandwidth, filter_len)
+			statuserr += "PSD - deltaF: %f, f0 %f, npoints %d\n" % (psd.deltaF, psd.f0, len(psd.data))
+			statuserr += "spectrum correlation - npoints %d" % len(spec_corr)
+			sys.exit( statuserr )
+
+		# save the frequency domain filters, if necessary
+		# We make a deep copy here because we don't want the zero padding that
+		# is about to be done to get the filters into the time domain
+		h_wind_copy = COMPLEX16FrequencySeries()
+		h_wind_copy.f0 = h_wind.f0
+		h_wind_copy.deltaF = h_wind.deltaF
+		h_wind_copy.data = copy.deepcopy(h_wind.data)
+		freq_filters.append( h_wind_copy )
+
+		# Zero pad up to lowest frequency
+		tmpdata = numpy.zeros(len(psd.data), dtype=numpy.complex128)
+		offset = int(h_wind.f0/h_wind.deltaF)
+		tmpdata[offset:offset+len(h_wind_copy.data)] = h_wind_copy.data
+		h_wind.data = tmpdata
+		h_wind.f0 = 0.0
+
+		# DEBUG: Uncomment to dump FD filters
+		#f = open( "filters_fd/hann_%dhz" % int(row.central_freq), "w" )
+		#for freq, s in enumerate( h_wind.data ):
+			#f.write( "%f %g\n" % (freq*h_wind.deltaF,s) )
+		#f.close()
+
+		# IFFT the window into a time series for use as a TD filter
+		try:
+			t_series = REAL8TimeSeries()
+			t_series.data = numpy.zeros( (d_len,), dtype="double" ) 
+			XLALREAL8FreqTimeFFT( 
+				# t_series =
+				t_series, 
+				# window_freq_series =
+				h_wind, 
+				# ifft plan =
+				ifftplan
+			)
+		except:
+			sys.exit( "Failed to get time domain filters. The usual cause of this is a filter length which is only a few PSD bins wide. Try increasing the fft-length property of the whitener." )
+
+		td_filter = t_series.data
+		# FIXME: This is a work around for a yet unfound timestamp
+		# drift. Once it's found this should be reverted.
+		#td_filter = numpy.roll( td_filter, filter_len/2 )[:filter_len]
+		td_filter = numpy.roll( td_filter, filter_len/2 )[:filter_len-1]
+		## normalize the filters
+		td_filter /= numpy.sqrt( numpy.dot(td_filter, td_filter) )
+		td_filter *= numpy.sqrt(row.bandwidth/psd.deltaF)
+		filters[(filter_len-1)*i:(filter_len-1)*(i+1)] = td_filter
+		
+		# DEBUG: Uncomment to dump TD filters
+		#f = open( "filters_td/hann_%dhz" % int(row.central_freq), "w" )
+		#for t, s in enumerate( td_filter ):
+			#f.write( "%g %g\n" % (t/rate,s) )
+		#f.close()
+
+	# Shape it into a "matrix-like" object
+	#filters.shape = ( bands, filter_len )
+	filters.shape = ( len(sb_table), filter_len-1 )
+	return filters, freq_filters
+
 def build_chan_matrix( nchannels=1, up_factor=0, norm=None ):
 	"""
 	Build the matrix to properly normalize nchannels coming out of the FIR filter. Norm should be an array of length equal to the number of output channels, with the proper normalization factor. up_factor controls the number of output channels. E.g. If thirty two input channels are indicated, and an up_factor of two is input, then an array of length eight corresponding to eight output channels are required. The output matrix uses 1/sqrt(A_i) where A_i is the element of the input norm array.
