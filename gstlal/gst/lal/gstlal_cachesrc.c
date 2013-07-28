126f413046105a3c0d0f31170f3870047a7a4370
Kipp Cannon <kipp.cannon@ligo.org>
2013-07-27 21:23:33 -0400

    lal_cachesrc:  handle URI scheme "lalcache"

--- gstlal/gst/lal/gstlal_cachesrc.c
+++ gstlal/gst/lal/gstlal_cachesrc.c
@@ -32,6 +32,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -51,6 +52,77 @@
 #include <gstlal_cachesrc.h>
 
 
+/*
+ * ========================================================================
+ *
+ *                               GstURIHandler
+ *
+ * ========================================================================
+ */
+
+
+#define URI_SCHEME "lalcache"
+
+
+static GstURIType uri_get_type(GType type)
+{
+	return GST_URI_SRC;
+}
+
+
+/* 1.0:  this becomes static const gchar *const * */
+static gchar **uri_get_protocols(GType type)
+{
+	/* 1.0:  this becomes
+	static const gchar *protocols[] = {URI_SCHEME, NULL};
+	*/
+	static gchar *protocols[] = {(gchar *) URI_SCHEME, NULL};
+
+	return protocols;
+}
+
+
+/* 1.0:  this becomes static gchar * */
+static const gchar *uri_get_uri(GstURIHandler *handler)
+{
+	GstLALCacheSrc *element = GSTLAL_CACHESRC(handler);
+
+	/* 1.0:  this won't be a memory leak */
+	return g_strdup_printf(URI_SCHEME "://%s", element->location);
+}
+
+
+/* 1.0:  this gets a GError ** argument */
+static gboolean uri_set_uri(GstURIHandler *handler, const gchar *uri)
+{
+	GstLALCacheSrc *element = GSTLAL_CACHESRC(handler);
+	gchar *scheme = g_uri_parse_scheme(uri);
+	gchar location[strlen(uri)];
+	gboolean success = TRUE;
+
+	success = !strcmp(scheme, URI_SCHEME);
+	if(success)
+		success &= sscanf(uri, URI_SCHEME "://%[^?]", location) == 1;
+	if(success)
+		g_object_set(G_OBJECT(element), "location", location, NULL);
+
+	g_free(scheme);
+	return success;
+}
+
+
+static void uri_handler_init(gpointer g_iface, gpointer iface_data)
+{
+	GstURIHandlerInterface *iface = (GstURIHandlerInterface *) g_iface;
+
+	iface->get_uri = GST_DEBUG_FUNCPTR(uri_get_uri);
+	iface->set_uri = GST_DEBUG_FUNCPTR(uri_set_uri);
+	/* 1.0:  this is ->get_type */
+	iface->get_type_full = GST_DEBUG_FUNCPTR(uri_get_type);
+	/* 1.0:  this is ->get_protocols */
+	iface->get_protocols_full = GST_DEBUG_FUNCPTR(uri_get_protocols);
+}
+
 
 /*
  * ============================================================================
@@ -67,6 +139,13 @@ GST_DEBUG_CATEGORY_STATIC(GST_CAT_DEFAULT);
 
 static void additional_initializations(GType type)
 {
+	static const GInterfaceInfo uri_handler_info = {
+		uri_handler_init,
+		NULL,
+		NULL
+	};
+	g_type_add_interface_static(type, GST_TYPE_URI_HANDLER, &uri_handler_info);
+
 	GST_DEBUG_CATEGORY_INIT(GST_CAT_DEFAULT, "lal_cachesrc", 0, "lal_cachesrc element");
 }
 
