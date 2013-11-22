ac3ee9c0e5c325b0ffaaf35e2fc5892c5555f27e
Chad Hanna <chad.hanna@ligo.org>
2013-11-22 09:45:48 -0800

    multirate_datasource: add documentation

--- gstlal/python/multirate_datasource.py
+++ gstlal/python/multirate_datasource.py
@@ -42,9 +42,100 @@ from gstlal import pipeparts
 from gstlal import reference_psd
 from gstlal import datasource
 
+## 
+# @file
+#
+# A file that contains the multirate_datasource module code
+#
 
+##
+# @package python.multirate_datasource
+#
+# multirate_datasource module
+
+## #### produced whitened h(t) at (possibly) multiple sample rates
+# ##### Gstreamer graph describing this function
+#
+# @dot
+# digraph mkbasicsrc {
+#	rankdir = LR;
+#	compound=true;
+#	node [shape=record fontsize=10 fontname="Verdana"];
+#	edge [fontsize=8 fontname="Verdana"];
+#
+#	capsfilter1 [URL="\ref pipeparts.mkcapsfilter()"];
+#	audioresample [URL="\ref pipeparts.mkresample()"];
+#	capsfilter2 [URL="\ref pipeparts.mkcapsfilter()"];
+#	nofakedisconts [URL="\ref pipeparts.mknofakedisconts()"];
+#	reblock [URL="\ref pipeparts.mkreblock()"];
+#	whiten [URL="\ref pipeparts.mkwhiten()"];
+#	audioconvert [URL="\ref pipeparts.mkaudioconvert()"];
+#	capsfilter3 [URL="\ref pipeparts.mkcapsfilter()"];
+#	"segmentsrcgate()" [URL="\ref datasource.mksegmentsrcgate()", label="segmentsrcgate() \n [iff veto segment list provided]", style=filled, color=lightgrey];
+#	tee [URL="\ref pipeparts.mktee()"];
+#	audioamplifyr1 [URL="\ref pipeparts.mkaudioamplify()"];
+#	capsfilterr1 [URL="\ref pipeparts.mkcapsfilter()"];
+#	nofakediscontsr1 [URL="\ref pipeparts.mknofakedisconts()"];
+#	htgater1 [URL="\ref datasource.mkhtgate()", label="htgate() \n [iff ht gate specified]", style=filled, color=lightgrey];
+#	tee1 [URL="\ref pipeparts.mktee()"];
+#	audioamplifyr2 [URL="\ref pipeparts.mkaudioamplify()"];
+#	capsfilterr2 [URL="\ref pipeparts.mkcapsfilter()"];
+#	nofakediscontsr2 [URL="\ref pipeparts.mknofakedisconts()"];
+#	htgater2 [URL="\ref datasource.mkhtgate()", label="htgate() \n [iff ht gate specified]", style=filled, color=lightgrey];
+#	tee2 [URL="\ref pipeparts.mktee()"];
+#	audioamplify_rn [URL="\ref pipeparts.mkaudioamplify()"];
+#	capsfilter_rn [URL="\ref pipeparts.mkcapsfilter()"];
+#	nofakedisconts_rn [URL="\ref pipeparts.mknofakedisconts()"];
+#	htgate_rn [URL="\ref datasource.mkhtgate()", style=filled, color=lightgrey, label="htgate() \n [iff ht gate specified]"];
+#	tee [URL="\ref pipeparts.mktee()"];
+#
+#	// nodes
+#
+#	"?" -> capsfilter1 -> audioresample;
+#	audioresample -> capsfilter2;
+#	capsfilter2 -> nofakedisconts;
+#	nofakedisconts -> reblock;
+#	reblock -> whiten;
+#	whiten -> audioconvert;
+#	audioconvert -> capsfilter3;
+#	capsfilter3 -> "segmentsrcgate()";
+#	"segmentsrcgate()" -> tee;
+#
+#	tee -> audioamplifyr1 [label="Rate 1"];
+#	audioamplifyr1 -> capsfilterr1;
+#	capsfilterr1 -> nofakediscontsr1;
+#	nofakediscontsr1 -> htgater1;
+#	htgater1 -> tee1 -> "? 1";
+#
+#	tee -> audioamplifyr2 [label="Rate 2"];
+#	audioamplifyr2 -> capsfilterr2;
+#	capsfilterr2 -> nofakediscontsr2;
+#	nofakediscontsr2 -> htgater2;
+#	htgater2 -> tee2 -> "? 2";
+#
+#	tee ->  audioamplify_rn [label="Rate N"];
+#	audioamplify_rn -> capsfilter_rn;
+#	capsfilter_rn -> nofakedisconts_rn;
+#	nofakedisconts_rn -> htgate_rn;
+#	htgate_rn -> tee_n -> "? 3";
+#
+# }
+# @enddot
 def mkwhitened_multirate_src(pipeline, src, rates, instrument, psd = None, psd_fft_length = 8, ht_gate_threshold = None, veto_segments = None, seekevent = None, nxydump_segment = None, track_psd = False, block_duration = 1 * gst.SECOND, zero_pad = 0, width = 64):
-	"""Build pipeline stage to whiten and downsample h(t)."""
+	"""!
+	Build pipeline stage to whiten and downsample h(t).
+
+	- pipeline: the gstreamer pipeline to add this to
+	- src: the gstreamer element that will be providing data to this 
+	- rates: a list of the requested sample rates, e.g., [512,1024].
+	- instrument: the instrument to process
+	- psd: a psd frequency series
+	- psd_fft_length: length of fft used for whitening
+	- ht_gate_threshold: gate h(t) if it crosses this value
+	- veto_segments: segments to mark as gaps after whitening
+	- track_psd: decide whether to dynamically track the spectrum or use the fixed spectrum provided
+	- width: type convert to either 32 or 64 bit float
+	"""
 
 	#
 	# down-sample to highest of target sample rates.  we include a caps
