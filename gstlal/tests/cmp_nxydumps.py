31f36f9526a6b10fea69645dc3ea8150791d536b
Kipp Cannon <kipp.cannon@ligo.org>
2014-01-27 12:10:15 -0500

    gstlal tests:  tweak cmp_nxydumps.py
    - port to lal's swig bindings
    - aesthetics in exception handling

--- gstlal/tests/cmp_nxydumps.py
+++ gstlal/tests/cmp_nxydumps.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 
-from pylal.xlal.datatypes.ligotimegps import LIGOTimeGPS
+from lal import LIGOTimeGPS
 
 
-default_timestamp_fuzz = 1e-9
-default_sample_fuzz = 1e-15
+default_timestamp_fuzz = 1e-9	# seconds
+default_sample_fuzz = 1e-15	# relative
 
 
 def compare(filename1, filename2, timestamp_fuzz = default_timestamp_fuzz, sample_fuzz = default_sample_fuzz):
 	timestamp_fuzz = LIGOTimeGPS(timestamp_fuzz)
 	try:
-		for n, (line1, line2) in enumerate(zip(open(filename1), open(filename2))):
+		for n, (line1, line2) in enumerate(zip(open(filename1), open(filename2)), start = 1):
 			line1 = line1.strip().split()
 			line2 = line2.strip().split()
 			line1 = [LIGOTimeGPS(line1[0])] +  map(float, line1[1:])
@@ -20,8 +20,8 @@ def compare(filename1, filename2, timestamp_fuzz = default_timestamp_fuzz, sampl
 			assert abs(line1[0] - line2[0]) <= timestamp_fuzz
 			for val1, val2 in zip(line1[1:], line2[1:]):
 				assert abs(val1 - val2) / max(val1, val2) <= sample_fuzz
-	except AssertionError, e:
-		raise AssertionError("line %d: %s" % (n + 1, str(e)))
+	except AssertionError as e:
+		raise AssertionError("line %d: %s" % (n, str(e)))
 
 
 if __name__ == "__main__":
@@ -33,5 +33,5 @@ if __name__ == "__main__":
 
 	try:
 		compare(filename1, filename2, timestamp_fuzz = options.timestamp_fuzz, sample_fuzz = options.sample_fuzz)
-	except AssertionError, e:
+	except AssertionError as e:
 		raise type(e)("%s <--> %s: %s" % (filename1, filename2, str(e)))
