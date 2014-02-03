b9172957fe13d6b686fe81769c4bb4ca7a617250
Kipp Cannon <kipp.cannon@ligo.org>
2014-02-03 13:15:10 -0500

    gstlal-inspiral:  cluster by likelihood ratio
    also cluster across instrument combinations

--- gstlal-inspiral/share/simplify_and_cluster.sql
+++ gstlal-inspiral/share/simplify_and_cluster.sql
@@ -57,17 +57,16 @@ FROM
 CREATE TEMPORARY TABLE _cluster_info_ AS
 	SELECT
 		coinc_event.coinc_event_id AS coinc_event_id,
-		(coinc_event.time_slide_id || ";" || coinc_event.instruments) AS category,
+		coinc_event.time_slide_id AS category,
 		(coinc_inspiral.end_time - (SELECT MIN(end_time) FROM coinc_inspiral)) + 1e-9 * coinc_inspiral.end_time_ns AS end_time,
-		coinc_inspiral.snr AS snr
-		--CASE WHEN coinc_inspiral.combined_far != 0 THEN 1.0 / coinc_inspiral.combined_far ELSE coinc_inspiral.snr / (SELECT MIN(coinc_inspiral.combined_far) FROM coinc_inspiral WHERE coinc_inspiral.combined_far != 0) END AS ifar
+		coinc_event.likelihood AS ranking_stat
 	FROM
 		coinc_event
 		JOIN coinc_inspiral ON (
 			coinc_inspiral.coinc_event_id == coinc_event.coinc_event_id
 		);
 CREATE INDEX tmpindex1 ON _cluster_info_ (coinc_event_id);
-CREATE INDEX tmpindex2 ON _cluster_info_ (category, end_time, snr);
+CREATE INDEX tmpindex2 ON _cluster_info_ (category, end_time, ranking_stat);
 
 --
 -- delete coincs that are within 4 s of coincs with higher SNR in the same
@@ -85,7 +84,7 @@ WHERE
 			JOIN _cluster_info_ AS _cluster_info_b_ ON (
 				_cluster_info_b_.category == _cluster_info_a_.category
 				AND (_cluster_info_b_.end_time BETWEEN _cluster_info_a_.end_time - 4.0 AND _cluster_info_a_.end_time + 4.0)
-				AND _cluster_info_b_.snr > _cluster_info_a_.snr
+				AND _cluster_info_b_.ranking_stat > _cluster_info_a_.ranking_stat
 			)
 		WHERE
 			_cluster_info_a_.coinc_event_id == coinc_event.coinc_event_id
