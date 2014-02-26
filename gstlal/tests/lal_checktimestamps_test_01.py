a5872cf9e22c8fc73f4b203b9cc512c67f2c8cdd
Chad Hanna <chad.hanna@ligo.org>
2014-02-26 07:09:01 -0800

    lal_checktimestamps_test_01.py: add documentation

--- gstlal/tests/lal_checktimestamps_test_01.py
+++ gstlal/tests/lal_checktimestamps_test_01.py
@@ -42,10 +42,31 @@ from gstlal import simplehandler
 import signal
 import time
 
+## @file lal_checktimestamps_test_01.py
+# A program to intentionally break a running stream in order to test lal_checktimestamps; see lal_checktimestamps_test_01 for more details
 
 #
 # Setup a signal hander to introduce a shift with ctrl+C
 #
+
+## @package lal_checktimestamps_test_01
+#
+# ### USAGE
+#
+# - This test program dynamically adds a one nanosecond time shift every time the user hits ctrl+C.  You need to do kill -9 to stop the program ;) Here is an example session
+#
+#		$ ./lal_checktimestamps_test_01.py
+#		src (00:00:05): 5 seconds
+#		^Cshifting by 1 ns
+#		lal_checktimestamps+lal_checktimestamps0: got timestamp 7.000666617 s expected 7.000666616 s (discont flag is not set)
+#		^Cshifting by 2 ns
+#		src (00:00:10): 10 seconds
+#		lal_checktimestamps+lal_checktimestamps0: got timestamp 10.000666618 s expected 10.000666617 s (discont flag is not set)
+#		lal_checktimestamps+lal_checktimestamps0: timestamp/offset mismatch:  got timestamp 10.000666618 s, buffer offset 20480 corresponds to timestamp 10.000666616 s (error = 2 ns)
+#		lal_checktimestamps+lal_checktimestamps0: timestamp/offset mismatch:  got timestamp 11.000666618 s, buffer offset 22528 corresponds to timestamp 11.000666616 s (error = 2 ns)
+#
+# - Note how the first ctrl+C only gives a warning since 1 ns is within the "fuzz".  But after the second ctrl+C there is an error. If this test is useful we can add it to the lal_checktimestamps documentation directly.
+
 
 class SigHandler(object):
 	def __init__(self, pipeline):
