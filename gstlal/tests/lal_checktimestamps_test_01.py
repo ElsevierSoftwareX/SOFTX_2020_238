adc30153a45d93cdd2ed760f31f53d4d9c004ed5
Chad Hanna <chad.hanna@ligo.org>
2014-03-05 08:15:53 -0800

    lal_checktimestamps_test_01: fix usage doc after lal_shift discont patch

--- gstlal/tests/lal_checktimestamps_test_01.py
+++ gstlal/tests/lal_checktimestamps_test_01.py
@@ -55,17 +55,19 @@ import time
 #
 # - This test program dynamically adds a one nanosecond time shift every time the user hits ctrl+C.  You need to do kill -9 to stop the program ;) Here is an example session
 #
-#		$ ./lal_checktimestamps_test_01.py
+#		./lal_checktimestamps_test_01.py 
 #		src (00:00:05): 5 seconds
-#		^Cshifting by 1 ns
-#		lal_checktimestamps+lal_checktimestamps0: got timestamp 7.000666617 s expected 7.000666616 s (discont flag is not set)
-#		^Cshifting by 2 ns
 #		src (00:00:10): 10 seconds
-#		lal_checktimestamps+lal_checktimestamps0: got timestamp 10.000666618 s expected 10.000666617 s (discont flag is not set)
-#		lal_checktimestamps+lal_checktimestamps0: timestamp/offset mismatch:  got timestamp 10.000666618 s, buffer offset 20480 corresponds to timestamp 10.000666616 s (error = 2 ns)
-#		lal_checktimestamps+lal_checktimestamps0: timestamp/offset mismatch:  got timestamp 11.000666618 s, buffer offset 22528 corresponds to timestamp 11.000666616 s (error = 2 ns)
+#		^Cshifting by 1 ns
+#		lal_checktimestamps+lal_checktimestamps0: discontinuity:  timestamp = 12.000682382 s, offset = 24576;  would have been 12.000682381 s, offset = 24576
+#		src (00:00:15): 15 seconds
+#		^Cshifting by 2 ns
+#		lal_checktimestamps+lal_checktimestamps0: discontinuity:  timestamp = 17.000682383 s, offset = 34816;  would have been 17.000682382 s, offset = 34816
+#		src (00:00:20): 20 seconds
+#		^Cshifting by 3 ns
+#		lal_checktimestamps+lal_checktimestamps0: discontinuity:  timestamp = 22.000682384 s, offset = 45056;  would have been 22.000682383 s, offset = 45056
+#		^Z
 #
-# - Note how the first ctrl+C only gives a warning since 1 ns is within the "fuzz".  But after the second ctrl+C there is an error. If this test is useful we can add it to the lal_checktimestamps documentation directly.
 
 
 class SigHandler(object):
