83e0d297e8a000aa4c3d6e0674eb3176b058e58e
Chris Pankow <chris.pankow@ligo.org>
2014-03-08 11:40:07 -0500

    gstlalugly.c: Add audioratefaker to gstlalugly module

--- gstlal-ugly/gst/lal/gstlalugly.c
+++ gstlal-ugly/gst/lal/gstlalugly.c
@@ -58,6 +58,7 @@
 #include <gstlal_trim.h>
 #include <gstlal_burst_triggergen.h>
 #include <gstlal_bitvectorgen.h>
+#include <audioratefaker.h>
 
 
 /*
@@ -83,6 +84,7 @@ static gboolean plugin_init(GstPlugin *plugin)
 		{"lal_bursttriggergen", GSTLAL_BURST_TRIGGERGEN_TYPE},
 		{"lal_trim", GST_TYPE_LALTRIM},
 		{"lal_bitvectorgen", GSTLAL_BITVECTORGEN_TYPE},
+		{"audioratefaker", GST_TYPE_AUDIO_RATE_FAKER},
 		{NULL, 0},
 	};
 
