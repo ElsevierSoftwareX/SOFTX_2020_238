f0c7f231f6a870d903b6c6ec21dc83aaab40db9c
Shinkee Chung <shinkee.chung@ligo.org>
2014-03-21 12:38:51 +0800

    Fixed the bug in the transform size giving incorrect bugs.

--- gstlal-ugly/gst/lal/gstlal_iirbank.c
+++ gstlal-ugly/gst/lal/gstlal_iirbank.c
@@ -546,10 +546,10 @@ static gboolean transform_size(GstBaseTransform *trans, GstPadDirection directio
 
 		*othersize = size / unit_size + get_available_samples(element);
 
-		if((gint) *othersize > dmax - dmin)
+		if((gint) *othersize > dmax - dmin && get_available_samples > 0 )
 		        *othersize = (*othersize - (dmax-dmin)) * other_unit_size;
 		else
-			*othersize = 0;
+			*othersize *= other_unit_size;
 		break;
 
 	case GST_PAD_UNKNOWN:
