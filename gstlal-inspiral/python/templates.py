a1a10d4dd53c753224430362ef49ac069150f946
Chad Hanna <chad.hanna@ligo.org>
2014-06-18 03:25:49 -0700

    templates.py: add documentation

--- gstlal-inspiral/python/templates.py
+++ gstlal-inspiral/python/templates.py
@@ -15,6 +15,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 ## @file
+# A module to implement template slicing and other operations as part of the LLOID aglorithm
 #
 # ### Review Status
 #
