a1043d9f0e01541c1db123bc850552af6c789d13
Kipp Cannon <kipp.cannon@ligo.org>
2014-09-11 15:00:49 -0400

    add copyright notice to de_calc_likelihood.sql

--- gstlal-inspiral/share/de_calc_likelihood.sql
+++ gstlal-inspiral/share/de_calc_likelihood.sql
@@ -1,3 +1,20 @@
+-- Copyright (C) 2014  Kipp Cannon
+--
+-- This program is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by the
+-- Free Software Foundation; either version 2 of the License, or (at your
+-- option) any later version.
+--
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+-- Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 -- gstlal_inspiral_calc_likelihood, by default, will not re-process a file it
 -- has already processed.  This behaviour allows a sort of "check-pointing"
 -- feature where by if a job is evicted from the cluster and started up again,
@@ -10,6 +27,7 @@
 -- tricking it into believing the file has not been processed yet.  For
 -- safety, this script also deletes the likleihood ratios from the
 -- coinc_event table.
+
 
 DELETE FROM
 	process_params
