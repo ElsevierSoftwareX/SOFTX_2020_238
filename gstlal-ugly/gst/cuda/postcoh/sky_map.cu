b5cdb39515a10dc953a13c1a6f3aa28d7eaaaec4
Qi Chu <qi.chu@ligo.org>
2014-11-03 23:16:43 +0800

    pseudo code

--- gstlal-ugly/gst/cuda/postcoh/sky_map.cu
+++ gstlal-ugly/gst/cuda/postcoh/sky_map.cu
@@ -3,12 +3,24 @@
 
 coh_sky_map(coh_snr, /* OUTPUT */
 	coh_nullstream, /* OUTPUT */
-	theta, /* INPUT, sky_direction */
-	phi, /* INPUT, sky_direction */
-	snr_all, /*INPUT, 2*data_points*/,
-	detector_turn, /* INPUT, which detector we are considering */,
-	offset0 /*INPUT place where the location of the trigger*/) 
+	snr_all, /* INPUT, 2*data_points*/
+	detector_turn, /* INPUT, which detector we are considering */
+	offset0, /* INPUT place where the location of the trigger */
+	need_update,
+	sky_map) /* INPUT, map to get the sky direction */
 
-	u(theta, phi); /* matrix 2*2, 3*3 */
-	offset1(theta, phi);
+
+	if need_update
+	  for all sky directions in sky_map
+	    update u matrix(index) = u(theta, phi, current_time);
+	    update time_delay_ij matrix(index) (1 <= i, j <= num_detectors);
+	else 
+	    do nothing;
+		
+	get u(index); /* matrix 2*2, 3*3 */
+	get time_delay_ij(index);
+	interploate offset_ij;
+
+	calc coh_snr, coh_nullstream;
+
 	
