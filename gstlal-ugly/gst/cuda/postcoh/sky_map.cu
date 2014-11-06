3e39c62fd23945d5ef1c6fe2fe5667cb19381f17
qi.chu <qi.chu@ligo.org>
2014-11-06 16:36:41 +0800

    sky_map.cu

--- gstlal-ugly/gst/cuda/postcoh/sky_map.cu
+++ gstlal-ugly/gst/cuda/postcoh/sky_map.cu
@@ -1,14 +1,16 @@
 
 /* for each sky direction, compute the coherent snr */
 
-coh_sky_map(coh_snr, /* OUTPUT */
-	coh_nullstream, /* OUTPUT */
-	snr_all, /* INPUT, 2*data_points*/
+coh_sky_map(float *coh_snr, /* OUTPUT */
+	float *coh_nullstream, /* OUTPUT */
+	complex_float *snr_all, /* INPUT, (2, 3)*data_points*/
 	det_label, /* INPUT, which detector we are considering */
 	detectors, /* INPUT, all the detectors that are in this coherent analysis */
-	offset0, /* INPUT place where the location of the trigger */
+	int *tiggers_offset0, /* INPUT place where the location of the trigger */
+	int num_triggers, /* INPUT number of triggers */
 	float *u_map, /* INPUT u matrix map, each u matrix corresponds to one sky direction  */
-	float *toa_diff_map, ) /* INPUT, time of arrival difference map*/
+	float *toa_diff_map, /* INPUT, time of arrival difference map*/
+	int num_sky_directions) 
 
 	get u(index); /* matrix 2*2, 3*3 */
 	get toa_diff(ij, index);
