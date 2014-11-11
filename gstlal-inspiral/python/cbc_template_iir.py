b2c94278e05bdb054de7ec6e19fb64770f820013
David McKenzie <20759498@student.uwa.edu.au>
2014-11-11 11:58:20 +0800

    cbc_template_iir.py: Actually fixed the monotonicity bug instead of making it worse here as well

--- gstlal-inspiral/python/cbc_template_iir.py
+++ gstlal-inspiral/python/cbc_template_iir.py
@@ -113,9 +113,9 @@ def cleanFreq(f,fLower):
 	f[0:fStartFix+1] = fLower
 
     i=-100;
-    while(i<0):
+    while(i<-1):
 	#require monotonicity
-	if(f[i]<f[i+1]):
+	if(f[i]>f[i+1]):
 	    f[i+1:]=10; #effectively throw away the end
 	    break;
 	else:
