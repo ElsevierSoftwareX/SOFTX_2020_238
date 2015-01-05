7cb919a33b850574cab9fbcdc5fcd158a31fd4f8
qi.chu <qi.chu@ligo.org>
2015-01-05 17:31:52 +0800

    pipeparts/__init__.py: change the new cudamultiratespiir interface

--- gstlal/python/pipeparts/__init__.py
+++ gstlal/python/pipeparts/__init__.py
@@ -511,8 +511,8 @@ def mkcudaiirbank(pipeline, src, a1, b0, delay, name = None):
  	return elem
 
 
-def mkcudamultiratespiir(pipeline, src, bank_struct, bank_id = 0, gap_handle = 0, name = None):
-	properties = dict((name, value) for name, value in (("name", name), ("spiir_bank", bank_struct), ("bank_id", bank_id), ("gap_handle", gap_handle)) if value is not None)
+def mkcudamultiratespiir(pipeline, src, bank_fname, gap_handle = 0, name = None):
+	properties = dict((name, value) for name, value in (("name", name), ("bank_fname", bank_fname), ("gap_handle", gap_handle)) if value is not None)
 	elem = mkgeneric(pipeline, src, "cuda_multiratespiir", **properties)
 	return elem
 
