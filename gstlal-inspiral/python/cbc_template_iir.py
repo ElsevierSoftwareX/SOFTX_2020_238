f8cc59008929906ba26298925f143c794f02a62a
Qi Chu <qi.chu@ligo.org>
2015-02-17 12:01:14 +0800

    update copyright for cbc_template_iir.py

--- gstlal-inspiral/python/cbc_template_iir.py
+++ gstlal-inspiral/python/cbc_template_iir.py
@@ -1,6 +1,6 @@
 
-# Copyright (C) 2010-2012 Shaun Hooper, David Mckenzie, Qi Chu, Kipp Cannon, Chad Hanna, Leo Singer
-# Copyright (C) 2013-2014 David Mckenzie, Qi Chu
+# Copyright (C) 2010-2012 Shaun Hooper
+# Copyright (C) 2013-2014 David Mckenzie, Qi Chu, Kipp Cannon, Chad Hanna, Leo Singer
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
