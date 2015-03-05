06c45f0c4ed1be7d5965f3a0e51e62be3543798d
CHAD RICHARD HANNA <crh184@psu.edu>
2015-03-04 19:48:14 -0500

    gstlal_interpolator: move to ugly

--- gstlal-ugly/gst/lal/gstlalugly.c
+++ gstlal-ugly/gst/lal/gstlalugly.c
@@ -52,6 +52,7 @@
 #include <gstlal/gstlal_tags.h>
 #include <gstlal_multiplier.h>
 #include <gstlal_iirbank.h>
+#include <gstlal_interpolator.h>
 #include <gstlal_mean.h>
 #include <gstlal_specgram.h>
 #include <gstlal_pad.h>
@@ -77,6 +78,7 @@ static gboolean plugin_init(GstPlugin *plugin)
 	} *element, elements[] = {
 		{"lal_multiplier", GSTLAL_TYPE_MULTIPLIER},
 		{"lal_iirbank", GSTLAL_IIRBANK_TYPE},
+		{"lal_interpolator", GSTLAL_INTERPOLATOR_TYPE},
 		{"lal_mean", GSTLAL_MEAN_TYPE},
 		{"lal_specgram", GSTLAL_SPECGRAM_TYPE},
 		{"lal_pad", GST_TYPE_LALPAD},
