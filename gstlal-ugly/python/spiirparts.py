182cc46d865bcb608851306464301406ae7a07c4
qi.chu <qi.chu@ligo.org>
2015-09-18 11:57:41 +0800

    spiirparts.py: fix the dump_xml name issue. The name of a gstreamer element must be unique

--- gstlal-ugly/python/spiirparts.py
+++ gstlal-ugly/python/spiirparts.py
@@ -434,7 +434,7 @@ def mkPostcohSPIIR(pipeline, detectors, banks, psd, psd_fft_length = 8, ht_gate_
 
 		# FIXME: hard-coded to do compression
 		if verbose:
-			postcoh = pipeparts.mkprogressreport(pipeline, postcoh, "progress_xml_dump")
+			postcoh = pipeparts.mkprogressreport(pipeline, postcoh, "progress_xml_dump_bank_%d" % i_dict)
 		head = mkpostcohfilesink(pipeline, postcoh, location = output_prefix[i_dict], compression = 1, snapshot_interval = 0)
 		triggersrcs.append(head)
 	return triggersrcs
@@ -536,7 +536,8 @@ def mkPostcohSPIIROnline(pipeline, detectors, banks, psd, psd_fft_length = 8, ht
 
 		# FIXME: hard-coded to do compression
 		if verbose:
-			postcoh = pipeparts.mkprogressreport(pipeline, postcoh, "progress_xml_dump")
+			postcoh = pipeparts.mkprogressreport(pipeline, postcoh, "progress_xml_dump_bank_%d" % i_dict)
+
 		head = mkpostcohfilesink(pipeline, postcoh, location = output_prefix[i_dict], compression = 1, snapshot_interval = snapshot_interval)
 		triggersrcs.append(head)
 	return triggersrcs
