a4f68c874d0601ba420b9da6176b2b3897f5f266
Yan Wang <yan.wang@ligo.org>
2015-11-09 11:20:45 +0800

    gstlal-inspiral/python/cbc_template_iir.py: set limit on the maximum number of iir filters to prevent too expensive compuational cost in extreme case, implement next pow of 2 as a function

--- gstlal-inspiral/python/cbc_template_iir.py
+++ gstlal-inspiral/python/cbc_template_iir.py
@@ -59,12 +59,21 @@ lsctables.use_in(DefaultContentHandler)
 class XMLContentHandler(ligolw.LIGOLWContentHandler):
 	pass
 
+def ceil_pow_2(x):
+	x = int(math.ceil(x))
+	x -= 1
+	n = 1
+	while n and (x & (x+1)):
+		x |= x >> n
+		n <<= 1
+	return x + 1
+
 def lefttukeywindow(data, samps = 200.):
-       assert (len(data) >= 2 * samps) # make sure that the user is requesting something sane
-       tp = float(samps) / len(data)
-       wn = lal.CreateTukeyREAL8Window(len(data), tp).data.data
-       wn[len(wn)//2:] = 1.0
-       return wn
+	assert (len(data) >= 2 * samps) # make sure that the user is requesting something sane
+	tp = float(samps) / len(data)
+	wn = lal.CreateTukeyREAL8Window(len(data), tp).data.data
+	wn[len(wn)//2:] = 1.0
+	return wn
 
 def Theta(eta, Mtot, t):
 	Tsun = lal.MTSUN_SI #4.925491e-6
@@ -267,7 +276,7 @@ def M_chi2_readline(flower=30., sampleRate=2048.):
 	length_max = working_f_low_extra_time * 10 * sampleRate
 
 	# Add 32 seconds to template length for PSD ringing, round up to power of 2 count of samples
-	working_length = templates.ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
+	working_length = ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
 	working_duration = float(working_length) / sampleRate
 	
 	amp, phase = gen_whitened_amp_phase(None, params[0], params[1], sampleRate, flower, 0, working_length, working_duration, length_max, params[3], params[4], params[5], params[6], params[7], params[8] )
@@ -296,7 +305,7 @@ def M_chi2(flower=30., sampleRate=2048.):
 		length_max = working_f_low_extra_time * 10 * sampleRate
 
 		# Add 32 seconds to template length for PSD ringing, round up to power of 2 count of samples
-		working_length = templates.ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
+		working_length = ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
 		working_duration = float(working_length) / sampleRate
 	
 		amp, phase = gen_whitened_amp_phase(None, params[ii, 0], params[ii, 1], sampleRate, flower, 0, working_length, working_duration, length_max, params[ii, 3], params[ii, 4], params[ii, 5], params[ii, 6], params[ii, 7], params[ii, 8] )
@@ -548,7 +557,6 @@ class Bank(object):
 		self.alpha = alpha
 		self.beta = beta
 
-		epsilon_increment = 0.001
 
 		if sampleRate is None:
 			sampleRate = int(2**(numpy.ceil(numpy.log2(fFinal)+1)))
@@ -599,7 +607,7 @@ class Bank(object):
 		length_max = working_f_low_extra_time * 10 * sampleRate
 
 		# Add 32 seconds to template length for PSD ringing, round up to power of 2 count of samples
-		working_length = templates.ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
+		working_length = ceil_pow_2(length_max + round((32.0 + working_f_low_extra_time) * sampleRate))
 		working_duration = float(working_length) / sampleRate
 
 		# Smooth the PSD and interpolate to required resolution
@@ -618,13 +626,14 @@ class Bank(object):
 		# waveforms by peak amplitude)
 		#
 
-	
 		original_epsilon = epsilon
+		epsilon_increment = 0.001
 	        for tmp, row in enumerate(sngl_inspiral_table):
 		    spiir_match = -1
 		    epsilon = original_epsilon
+		    n_filters = 0
 
-		    while(spiir_match < req_min_match and epsilon > 0):
+		    while(spiir_match < req_min_match and epsilon > 0 and n_filters < 2000):
 			m1 = row.mass1
 			m2 = row.mass2
 			
@@ -695,9 +704,10 @@ class Bank(object):
 
 		    self.matches.append(spiir_match)
 		    self.sigmasq.append(1.0 * norm_h / sampleRate)
+		    n_filters = len(a1)
 
 		    if verbose:
-			    logging.info("template %4.0d/%4.0d, m1 = %10.6f m2 = %10.6f, epsilon = %1.4f:  %4.0d filters, %10.8f match. original_eps = %1.4f: %4.0d filters, %10.8f match" % (tmp+1, len(sngl_inspiral_table), m1,m2, epsilon, len(a1), spiir_match, original_epsilon, original_filters, original_match))	
+			    logging.info("template %4.0d/%4.0d, m1 = %10.6f m2 = %10.6f, epsilon = %1.4f:  %4.0d filters, %10.8f match. original_eps = %1.4f: %4.0d filters, %10.8f match" % (tmp+1, len(sngl_inspiral_table), m1,m2, epsilon, n_filters, spiir_match, original_epsilon, original_filters, original_match))	
 		    # get the filter frequencies
 		    fs = -1. * numpy.angle(a1) / 2 / numpy.pi # Normalised freqeuncy
 		    a1dict = {}
