afccf06c2687fdd9bee6b5043612249ba7ff6d28
Kipp Cannon <kipp.cannon@ligo.org>
2016-03-01 11:31:29 +0900

    gstlal:  add some audio helper macros to library

--- gstlal/lib/gstlal/gstlal.h
+++ gstlal/lib/gstlal/gstlal.h
@@ -47,6 +47,25 @@ G_BEGIN_DECLS
 
 
 /*
+ * Add complex types to some audio macros
+ * FIXME:  remove as accepted upstream
+ */
+
+
+#define GST_AUDIO_FORMAT_INFO_IS_COMPLEX(info)	!!((info)->flags & GST_AUDIO_FORMAT_FLAG_COMPLEX)
+
+#define GSTLAL_AUDIO_FORMATS_ALL " { S8, U8, " \
+    "S16LE, S16BE, U16LE, U16BE, " \
+    "S24_32LE, S24_32BE, U24_32LE, U24_32BE, " \
+    "S32LE, S32BE, U32LE, U32BE, " \
+    "S24LE, S24BE, U24LE, U24BE, " \
+    "S20LE, S20BE, U20LE, U20BE, " \
+    "S18LE, S18BE, U18LE, U18BE, " \
+    "F32LE, F32BE, F64LE, F64BE, " \
+    "Z64LE, Z64BE, Z128LE, Z128BE }"
+
+
+/*
  * Function prototypes
  */
 
