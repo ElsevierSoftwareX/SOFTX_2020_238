f97d0dd7d7a5029a41fe3d918227a75f411792fd
Chad Hanna <crh184@psu.edu>
2016-03-02 21:44:26 -0500

    pipeio: more 1.0 porting

--- gstlal/python/pipeio.py
+++ gstlal/python/pipeio.py
@@ -31,11 +31,9 @@ import numpy
 import sys
 
 
-import pygtk
-pygtk.require("2.0")
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gst
+from gi.repository import GObject, Gst, GstAudio
 GObject.threads_init()
 Gst.init(None)
 
@@ -110,79 +108,56 @@ def get_unit_size(caps):
 
 
 def numpy_dtype_from_caps(caps):
-	struct = caps[0]
-	name = struct.get_name()
-	if name == "audio/x-raw":
-		assert struct["width"] % 8 == 0
-		return "f%d" % (struct["width"] // 8)
-	elif name == "audio/x-raw":
-		assert struct["width"] % 8 == 0
-		if struct["signed"]:
-			return "i%d" % (struct["width"] // 8)
-		else:
-			return "u%d" % (struct["width"] // 8)
-	elif name == "audio/x-raw-complex":
-		assert struct["width"] % 8 == 0
-		return "c%d" % (struct["width"] // 8)
-	raise ValueError(name)
+	formats_dict = {
+		GstAudio.AudioFormat.F32: numpy.dtype("float32"),
+		GstAudio.AudioFormat.F64: numpy.dtype("float64"),
+		GstAudio.AudioFormat.S8: numpy.dtype("int8"),
+		GstAudio.AudioFormat.U8: numpy.dtype("uint8"),
+		GstAudio.AudioFormat.S16: numpy.dtype("int16"),
+		GstAudio.AudioFormat.U16: numpy.dtype("uint16"),
+		GstAudio.AudioFormat.S32: numpy.dtype("int32"),
+		GstAudio.AudioFormat.U32: numpy.dtype("uint32")
+	}
+	info = GstAudio.AudioInfo()
+	info.from_caps(caps)
+	return formats_dict[info.finfo.format]
 
 
-def caps_from_numpy_dtype(dtype):
-	if dtype.char == 'f':
-		caps = Gst.Caps.from_string("audio/x-raw, width=32")
-	elif dtype.char == 'd':
-		caps = Gst.Caps.from_string("audio/x-raw, width=64")
-	elif dtype.char == 'b':
-		caps = Gst.Caps.from_string("audio/x-raw, width=8, signed=true")
-	elif dtype.char == 'B':
-		caps = Gst.Caps.from_string("audio/x-raw, width=8, signed=false")
-	elif dtype.char == 'h':
-		caps = Gst.Caps.from_string("audio/x-raw, width=16, signed=true")
-	elif dtype.char == 'H':
-		caps = Gst.Caps.from_string("audio/x-raw, width=16, signed=false")
-	elif dtype.char == 'i':
-		caps = Gst.Caps.from_string("audio/x-raw, width=32, signed=true")
-	elif dtype.char == 'I':
-		caps = Gst.Caps.from_string("audio/x-raw, width=32, signed=false")
-	elif dtype.char == 'l':
-		caps = Gst.Caps.from_string("audio/x-raw, width=64, signed=true")
-	elif dtype.char == 'L':
-		caps = Gst.Caps.from_string("audio/x-raw, width=64, signed=false")
-	else:
-		raise ValueError(dtype)
-	caps[0]["endianness"] = {
-		"=": 1234 if sys.byteorder == "little" else 4321,
-		"<": 1234,
-		">": 4321
-	}[dtype.byteorder]
-	return caps
+def format_string_from_numpy_dtype(dtype):
+	formats_dict = {
+		numpy.dtype("float32"): GstAudio.AudioFormat.F32,
+		numpy.dtype("float64"): GstAudio.AudioFormat.F64,
+		numpy.dtype("int8"): GstAudio.AudioFormat.S8,
+		numpy.dtype("uint8"): GstAudio.AudioFormat.U8,
+		numpy.dtype("int16"): GstAudio.AudioFormat.S16,
+		numpy.dtype("uint16"): GstAudio.AudioFormat.U16,
+		numpy.dtype("int32"): GstAudio.AudioFormat.S32,
+		numpy.dtype("uint32"): GstAudio.AudioFormat.U32
+	}
+	return GstAudio.AudioFormat.to_string(formats_dict[dtype])
 
 
 def caps_from_array(arr, rate = None):
-	caps = caps_from_numpy_dtype(arr.dtype)
-	if rate is not None:
-		caps[0]["rate"] = rate
-	caps[0]["channels"] = arr.shape[1]
-	return caps
+	return Gst.Caps.from_string("audio/x-raw, format=(string)%s, rate=(int)%d, channels=(int)%d, layout=(string)interleaved, channel-mask=(bitmask)0" % (format_string_from_numpy_dtype(arr.dtype), rate, arr.shape[1]))
 
 
-def array_from_audio_buffer(buf):
-	channels = buf.caps[0]["channels"]
+def array_from_audio_sample(sample):
+	caps = sample.get_caps()
+	bufmap = sample.get_buffer().map(Gst.MapFlags.READ)[1]
+	channels = caps[0]["channels"]
 	# FIXME:  conditional is work-around for broken handling of
 	# zero-length buffers in numpy < 1.7.  remove and use frombuffer()
 	# unconditionally when we can rely on numpy >= 1.7
-	if len(buf) > 0:
-		a = numpy.frombuffer(buf, dtype = numpy_dtype_from_caps(buf.caps))
+	if bufmap.size > 0:
+		a = numpy.frombuffer(bufmap.data, dtype = numpy_dtype_from_caps(caps))
 	else:
-		a = numpy.array((), dtype = numpy_dtype_from_caps(buf.caps))
-	a.shape = (len(a) // channels, channels)
-	return a
+		a = numpy.array((), dtype = numpy_dtype_from_caps(caps))
+	return numpy.reshape(a, (len(a) // channels, channels))
 
 
 def audio_buffer_from_array(arr, timestamp, offset, rate):
-	buf = Gst.Buffer(arr.data)
-	buf.caps = caps_from_array(arr, rate = rate)
-	buf.timestamp = timestamp
+	buf = Gst.Buffer.new_wrapped(numpy.getbuffer(arr))
+	buf.pts = timestamp
 	buf.duration = (Gst.SECOND * arr.shape[0] + rate // 2) // rate
 	buf.offset = offset
 	buf.offset_end = offset + arr.shape[0]
