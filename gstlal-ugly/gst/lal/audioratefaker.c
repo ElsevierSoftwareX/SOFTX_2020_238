1d18f971cb27360cbcea0fbe977ff97e0192458a
Kipp Cannon <kipp.cannon@ligo.org>
2016-03-03 12:09:14 +0900

    audioratefaker:  port to 1.x

--- gstlal-ugly/gst/lal/audioratefaker.c
+++ gstlal-ugly/gst/lal/audioratefaker.c
@@ -76,47 +76,22 @@ static gboolean do_new_segment(GstAudioRateFaker *element)
 	gboolean success = TRUE;
 
 	if(element->last_segment) {
+		GstSegment segment;
 
-        /* 1.0 code gst_event_copy_segment doesn't exist
-		GstSegment *segment;
 		gst_event_copy_segment(element->last_segment, &segment);
-		*/
-        gboolean update;
-        gdouble rate;
-        GstFormat format;
-        gint64 start, stop, position;
-        gst_event_parse_new_segment(element->last_segment, &update, &rate, &format, &start, &stop, &position);
 
-        /* 1.0 code
-		if(segment->format == GST_FORMAT_TIME) {
-            fprintf(stderr, "updating last segment\n");
-			if(GST_CLOCK_TIME_IS_VALID(segment->start))
-				segment->start = gst_util_uint64_scale_int_round(segment->start, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-			if(GST_CLOCK_TIME_IS_VALID(segment->stop))
-				segment->stop = gst_util_uint64_scale_int_round(segment->stop, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
+		if(segment.format == GST_FORMAT_TIME) {
+			if(GST_CLOCK_TIME_IS_VALID(segment.start))
+				segment.start = gst_util_uint64_scale_int_round(segment.start, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
+			if(GST_CLOCK_TIME_IS_VALID(segment.stop))
+				segment.stop = gst_util_uint64_scale_int_round(segment.stop, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
 			if(GST_CLOCK_TIME_IS_VALID(segment.position))
 				segment.position = gst_util_uint64_scale_int_round(segment.position, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-
-			success = gst_pad_push_event(GST_BASE_TRANSFORM_SRC_PAD(element), gst_event_new_segment(segment));
-        */
-		if(format == GST_FORMAT_TIME) {
-			if(GST_CLOCK_TIME_IS_VALID(start))
-				start = gst_util_uint64_scale_int_round(start, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-			if(GST_CLOCK_TIME_IS_VALID(stop))
-				stop = gst_util_uint64_scale_int_round(stop, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-			if(GST_CLOCK_TIME_IS_VALID(position))
-				position = gst_util_uint64_scale_int_round(position, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-
-			success = gst_pad_push_event(GST_BASE_TRANSFORM_SRC_PAD(element), gst_event_new_new_segment(update, rate, format, start, stop, position));
+			success = gst_pad_push_event(GST_BASE_TRANSFORM_SRC_PAD(element), gst_event_new_segment(&segment));
 		} else {
 			gst_event_ref(element->last_segment);
 			success = gst_pad_push_event(GST_BASE_TRANSFORM_SRC_PAD(element), element->last_segment);
 		}
-        /* 1.0 Code
-        * copy_segment requires free_segment
-        g_free(segment);
-        gst_segment_free(segment);
-        */
 
 		element->need_new_segment = FALSE;
 	}
@@ -134,18 +109,12 @@ static gboolean do_new_segment(GstAudioRateFaker *element)
  */
 
 
-/* 1.0 code: transform_caps has additional *filter argument
 static GstCaps *transform_caps(GstBaseTransform *trans, GstPadDirection direction, GstCaps *caps, GstCaps *filter)
- */
-static GstCaps *transform_caps(GstBaseTransform *trans, GstPadDirection direction, GstCaps *caps)
 {
 	GstCaps *othercaps = NULL;
 	guint i;
 
-	/* 1.0 code
-    GST_DEBUG_OBJECT(trans, "transforming %" GST_PTR_FORMAT " on %s pad with filter %" GST_PTR_FORMAT, caps, direction == GST_PAD_SRC ? GST_BASE_TRANSFORM_SRC_NAME : GST_BASE_TRANSFORM_SINK_NAME, filter);
-    */
-    GST_DEBUG_OBJECT(trans, "transforming %" GST_PTR_FORMAT " on %s pad" GST_PTR_FORMAT, caps, direction == GST_PAD_SRC ? GST_BASE_TRANSFORM_SRC_NAME : GST_BASE_TRANSFORM_SINK_NAME);
+	GST_DEBUG_OBJECT(trans, "transforming %" GST_PTR_FORMAT " on %s pad with filter %" GST_PTR_FORMAT, caps, direction == GST_PAD_SRC ? GST_BASE_TRANSFORM_SRC_NAME : GST_BASE_TRANSFORM_SINK_NAME, filter);
 
 	/*
 	 * sink and source pads must have same channel count and sample
@@ -166,19 +135,19 @@ static GstCaps *transform_caps(GstBaseTransform *trans, GstPadDirection directio
 	/* intersect that result with the caps allowed by the pad template.
 	 * this repopulates the rate elements with the allowed ranges */
 	switch(direction) {
-	case GST_PAD_SRC:
-        /* 1.0 code
-		othercaps = gst_caps_intersect_full(caps, gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SINK_PAD(trans)), GST_CAPS_INTERSECT_FIRST);
-        */
-		othercaps = gst_caps_intersect(caps, gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SINK_PAD(trans)));
+	case GST_PAD_SRC: {
+		GstCaps *tmpltcaps = gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SINK_PAD(trans));
+		othercaps = gst_caps_intersect_full(caps, tmpltcaps, GST_CAPS_INTERSECT_FIRST);
+		gst_caps_unref(tmpltcaps);
 		break;
+	}
 
-	case GST_PAD_SINK:
-        /* 1.0 code
-		othercaps = gst_caps_intersect_full(caps, gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SRC_PAD(trans)), GST_CAPS_INTERSECT_FIRST);
-        */
-		othercaps = gst_caps_intersect(caps, gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SRC_PAD(trans)));
+	case GST_PAD_SINK: {
+		GstCaps *tmpltcaps = gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SRC_PAD(trans));
+		othercaps = gst_caps_intersect_full(caps, tmpltcaps, GST_CAPS_INTERSECT_FIRST);
+		gst_caps_unref(tmpltcaps);
 		break;
+	}
 
 	default:
 		g_assert_not_reached();
@@ -189,25 +158,19 @@ static GstCaps *transform_caps(GstBaseTransform *trans, GstPadDirection directio
 	}
 	gst_caps_unref(caps);
 
-    /* 1.0 code
 	if(filter) {
 		caps = othercaps;
-        // 1.0 code
 		othercaps = gst_caps_intersect_full(filter, othercaps, GST_CAPS_INTERSECT_FIRST);
-		othercaps = gst_caps_intersect(filter, othercaps);
 		gst_caps_unref(caps);
 	}
-    */
 
 	GST_DEBUG_OBJECT(trans, "transformed to %" GST_PTR_FORMAT, othercaps);
 
 	return othercaps;
 }
 
-/* 1.0 code: fixate_caps is a void return signature
+
 static GstCaps *fixate_caps(GstBaseTransform *trans, GstPadDirection direction, GstCaps *caps, GstCaps *othercaps)
-*/
-static void fixate_caps(GstBaseTransform *trans, GstPadDirection direction, GstCaps *caps, GstCaps *othercaps)
 {
 	GstStructure *s;
 	gint rate_num, rate_den = 1;
@@ -221,10 +184,7 @@ static void fixate_caps(GstBaseTransform *trans, GstPadDirection direction, GstC
 			goto done;
 		}
 
-    /* 1.0 code -- return value is void
-	othercaps = gst_caps_truncate(othercaps);	// does this leak memory? //
-    */
-	gst_caps_truncate(othercaps);
+	othercaps = gst_caps_truncate(othercaps);	/* does this leak memory? */
 	s = gst_caps_get_structure(othercaps, 0);
 	if(gst_structure_has_field_typed(s, "rate", G_TYPE_INT))
 		gst_structure_fixate_field_nearest_int(s, "rate", rate_den == 1 ? rate_num : (int) round((double) rate_num / rate_den));
@@ -232,10 +192,7 @@ static void fixate_caps(GstBaseTransform *trans, GstPadDirection direction, GstC
 		gst_structure_fixate_field_nearest_fraction(s, "rate", rate_num, rate_den);
 
 done:
-    return;
-    /* 1.0 code: fixate_caps is a void return signature
 	return othercaps;
-    */
 }
 
 
@@ -272,10 +229,7 @@ static gboolean sink_event(GstBaseTransform *trans, GstEvent *event)
 	gboolean success = TRUE;
 
 	switch(GST_EVENT_TYPE(event)) {
-    /* 1.0 code -- no GST_EVENT_NEWSEGMENT -> GST_EVENT_SEGMENT
 	case GST_EVENT_SEGMENT:
-    */
-	case GST_EVENT_NEWSEGMENT:
 		if(element->last_segment)
 			gst_event_unref(element->last_segment);
 		element->last_segment = event;
@@ -283,11 +237,7 @@ static gboolean sink_event(GstBaseTransform *trans, GstEvent *event)
 		break;
 
 	default:
-        /* 1.0 code -- GstBaseTransform has no sink_event function
-         * sink_event replaces event in 1.0
 		success = GST_BASE_TRANSFORM_CLASS(gst_audio_rate_faker_parent_class)->sink_event(trans, event);
-        */
-		success = GST_BASE_TRANSFORM_CLASS(gst_audio_rate_faker_parent_class)->event(trans, event);
 		break;
 	}
 
@@ -303,37 +253,18 @@ static GstFlowReturn transform_ip(GstBaseTransform *trans, GstBuffer *buf)
 	if(element->need_new_segment)
 		do_new_segment(element);
 
-    /* 1.0 code -- GST_BUFFER_TIMESTAMP -> GST_BUFFER_PTS */
-    /*
 	if(GST_BUFFER_PTS_IS_VALID(buf) && GST_BUFFER_DURATION_IS_VALID(buf)) {
 		GstClockTime timestamp = GST_BUFFER_PTS(buf);
-    */
-	if(GST_BUFFER_TIMESTAMP_IS_VALID(buf) && GST_BUFFER_DURATION_IS_VALID(buf)) {
-		GstClockTime timestamp = GST_BUFFER_TIMESTAMP(buf);
 		GstClockTime duration = GST_BUFFER_DURATION(buf);
 
-        /*
 		GST_BUFFER_PTS(buf) = gst_util_uint64_scale_int_round(timestamp, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-        */
-		GST_BUFFER_TIMESTAMP(buf) = gst_util_uint64_scale_int_round(timestamp, element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-
-        /*
 		GST_BUFFER_DURATION(buf) = gst_util_uint64_scale_int_round(timestamp + duration, element->inrate_over_outrate_num, element->inrate_over_outrate_den) - GST_BUFFER_PTS(buf);
-        */
-		GST_BUFFER_DURATION(buf) = gst_util_uint64_scale_int_round(timestamp + duration, element->inrate_over_outrate_num, element->inrate_over_outrate_den) - GST_BUFFER_TIMESTAMP(buf);
-    /*
 	} else if(GST_BUFFER_PTS_IS_VALID(buf))
 		GST_BUFFER_PTS(buf) = gst_util_uint64_scale_int_round(GST_BUFFER_PTS(buf), element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-    */
-	} else if(GST_BUFFER_TIMESTAMP_IS_VALID(buf)) {
-		GST_BUFFER_TIMESTAMP(buf) = gst_util_uint64_scale_int_round(GST_BUFFER_TIMESTAMP(buf), element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-	} else if(GST_BUFFER_DURATION_IS_VALID(buf)) {
+	else if(GST_BUFFER_DURATION_IS_VALID(buf))
 		GST_BUFFER_DURATION(buf) = gst_util_uint64_scale_int_round(GST_BUFFER_DURATION(buf), element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-    }
-    /* 1.0 code -- DTS not yet available
 	if(GST_BUFFER_DTS_IS_VALID(buf))
 		GST_BUFFER_DTS(buf) = gst_util_uint64_scale_int_round(GST_BUFFER_DTS(buf), element->inrate_over_outrate_num, element->inrate_over_outrate_den);
-    */
 
 	return result;
 }
@@ -352,11 +283,8 @@ static void finalize(GObject *object)
 {
 	GstAudioRateFaker *element = GST_AUDIO_RATE_FAKER(object);
 
-	/*
-	 * This will throw up an error -- I don't think we own this object in 0.10
 	if(element->last_segment)
 		gst_event_unref(element->last_segment);
-	*/
 	element->last_segment = NULL;
 
 	/*
@@ -366,28 +294,22 @@ static void finalize(GObject *object)
 	G_OBJECT_CLASS(gst_audio_rate_faker_parent_class)->finalize(object);
 }
 
+
 static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE(
 	GST_BASE_TRANSFORM_SINK_NAME,
 	GST_PAD_SINK,
 	GST_PAD_ALWAYS,
-    /* 1.0 code: Don't know how to match up with Kipp's caps decription
-    */
 	GST_STATIC_CAPS(
-		"audio/x-raw-int, " \
-            "width = (int) {8, 16, 32, 64}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]; " \
-		"audio/x-raw-float, " \
-            "width = (int) {8, 16, 32, 64}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]; " \
-		"audio/x-raw-complex, " \
-            "width = (int) {8, 16, 32, 64, 128}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]" \
+		"audio/x-raw, " \
+			"format = " GST_AUDIO_FORMATS_ALL ", " \
+			"channels = " GST_AUDIO_CHANNELS_RANGE ", " \
+			"rate = (int) [1, MAX], " \
+			"layout = (string) interleaved; " \
+		"audio/x-raw, " \
+			"format = " GST_AUDIO_FORMATS_ALL ", " \
+			"channels = " GST_AUDIO_CHANNELS_RANGE ", " \
+			"rate = (fraction) [0/1, MAX], " \
+			"layout = (string) interleaved"
 	)
 );
 
@@ -397,21 +319,16 @@ static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE(
 	GST_PAD_SRC,
 	GST_PAD_ALWAYS,
 	GST_STATIC_CAPS(
-		"audio/x-raw-int, " \
-            "width = (int) {8, 16, 32, 64}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]; " \
-		"audio/x-raw-float, " \
-            "width = (int) {8, 16, 32, 64}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]; " \
-		"audio/x-raw-complex, " \
-            "width = (int) {8, 16, 32, 64, 128}, " \
-            "endianess = (int) BYTE_ORDER, " \
-			"channels = (int) [1, MAX], " \
-			"rate = (int) [1, MAX]" \
+		"audio/x-raw, " \
+			"format = " GST_AUDIO_FORMATS_ALL ", " \
+			"channels = " GST_AUDIO_CHANNELS_RANGE ", " \
+			"rate = (int) [1, MAX], " \
+			"layout = (string) interleaved; " \
+		"audio/x-raw, " \
+			"format = " GST_AUDIO_FORMATS_ALL ", " \
+			"channels = " GST_AUDIO_CHANNELS_RANGE ", " \
+			"rate = (fraction) [0/1, MAX], " \
+			"layout = (string) interleaved"
 	)
 );
 
@@ -427,15 +344,11 @@ static void gst_audio_rate_faker_class_init(GstAudioRateFakerClass *klass)
 	transform_class->transform_caps = GST_DEBUG_FUNCPTR(transform_caps);
 	transform_class->fixate_caps = GST_DEBUG_FUNCPTR(fixate_caps);
 	transform_class->set_caps = GST_DEBUG_FUNCPTR(set_caps);
-    /* 1.0 code: class has no sink_event member 
-     * sink_event replaced event in 1.0
 	transform_class->sink_event = GST_DEBUG_FUNCPTR(sink_event);
-    */
-	transform_class->event = GST_DEBUG_FUNCPTR(sink_event);
 	transform_class->transform_ip = GST_DEBUG_FUNCPTR(transform_ip);
 	transform_class->passthrough_on_same_caps = TRUE;
 
-	gst_element_class_set_details_simple(element_class, 
+	gst_element_class_set_details_simple(element_class,
 		"Audio rate faker",
 		"Filter/Audio",
 		"Adjusts segments and audio buffer metadata to assign a new sample rate.  Allows input and/or output streams to have rational sample rates.",
