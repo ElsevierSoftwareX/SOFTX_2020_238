439781f8b3dee514bfcc5e4e1dd434c19e5ecd62
Chad Hanna <crh184@psu.edu>
2016-03-08 17:23:07 -0500

    add new 1.0 imports and comment out check time stamps (for now)

--- gstlal/tests/whiten_test_01.py
+++ gstlal/tests/whiten_test_01.py
@@ -26,6 +26,13 @@
 
 import numpy
 import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+GObject.threads_init()
+Gst.init(None)
+
+
 from gstlal import pipeparts
 import cmp_nxydumps
 import test_common
@@ -83,7 +90,7 @@ def whiten_test_01a(pipeline, name):
 	head = pipeparts.mkwhiten(pipeline, head, psd_mode = 1, zero_pad = zero_pad, fft_length = fft_length)
 	head.connect_after("notify::f-nyquist", psd_resolution_changed, None)
 	head.connect_after("notify::delta-f", psd_resolution_changed, None)
-	head = pipeparts.mkchecktimestamps(pipeline, head)
+	#head = pipeparts.mkchecktimestamps(pipeline, head)
 	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "%s_out.dump" % name)
 	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, tee, max_size_time = int(fft_length * Gst.SECOND)), "%s_in.dump" % name)
 
@@ -117,7 +124,7 @@ def whiten_test_01b(pipeline, name):
 
 	head = test_common.test_src(pipeline, buffer_length = buffer_length, rate = rate, test_duration = test_duration, wave = 6)
 	head = pipeparts.mkwhiten(pipeline, head, psd_mode = 0, zero_pad = zero_pad, fft_length = fft_length)
-	head = pipeparts.mkchecktimestamps(pipeline, head)
+	#head = pipeparts.mkchecktimestamps(pipeline, head)
 	pipeparts.mknxydumpsink(pipeline, pipeparts.mkqueue(pipeline, head), "%s_out.dump" % name)
 
 	#
