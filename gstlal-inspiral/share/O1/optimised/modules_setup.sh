0a7d6b8a9d5987ee5b93bd223e6200fb27f4418f
Duncan Meacher <duncan.meacher@ligo.org>
2016-03-09 16:26:50 -0600

    icc optimisation Makefiles for uwm

---
+++ gstlal-inspiral/share/O1/optimised/modules_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# cit: not needed
+
+# uwm
+source /opt/intel/bin/compilervars.sh intel64
+# atlas
+#source /opt/intel/2015/intel.sh
+
+module use /home/gstlalcbc/modules/modulefiles
+module load gstlal-opt-icc
+
+module list
+
