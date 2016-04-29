cf1b7832588c7d3e190968f90bbdcc793d5507f3
Kipp Cannon <kipp.cannon@ligo.org>
2016-04-29 03:10:43 -0700

    lvshmsinksrc_test_01.sh:  fix gst-launch name

--- gstlal-ugly/tests/lvshmsinksrc_test_01.sh
+++ gstlal-ugly/tests/lvshmsinksrc_test_01.sh
@@ -3,7 +3,7 @@
 smname="gstlal-test"
 
 pass_through () {
-	gst-launch --quiet fdsrc fd=0 ! gds_lvshmsink buffer-mode=1 shm-name=$smname gds_lvshmsrc shm-name=$smname num-buffers=1 ! fdsink fd=1 sync=false async=false
+	gst-launch-1.0 --quiet fdsrc fd=0 ! gds_lvshmsink buffer-mode=1 shm-name=$smname gds_lvshmsrc shm-name=$smname num-buffers=1 ! fdsink fd=1 sync=false async=false
 	smkill $smname 2>/dev/null
 }
 
