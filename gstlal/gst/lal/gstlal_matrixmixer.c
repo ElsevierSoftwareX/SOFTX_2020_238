7e9bfe7de2def7f8e7074866482a2367adb9ecfa
Kipp Cannon <kipp.cannon@ligo.org>
2016-05-12 18:53:58 +0900

    lal_matrixmixer:  typo

--- gstlal/gst/lal/gstlal_matrixmixer.c
+++ gstlal/gst/lal/gstlal_matrixmixer.c
@@ -628,7 +628,7 @@ static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE(
 		"rate = " GST_AUDIO_RATE_RANGE ", " \
 		"channels = " GST_AUDIO_CHANNELS_RANGE ", " \
 		"format = (string) {" GST_AUDIO_NE(F32) ", " GST_AUDIO_NE(F64) ", " GST_AUDIO_NE(Z64) ", " GST_AUDIO_NE(Z128) "}, " \
-		"layout = (string) interleaved, "
+		"layout = (string) interleaved, " \
 		"channel-mask = (bitmask) 0"
 	)
 );
