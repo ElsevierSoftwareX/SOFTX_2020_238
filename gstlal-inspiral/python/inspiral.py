391040394ffec6cd14ab9c49f4d81e8dc53aa11f
Duncan Meacher <duncan.meacher@ligo.org>
2016-09-29 13:08:36 -0700

    Removed function that are contained within datasource.py

--- gstlal-inspiral/python/inspiral.py
+++ gstlal-inspiral/python/inspiral.py
@@ -123,38 +123,6 @@ def message_new_checkpoint(src, timestamp = None):
 	return message
 
 
-def channel_dict_from_channel_list(channel_list, channel_dict = {"H1" : "LSC-STRAIN", "H2" : "LSC-STRAIN", "L1" : "LSC-STRAIN", "V1" : "LSC-STRAIN"}):
-	"""
-	given a list of channels like this ["H1=LSC-STRAIN",
-	H2="SOMETHING-ELSE"] produce a dictionary keyed by ifo of channel
-	names.  The default values are LSC-STRAIN for all detectors
-	"""
-
-	for channel in channel_list:
-		ifo = channel.split("=")[0]
-		chan = "".join(channel.split("=")[1:])
-		channel_dict[ifo] = chan
-
-	return channel_dict
-
-
-def pipeline_channel_list_from_channel_dict(channel_dict, opt = "channel-name"):
-	"""
-	produce a string of channel name arguments suitable for a pipeline.py
-	program that doesn't technically allow multiple options. For example
-	--channel-name=H1=LSC-STRAIN --channel-name=H2=LSC-STRAIN
-	"""
-
-	outstr = ""
-	for i, ifo in enumerate(channel_dict):
-		if i == 0:
-			outstr += "%s=%s " % (ifo, channel_dict[ifo])
-		else:
-			outstr += "--%s=%s=%s " % (opt, ifo, channel_dict[ifo])
-
-	return outstr
-
-
 def state_vector_on_off_dict_from_bit_lists(on_bit_list, off_bit_list, state_vector_on_off_dict = {"H1" : [0x7, 0x160], "L1" : [0x7, 0x160], "V1" : [0x67, 0x100]}):
 	"""
 	"""
