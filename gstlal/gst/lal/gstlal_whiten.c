a0030ae1aba68ca6cd9b7f443da28b47baa6f192
Kipp Cannon <kipp.cannon@ligo.org>
2016-10-17 12:08:32 +0900

    gstlal_whiten.c: update copyright

--- gstlal/gst/lal/gstlal_whiten.c
+++ gstlal/gst/lal/gstlal_whiten.c
@@ -1,7 +1,7 @@
 /*
  * PSD Estimation and whitener
  *
- * Copyright (C) 2008-2015  Kipp Cannon, Chad Hanna, Drew Keppel
+ * Copyright (C) 2008-2016  Kipp Cannon, Chad Hanna, Drew Keppel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
