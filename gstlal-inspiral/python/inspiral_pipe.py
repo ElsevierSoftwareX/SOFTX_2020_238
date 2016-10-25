358320907e3eeb92eb5cf46083ed3f8473471234
Cody Messick <cody.messick@ligo.org>
2016-10-24 20:04:27 -0500

    Reworked offline inspiral dag organization. With few exceptions, files produced by prog will now be stored in prog/XXXXX/YYYY, where XXXXX are the first 5 digits of the GPS time and YYYY is the mass bin.

--- gstlal-inspiral/python/inspiral_pipe.py
+++ gstlal-inspiral/python/inspiral_pipe.py
@@ -251,7 +251,7 @@ class generic_node(InspiralNode):
 		for opt, val in input_cache_files.items():
 			cache_entries = [lal.CacheEntry.from_T050017("file://localhost%s" % os.path.abspath(filename)) for filename in val]
 			if input_cache_file_name is None:
-				cache_file_name = group_T050017_filename_from_T050017_files(cache_entries, '.cache', path = job.tag_base)
+				cache_file_name = group_T050017_filename_from_T050017_files(cache_entries, '.cache', path = os.path.join(job.tag_base, 'cache'))
 			else:
 				cache_file_name = input_cache_file_name
 			with open(cache_file_name, "w") as cache_file:
@@ -262,7 +262,7 @@ class generic_node(InspiralNode):
 
 		for opt, val in output_cache_files.items():
 			cache_entries = [lal.CacheEntry.from_T050017("file://localhost%s" % os.path.abspath(filename)) for filename in val]
-			cache_file_name = group_T050017_filename_from_T050017_files(cache_entries, '.cache', path = job.tag_base)
+			cache_file_name = group_T050017_filename_from_T050017_files(cache_entries, '.cache', path = os.path.join(job.tag_base, 'cache'))
 			with open(cache_file_name, "w") as cache_file:
 				lal.Cache(cache_entries).tofile(cache_file)
 			self.add_var_opt(opt, cache_file_name)
