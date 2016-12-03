116960334d3ec36caed5d8625185cfa35abab8a3
Aaron Viets <aaron.viets@ligo.org>
2016-12-03 09:16:14 -0600

    gstlal_compute_strain:  Removed audiocheblimit element from pipeline and used lal_firbank in its place (audiocheblimit appears to have a bug)

--- gstlal-calibration/python/calibration_parts.py
+++ gstlal-calibration/python/calibration_parts.py
@@ -260,31 +260,32 @@ def split_into_real(pipeline, complex_chan):
 	pipeparts.src_deferred_link(elem, "src_1", imag.get_static_pad("sink"))
 	return real, imag
 
-def demodulate(pipeline, head, freq, td, caps):
+def demodulate(pipeline, head, freq, td, caps, integration_samples):
 	# demodulate input at a given frequency freq
 
 	head = pipeparts.mkgeneric(pipeline, head, "lal_demodulate", line_frequency = freq)
-	head = pipeparts.mktogglecomplex(pipeline, head)
-	head = pipeparts.mkresample(pipeline, head)
-	head = pipeparts.mkcapsfilter(pipeline, head, caps)
-	head = pipeparts.mkgeneric(pipeline, head, "audiocheblimit", cutoff = 0.05)
-	head = pipeparts.mktogglecomplex(pipeline, head)
-
-	""""
-	headR, headI = split_into_real(pipeline, headtee)
+	headR, headI = split_into_real(pipeline, head)
 	headR = pipeparts.mkresample(pipeline, headR)
-	headR = pipeparts.mkcapsfilter(pipeline, headR, "audio/x-raw, rate=512")
-	headR = pipeparts.mkaudioamplify(pipeline, headR, amplification = 1.0/16384.0);
-	headR = pipeparts.mkfirbank(pipeline, headR, fir_matrix=[numpy.hanning(integration_samples+1)], time_domain = td)
+	headR = pipeparts.mkcapsfilter(pipeline, headR, caps)
+	headR = pipeparts.mkfirbank(pipeline, headR, fir_matrix=[numpy.hanning(integration_samples + 1)], time_domain = td)
 	headI = pipeparts.mkresample(pipeline, headI)
-	headI = pipeparts.mkcapsfilter(pipeline, headI, "audio/x-raw, rate=512")
-	headI = pipeparts.mkaudioamplify(pipeline, headI, amplification = -1.0/16384.0);
-	headI = pipeparts.mkfirbank(pipeline, headI, fir_matrix=[numpy.hanning(integration_samples+1)], time_domain = td)
-	#pipeparts.mknxydumpsink(pipeline, headR, "real_new.txt")
-	#pipeparts.mknxydumpsink(pipeline, headI, "imag_new.txt")
-	head2 = merge_into_complex(pipeline, headR, headI)
-	pipeparts.mknxydumpsink(pipeline, head2, "old_method_demod.dump")
-	"""
+        headI = pipeparts.mkcapsfilter(pipeline, headI, caps)
+	headI = pipeparts.mkfirbank(pipeline, headI, fir_matrix=[numpy.hanning(integration_samples + 1)], time_domain = td)
+	head = merge_into_complex(pipeline, headR, headI)
+
+	# headR, headI = split_into_real(pipeline, headtee)
+	# headR = pipeparts.mkresample(pipeline, headR)
+	# headR = pipeparts.mkcapsfilter(pipeline, headR, "audio/x-raw, rate=512")
+	# headR = pipeparts.mkaudioamplify(pipeline, headR, amplification = 1.0/16384.0);
+	# headR = pipeparts.mkfirbank(pipeline, headR, fir_matrix=[numpy.hanning(integration_samples+1)], time_domain = td)
+	# headI = pipeparts.mkresample(pipeline, headI)
+	# headI = pipeparts.mkcapsfilter(pipeline, headI, "audio/x-raw, rate=512")
+	# headI = pipeparts.mkaudioamplify(pipeline, headI, amplification = -1.0/16384.0);
+	# headI = pipeparts.mkfirbank(pipeline, headI, fir_matrix=[numpy.hanning(integration_samples+1)], time_domain = td)
+	# #pipeparts.mknxydumpsink(pipeline, headR, "real_new.txt")
+	# #pipeparts.mknxydumpsink(pipeline, headI, "imag_new.txt")
+	# head2 = merge_into_complex(pipeline, headR, headI)
+	# pipeparts.mknxydumpsink(pipeline, head2, "old_method_demod.dump")
 
 	return head
 
