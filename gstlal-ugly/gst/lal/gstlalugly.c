1c9ba9f1ccf1812065f75ac8b0ac6f8b9bc4c44c
Leo Tsukada <tsukada@resceu.s.u-tokyo.ac.jp>
2017-01-25 16:54:29 +0900

    gstlal-ugly: add lal_tdwhiten element

--- gstlal-ugly/gst/lal/gstlalugly.c
+++ gstlal-ugly/gst/lal/gstlalugly.c
@@ -52,8 +52,8 @@
 #include <gstlal/gstlal_tags.h>
 #include <gstlal_iirbank.h>
 #include <gstlal_interpolator.h>
-/*#include <gstlal_mean.h>
-#include <gstlal_specgram.h>
+#include <gstlal_tdwhiten.h>
+/*#include <gstlal_specgram.h>
 #include <gstlal_pad.h>
 #include <gstlal_trim.h>*/
 #include <gstlal_bitvectorgen.h>
@@ -78,7 +78,7 @@ static gboolean plugin_init(GstPlugin *plugin)
 	} *element, elements[] = {
 		{"lal_iirbank", GSTLAL_IIRBANK_TYPE},
 		{"lal_interpolator", GSTLAL_INTERPOLATOR_TYPE},
-		/*{"lal_mean", GSTLAL_MEAN_TYPE},*/
+		{"lal_tdwhiten", GSTLAL_TDWHITEN_TYPE},
 		/*{"lal_specgram", GSTLAL_SPECGRAM_TYPE},*/
 		/*{"lal_pad", GST_TYPE_LALPAD},
 		{"lal_trim", GST_TYPE_LALTRIM},*/
