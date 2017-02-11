ElsevierSoftwareX/SOFTX_2020_238

commit 7400de21570c4232f6b0b74b546fad51286db6bc

far.py: fix issues related to t_offset in coinc_params
- remove the FIXME claiming t_offset doesn't work for more two detectors.  this code works for more than two detectors, it's the likelihood ratio that doesn't work
- t_offset was being computed inside the mode="ranking"-only path of coinc_params, move out of conditional
- change choice of reference time so that singles always have an "offset" of 0.
- fix comment describing choice of offset

gstlal-inspiral/python/far.py
@@ -217,23 +217,23 @@ class ThincaCoincParamsDistributions(snglcoinc.CoincParamsDistributions):
 			if len(events) < self.min_instruments:
 				raise ValueError("candidates require >= %d events in ranking mode" % self.min_instruments)
 			params["instruments"] = (frozenset(event.ifo for event in events),)
-			# FIXME This doesnt work for more tan two detectors
-			zero_offset = min((event.ifo, event.end) for event in events)[-1]
-			params.t_offset = dict((event.ifo, float(event.end - zero_offset) + offsetvector[event.ifo]) for event in events)
 		elif mode == "counting":
 			if len(events) != 1:
 				raise ValueError("only singles are allowed in counting mode")
 		else:
 			raise ValueError("invalid mode '%s'" % mode)
 
-		# record coa_phase and offset from end_time, where offset is
-		# the difference between a sngl inspiral's end time and the
-		# coinc inspiral's end time. Since the coinc inspiral's end
-		# time is just the end time of the first sngl inspiral
-		# alphabetically, non-coinc sngl inspirals will have offsets of
-		# 0, and the first sngl inspiral in a coinc will have an offset
-		# of 0
+		#
+		# record coa_phase and offset from epoch.  the epoch is
+		# chosen to be the time-slid end-time of the 0th trigger.
+		# the objective here is to allow the time-shifted end times
+		# to be converted to floats without loss of precision in
+		# such a way that singles always have an offset of 0.
+		#
+
 		params.coa_phase = dict((event.ifo, event.coa_phase) for event in events)
+		ref, ref_offset = events[0].end, offsetvector[events[0].ifo]
+		params.t_offset = dict((event.ifo, float(event.end - ref) + offsetvector[event.ifo] - ref_offset) for event in events)
 
 		#
 		# record the horizon distances.  pick one trigger at random

commit 2f6d9acebb0d1630774c579471beb717050bbea5

far.py:  fix bug introduced in 7400de21570c4232f6b0b74b546fad51286db6bc
- in "counting" mode no offsetvector is supplied.  this is probably why the
  t_offset stuff was inside the non-counting conditional, but it's tidier
  to always compute the same parameters in both counting and ranking mode,
  so the better approach is to construct a place-holder offset vector

gstlal-inspiral/python/far.py
@@ -220,6 +220,11 @@ class ThincaCoincParamsDistributions(snglcoinc.CoincParamsDistributions):
 		elif mode == "counting":
 			if len(events) != 1:
 				raise ValueError("only singles are allowed in counting mode")
+			# we don't require an offsetvector in counting mode
+			# because that would be nonsensical, but we need
+			# something for the code below to be happy
+			if offsetvector is None:
+				offsetvector = {events[0].ifo: 0.0}
 		else:
 			raise ValueError("invalid mode '%s'" % mode)
 