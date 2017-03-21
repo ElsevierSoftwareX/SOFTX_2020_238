9f64769e9b7ff9d6a9e7e561b896a6b379e87d68
Duncan Meacher <duncan.meacher@ligo.org>
2017-03-21 10:01:51 -0700

    ugly/test/trigger_test_01a.py: Updated test code

--- gstlal-ugly/tests/trigger_test_01.py
+++ gstlal-ugly/tests/trigger_test_01.py
@@ -20,6 +20,7 @@ gi.require_version('Gst', '1.0')
 from gi.repository import GObject
 from gi.repository import Gst
 
+
 ####################
 # 
 #      classes
@@ -30,7 +31,7 @@ from gi.repository import Gst
 class MultiChannelHandler(simplehandler.Handler):
 	def __init__(self, *args, **kwargs):
 		self.output = kwargs.pop("output")
-		#self.instrument = kwargs.pop("instrument")
+		self.instrument = kwargs.pop("instrument")
 		super(MultiChannelHandler, self).__init__(*args, **kwargs)
 		self.timedeq = deque(maxlen = 10000)
 
@@ -42,18 +43,20 @@ class MultiChannelHandler(simplehandler.Handler):
 		del buf
 		return Gst.FlowReturn.OK
 
-	def bufhandler(self, elem, sink_dict):
+	#def bufhandler(self, elem, sink_dict):
+	def bufhandler(self, elem):
 		buf = elem.emit("pull-sample").get_buffer()
 		buftime = int(buf.pts / 1e9)
 		(result, mapinfo) = buf.map(Gst.MapFlags.READ)
 		assert result
 		if mapinfo.data:
 			data = StringIO.StringIO(mapinfo.data).getvalue()
-			channel = sink_dict[elem]
+			#channel = sink_dict[elem]
 			fdata = ""
 			for line in data.split('\n'):
 				if len(line) > 0:
-					fdata += "%s\t" % channel + line.rstrip('\r') + "\n"
+					#fdata += "%s\t" % channel + line.rstrip('\r') + "\n"
+					fdata += line.rstrip('\r') + "\n"
 			self.timedeq.append(buftime)
 		else:
 			buf.unmap(mapinfo)
@@ -68,6 +71,7 @@ class MultiChannelHandler(simplehandler.Handler):
 		return Gst.FlowReturn.OK
 
 	def to_trigger_file(self, path, data):
+		#os.remove(path)
 		with open(path, 'a') as f:
  			f.write(data)
 
@@ -85,20 +89,15 @@ def build_and_run(pipelinefunc, name, segment = None, **pipelinefunc_kwargs):
 		print("{0} = {1}".format(key, value))
 	print >>sys.stderr, "=== Running Test %s ===" % name
 	mainloop = GObject.MainLoop()
-	print >>sys.stderr, "=== Checkpoint #1 ==="
-	pipeline = pipelinefunc(Gst.Pipeline(name = name), name, **pipelinefunc_kwargs)
-	print >>sys.stderr, "=== Checkpoint #2 ==="
-	#handler = simplehandler.Handler(mainloop, pipeline)
-	#handler = MultiChannelHandler(mainloop, pipeline, output = "output.txt", instrument = instrument)
-	handler = MultiChannelHandler(mainloop, pipeline, output = "")
-	print >>sys.stderr, "=== Checkpoint #3 ==="
+	pipeline = Gst.Pipeline(name = name)
+	handler = MultiChannelHandler(mainloop, pipeline, output = "/home/dmeacher/local/src/gstlal/gstlal-ugly/tests", instrument = None)
+	pipeline = pipelinefunc(pipeline, name, handler, **pipelinefunc_kwargs)
 	if segment is not None:
 		if pipeline.set_state(Gst.State.PAUSED) == Gst.StateChangeReturn.FAILURE:
 			raise RuntimeError("pipeline failed to enter PLAYING state")
 		pipeline.seek(1.0, Gst.Format(Gst.Format.TIME), Gst.SeekFlags.FLUSH, Gst.SeekType.SET, segment[0].ns(), Gst.SeekType.SET, segment[1].ns())
 	if pipeline.set_state(Gst.State.PLAYING) == Gst.StateChangeReturn.FAILURE:
 		raise RuntimeError("pipeline failed to enter PLAYING state")
-	print >>sys.stderr, "=== Checkpoint #4 ==="
 	mainloop.run()
 
 #
@@ -116,12 +115,11 @@ def build_and_run(pipelinefunc, name, segment = None, **pipelinefunc_kwargs):
 #
 
 
-def peak_test_01a(pipeline,name):
+def peak_test_01a(pipeline,name,handler):
 	#
 	# try changing these.  test should still work!
 	#
 
-	print >>sys.stderr, "=== Checkpoint #1a ==="
 	initial_channels = 2
 	rate = 2048	#Hz
 	width = 32
@@ -137,20 +135,16 @@ def peak_test_01a(pipeline,name):
 	# build pipeline
 	#
 
-	print >>sys.stderr, "=== Checkpoint #1b ==="
-
 	head = test_common.gapped_test_src(pipeline, buffer_length = buffer_length, rate = rate, width = width, channels = initial_channels, test_duration = test_duration, gap_frequency = gap_frequency, gap_threshold = gap_threshold, control_dump_filename = "%s_control.dump" % name)
-	print >>sys.stderr, "=== Checkpoint #1c ==="
 	head = pipeparts.mktogglecomplex(pipeline, head)
-	print >>sys.stderr, "=== Checkpoint #1d ==="
 	head = pipeparts.mktaginject(pipeline, head, "instrument=H1,channel-name=LSC-STRAIN,units=strain")
-	print >>sys.stderr, "=== Checkpoint #1e ==="
+
 	#head = test_common.gapped_complex_test_src(pipeline, buffer_length = buffer_length, rate = in_rate, test_duration = test_duration, wave = wave, freq = sine_frequency, gap_frequency = gap_frequency, gap_threshold = gap_threshold, control_dump_filename = "itac_test_01a_control.dump", tags = "instrument=H1,channel-name=LSC-STRAIN,units=strain")
 	#head = tee = pipeparts.mktee(pipeline, head)
 	#pipeparts.mktrigger(pipeline, head, peak_window, "test_bank.xml")
 
-	pipeparts.mktrigger(pipeline, head, peak_window,autocorrelation_matrix = numpy.ones((1,21), dtype=numpy.complex))
-	print >>sys.stderr, "=== Checkpoint #1f ==="
+	# Does not recieve EOS, hangs
+	#pipeparts.mktrigger(pipeline, head, peak_window,autocorrelation_matrix = numpy.ones((1,21), dtype=numpy.complex))
 
 	#head = pipeparts.mkqueue(pipeline, pipeparts.mkitac(pipeline, head, peak_window, "test_bank.xml", autocorrelation_matrix = numpy.array([[0+0.j, 0+0.j, 1+1.j, 0+0.j, 0+0.j]])))
 	#head = pipeparts.mkprogressreport(pipeline, head, "test")
@@ -163,12 +157,11 @@ def peak_test_01a(pipeline,name):
 	
 	#a = pipeparts.mkappsink(pipeline, pipeparts.mkqueue(pipeline, head))
 
+	head = pipeparts.mkgeneric(pipeline, head, "lal_nxydump")
+
 	sink = pipeparts.mkappsink(pipeline, head, max_buffers = 1, sync = False)
-	print >>sys.stderr, "=== Checkpoint #1g ==="
-	#sink.connect("new-sample",  handler.bufhandler)
-	print >>sys.stderr, "=== Checkpoint #1h ==="
-	#sink.connect("new-preroll", handler.prehandler)
-	print >>sys.stderr, "=== Checkpoint #1i ==="
+	sink.connect("new-sample",  handler.bufhandler)
+	sink.connect("new-preroll", handler.prehandler)
 
 	#outfile = open("itac_test_01a_out.dump", "w")
 
