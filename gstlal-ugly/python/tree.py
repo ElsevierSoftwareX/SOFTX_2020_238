f3259a8a93507a48de212659eacc3d35674f049c
Chad Hanna <crh184@psu.edu>
2017-04-07 17:00:12 -0400

    treebank: config 5

--- gstlal-ugly/python/tree.py
+++ gstlal-ugly/python/tree.py
@@ -227,7 +227,7 @@ class Node(object):
 		if  (self.cube.constraint_func(self.cube.vertices + [self.cube.center]) and (numtmps > split_num_templates or ((numtmps > split_num_templates/2.) and not (1./vtol < par_vratio < vtol)))):
 			self.template_count[0] = self.template_count[0] + 1
 			bifurcation += 1
-			if numtmps < 1**len(size) and (1./vtol < par_vratio < vtol):
+			if numtmps < 3**len(size) and (1./vtol < par_vratio < vtol):
 				left, right = self.cube.split(splitdim, reuse_metric = True)
 			else:
 				left, right = self.cube.split(splitdim)
