b3cddfe8fbe041d4ea7d2cda00ed8a7d4c08534f
Patrick Godwin <patrick.godwin@ligo.org>
2017-07-05 12:35:10 -0700

    idq_aggregator.py: changed indexing scheme from MultiIndex to DatetimeIndex, multilevel aggregation fixed (except for possibly some edge cases)

--- gstlal-ugly/python/idq_aggregator.py
+++ gstlal-ugly/python/idq_aggregator.py
@@ -32,6 +32,7 @@ import sys
 import numpy
 import pandas
 
+from lal import gpstime
 from gstlal import aggregator
 
 
@@ -41,38 +42,37 @@ from gstlal import aggregator
 #
 ####################
 
-
-# FIXME: works for level 1 aggregation only, not tested for other levels
-def reduce_data(data, gps_start, gps_end, path, reduce_count, aggregate = 'max', level = 1):
+def reduce_data(data, gps_start, gps_end, path, reduce_count, aggregate = 'max', level = 0):
 	"""!
 	This function does a data reduction recursively as needed
 	by powers of 10 where level specifies the power.
 	Default minimum is 1 e.g., reduction by 1 order of magnitude.
 	"""
-	if level == 1 or (reduce_count % (10 ** level) == 0 and level <= aggregator.DIRS):
-		if level == 1:
+	if (reduce_count % (10 ** level) == 0 and level < aggregator.DIRS):
+		if level == 0:
 			agg_data = aggregate_data(data, gps_start, gps_end, aggregate = aggregate)
 		else:
-			agg_data = aggregate_data(get_dataset_by_range(gps_start, gps_end, path, aggregate = aggregate, level = level), gps_start, gps_end, level = level)
+			agg_data = aggregate_data(get_dataset_by_range(gps_start, gps_end, path, aggregate = aggregate, level = level - 1), gps_start, gps_end, level = level)
+			path = update_agg_path(path, gps_start, cur_level = level - 1, new_level = level)
 		if agg_data is not None:
 			create_new_dataset(path, 'aggregates', agg_data, aggregate = aggregate)
-		path = update_agg_path(path, gps_start, level = level)
 		reduce_data(data, gps_start, gps_end, path, reduce_count, aggregate = aggregate, level = level + 1)
 
-# FIXME: works for level 1 aggregation only, not tested for other levels
-def aggregate_data(data, gps_start, gps_end, column = 'snr', aggregate = 'max', level = 1):
+def aggregate_data(data, gps_start, gps_end, column = 'snr', aggregate = 'max', level = 0):
 	"""!
 	Reduces data of a given level for a given gps range,
 	column, and aggregate. Returns the aggregated data.
 	"""
-	gps_start_index = floor_div(gps_start, 10 ** level)
-	gps_end_index = floor_div(gps_end, 10 ** level) - (10 ** level)
+	gps_start_idx = floor_div(gps_start, 10 ** (level+1))
+	gps_end_idx = floor_div(gps_end, 10 ** (level+1))
 	if aggregate == 'max':
-		max_index = data.loc[gps_start_index:gps_end_index].groupby(level='gps_level%d' % level)[column].idxmax().dropna().values
-		if max_index.size > 0:
-			return data.loc[max_index]
-		else:
-			return None
+		max_index = get_dataframe_subset(gps_start_idx, gps_end_idx, data, level = level).groupby(pandas.TimeGrouper('%ds' % (10 ** (level+1))))[column].idxmax().dropna().values
+	else:
+		raise NotImplementedError
+	if max_index.size > 0:
+		return data.loc[max_index]
+	else:
+		return None
 
 def get_dataset(path, base, aggregate = None):
 	"""!
@@ -91,6 +91,8 @@ def create_new_dataset(path, base, data, aggregate = None, tmp = False):
 	The data will be stored in an hdf5 file at path @param path with
 	base name @param base.  You can also make a temporary file.
 	"""
+	if not os.path.exists(path):
+		aggregator.makedir(path)
 	if tmp:
 		fname = os.path.join(path, "%s.h5.tmp" % base)
 	else:
@@ -102,18 +104,17 @@ def create_new_dataset(path, base, data, aggregate = None, tmp = False):
 			store.append(aggregate, data)
 	return fname
 
-# FIXME: works for level 1 aggregation only, not tested for other levels
 def get_dataset_by_range(gps_start, gps_end, path, aggregate = None, level = 0):
 	"""!
 	Returns a dataset for a given aggregation level and gps range.
 	"""
 	global_start_index = floor_div(gps_start, 10 ** (level+1))
 	global_end_index = floor_div(gps_end, 10 ** (level+1))
-	gps_epochs = (floor_div(t, aggregator.MIN_TIME_QUANTA * (10 ** level)) for t in range(global_start_index, global_end_index + aggregator.MIN_TIME_QUANTA * (10 ** level), aggregator.MIN_TIME_QUANTA * (10 ** level)))
+	gps_epochs = (floor_div(t, aggregator.MIN_TIME_QUANTA * (10 ** level)) for t in range(global_start_index, global_end_index, aggregator.MIN_TIME_QUANTA * (10 ** level)))
 	# generate all relevant files and combine to one dataset
 	# FIXME: more efficient to filter relevant datasets first
 	#        rather than overpopulating dataset and then filtering
-	paths = (update_agg_path(path, gps_epoch, level = level) for gps_epoch in gps_epochs)
+	paths = (update_agg_path(path, gps_epoch, cur_level = level, new_level = level) for gps_epoch in gps_epochs)
 	if aggregate is None:
 		pattern = '[0-9]' * 10 + '.h5'
 		filelist = (glob.glob(os.path.join(path, pattern)) for path in paths)
@@ -122,29 +123,28 @@ def get_dataset_by_range(gps_start, gps_end, path, aggregate = None, level = 0):
 	else:
 		files = (os.path.join(path, 'aggregates.h5') for path in paths)
 	datasets = (pandas.read_hdf(file_, aggregate) for file_ in files)
-	return pandas.concat(datasets).loc[global_start_index:global_end_index]
+	return get_dataframe_subset(global_start_index, global_end_index, pandas.concat(datasets), level = level)
 
-def gps_to_index(gps_time, n_levels = 2):
-	"""!
-	Returns a tuple containing a multilevel gps index.
-	"""
-	index = [gps_time]
-	for level in range(1, n_levels):
-		index.append(floor_div(gps_time, 10 ** level))
-	return tuple(index[::-1])
+def get_dataframe_subset(gps_start, gps_end, dataframe, level = 0):
+	start_index = gps_to_index(gps_start)
+	end_index = gps_to_index(gps_end - (10 ** level))
+	return dataframe.loc[start_index:end_index]
 
-def create_dataframe_index(gps_time, n_levels = 2):
+def create_dataframe_index(gps_time):
 	"""
-	Returns an n level index based on gps times
+	Returns a datetime index based on gps times
 	per minimum aggregator quanta.
 	"""
-	index_t = numpy.arange(floor_div(gps_time, aggregator.MIN_TIME_QUANTA), floor_div(gps_time, aggregator.MIN_TIME_QUANTA) + aggregator.MIN_TIME_QUANTA, dtype = numpy.int)
-	indices = [index_t]
-	index_names = ['gps_level0']
-	for level in range(1, n_levels):
-		indices.append(numpy.fromiter(( floor_div(x, 10 ** level) for x in index_t), dtype = numpy.int))
-		index_names.append('gps_level%d' % level)
-	return pandas.MultiIndex.from_tuples(list(zip(*(indices[::-1]))), names = index_names[::-1])
+	start_time = gps_to_index(floor_div(gps_time, aggregator.MIN_TIME_QUANTA))
+	return pandas.date_range(start_time, periods = aggregator.MIN_TIME_QUANTA, freq = 's')
+
+def gps_to_index(gps_time):
+	# FIXME: round microseconds is a workaround for possible bug in the
+	#        gpstime module in lal that adds a microsecond 
+	return pandas.Timestamp(gpstime.gps_to_utc(gps_time).replace(microsecond=0))
+
+def gps_from_index(timestamp):
+	return int(gpstime.utc_to_gps(timestamp))
 
 def to_agg_path(base_path, tag, gps_time, channel, rate, level = 0):
 	"""
@@ -161,15 +161,15 @@ def to_agg_path(base_path, tag, gps_time, channel, rate, level = 0):
 		path = os.path.join(path, str(rate).zfill(4))
 	return path
 
-def update_agg_path(path, gps_time, level = 0):
+def update_agg_path(path, gps_time, cur_level = 0, new_level = 0):
 	"""
 	Returns an updated aggregator path based on
 	an existing path and a gps time.
 	"""
-	path, rate = os.path.split(os.path.normpath(path))
-	for agg_level in range(aggregator.DIRS):
+	path, rate = os.path.split(os.path.abspath(path))
+	for agg_level in range(max(0, cur_level), aggregator.DIRS):
 		path, _ = os.path.split(path)
-	return os.path.join(path, aggregator.gps_to_leaf_directory(gps_time, level = level), rate)
+	return os.path.join(path, os.path.join(aggregator.gps_to_leaf_directory(gps_time, level = new_level), rate))
 
 def in_new_epoch(new_gps_time, prev_gps_time, gps_epoch):
 	"""
@@ -187,4 +187,3 @@ def floor_div(x, n):
 	"""
 	assert n > 0
 	return (x / n) * n
-
