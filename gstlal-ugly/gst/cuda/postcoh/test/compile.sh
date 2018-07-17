732940a990befedf895e4c93a5ed10e1fe5a3bc4
Joel Bosveld <joel.bosveld@ligo.org>
2018-07-17 00:27:19 -0700

    gstlal-ugly: remove spiir components

--- gstlal-ugly/gst/cuda/postcoh/test/compile.sh
+++
@@ -1,3 +0,0 @@
-
-gcc -g -c gen_detrsp_maps.c `pkg-config --cflags gstlal` `pkg-config --libs gstlal` `pkg-config --cflags chealpix` `pkg-config --libs chealpix` -llapack -llapacke
-gcc -g -o gen_detrsp_maps gen_detrsp_maps.o ../../LIGOLw_xmllib/test/LIGOLwUtils.o ../../LIGOLw_xmllib/test/LIGOLwWriter.o `pkg-config --cflags gstlal` `pkg-config --libs gstlal` `pkg-config --cflags chealpix` `pkg-config --libs chealpix` -llapack -llapacke
