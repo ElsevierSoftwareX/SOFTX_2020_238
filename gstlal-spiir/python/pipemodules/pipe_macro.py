81b97e86ae24d70e0ec6f8aa24d887ea3e1bfe5e
Qi Chu <qi.chu@ligo.org>
2018-08-23 23:30:54 -0700

    add pipe_macro.py

---
+++ gstlal-spiir/python/pipemodules/pipe_macro.py
@@ -0,0 +1,17 @@
+# need to be consistent with pipe_macro.h
+# cohsnr
+xmin = 0.54
+xmax = 3.0
+xstep = 0.0082
+# chisq
+ymin = -1.2
+ymax = 4.0
+ystep = 0.0173
+
+# rank
+rankmin = -30
+rankmax = 0
+rankstep = 0.1 # 30/299
+
+SOURCE_TYPE_BNS = 1
+STATS_XML_ID_NAME = "gstlal_postcohspiir_stats"
