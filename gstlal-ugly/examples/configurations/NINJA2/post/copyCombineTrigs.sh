81532f8905c3b34e3d479cabc22a87d273321561
Kipp Cannon <kipp.cannon@ligo.org>
2018-09-21 21:35:34 +0900

    add --ilwdchar-compat to ligolw_sqlite

--- gstlal-ugly/examples/configurations/NINJA2/post/copyCombineTrigs.sh
+++ gstlal-ugly/examples/configurations/NINJA2/post/copyCombineTrigs.sh
@@ -1,5 +1,5 @@
 for i in `ls ../week*/*ALL*.sqlite`;do
-	ligolw_sqlite -v --database ${i} --extract `basename ${i%%.s*}`.xml.gz
+	ligolw_sqlite --ilwdchar-compat -v --database ${i} --extract `basename ${i%%.s*}`.xml.gz
 done
 
 ligolw_add --ilwdchar-compat -v --output H1L1V1-ALL-COMBINED-871147552-876357464-LLOID.xml.gz `ls H1L1V1-ALL-8*.xml.gz`
--- gstlal-ugly/examples/configurations/PBH/scripts/find_injections.sh
+++ gstlal-ugly/examples/configurations/PBH/scripts/find_injections.sh
@@ -2,7 +2,7 @@
 set -e 
 for k in *.sqlite
 do
-ligolw_sqlite --verbose --tmp-space /dev/shm --extract ${k}.xml --database ${k}
+ligolw_sqlite --ilwdchar-compat --verbose --tmp-space /dev/shm --extract ${k}.xml --database ${k}
 ligolw_sicluster --verbose --cluster-window 1 ${k}.xml 
 done
 
@@ -10,10 +10,10 @@ for i in H1 H2 L1
 do
 ligolw_add --ilwdchar-compat --verbose --output ${i}all_added.xml --verbose svd_*${i}_split_bank*.xml ../segments/injections.xml
 ligolw_sicluster --verbose --cluster-window 1 ${i}all_added.xml
-ligolw_sqlite --verbose --database ${i}all.sqlite --tmp-space /dev/shm --replace ${i}all_added.xml
+ligolw_sqlite --ilwdchar-compat --verbose --database ${i}all.sqlite --tmp-space /dev/shm --replace ${i}all_added.xml
 sqlite3 ${i}all.sqlite 'DROP TABLE sim_inspiral'
-ligolw_sqlite --verbose --database ${i}all.sqlite --tmp-space /dev/shm --extract ${i}all_added.xml
+ligolw_sqlite --ilwdchar-compat --verbose --database ${i}all.sqlite --tmp-space /dev/shm --extract ${i}all_added.xml
 ligolw_add --ilwdchar-compat --verbose --output ${i}all.xml ../segments/injections.xml ${i}all_added.xml
 lalapps_inspinjfind ${i}all.xml
-ligolw_sqlite --verbose --database ${i}all.sqlite --tmp-space /dev/shm --replace ${i}all.xml
+ligolw_sqlite --ilwdchar-compat --verbose --database ${i}all.sqlite --tmp-space /dev/shm --replace ${i}all.xml
 done
