3ba38b468e5f55b2c93f4776c73f8351f1eaa027
Aaron Viets <aaron.viets@ligo.org>
2019-02-18 15:29:49 -0600

    gstlal-calibration:  Add front-end kappas to plots.

--- gstlal-calibration/tests/check_calibration/timeserieskappas.py
+++ gstlal-calibration/tests/check_calibration/timeserieskappas.py
@@ -12,6 +12,8 @@ parser.add_option("--gps-end-time", metavar = "seconds", help = "Set the GPS end
 parser.add_option("--ifo", metavar = "name", help = "Interferometer to perform the analysis on")
 parser.add_option("--frame-cache", metavar = "name", help = "Filename for frame cache to be analyzed.")
 parser.add_option("--channel-list", metavar = "list", help = "List of channels to be plotted.")
+parser.add_option("--raw-frame-cache", metavar = "name", help = "Filename for raw frame cache to be analyzed.")
+parser.add_option("--raw-channel-list", metavar = "list", help = "List of raw channels to be plotted.")
 
 options, filenames = parser.parse_args()
 
@@ -27,15 +29,28 @@ if options.channel_list is not None:
 else:
 	raise ValueError('Channel list option must be set.')
 
+raw_channel_list = []
+if options.raw_channel_list is not None:
+	raw_channels = options.raw_channel_list.split(',')
+	for raw_channel in raw_channels:
+		raw_channel_list.append((ifo, raw_channel))
+else:
+	raise ValueError('Raw channel list option must be set.')
+
+raw_data = TimeSeriesDict.read(options.raw_frame_cache, map("%s:%s".__mod__, raw_channel_list), start = start, end = end)
 data = TimeSeriesDict.read(options.frame_cache, map("%s:%s".__mod__, channel_list), start = start, end = end)
 
 segs = DataQualityFlag.query('%s:DMT-CALIBRATED:1' % ifo, start, end)
 
-for n, channel in enumerate(channels):
-	plot = TimeSeries.plot(data["%s:%s" % (ifo, channel)])
+for n in range(0, len(channels)):
+	plot = TimeSeries.plot(data["%s:%s" % (ifo, channels[n])], label = 'GDS')
 	mpl.ylabel('Correction value')
+	ax = plot.gca()
+	ax.plot(raw_data["%s:%s" % (ifo, raw_channels[n])], label='CAL-CS')
+	ax.legend()
 	#title = item
 	#title = title.replace('_', '\_')
-	mpl.title(channel.replace('_', '\_'))
+	mpl.title(channels[n].replace('_', '\_'))
 	plot.add_state_segments(segs, plotargs=dict(label='Calibrated'))
-	plot.savefig('%s_%s_%s_plot_%s.png' % (ifo, options.gps_start_time, options.gps_end_time, channel))
+	plot.savefig('%s_%s_%s_plot_%s.png' % (ifo, options.gps_start_time, options.gps_end_time, channels[n]))
+
