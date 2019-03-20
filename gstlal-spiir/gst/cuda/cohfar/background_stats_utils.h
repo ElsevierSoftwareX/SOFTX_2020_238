423712badd0a47b2ba5c55a1ac22297da26a0d80
Qi Chu <qi.chu@ligo.org>
2019-03-19 19:55:15 -0700

    background_stats_utils.h: change min_background points from 2 billion to 1 billion as I calculated wrong that 2 billion took about 4 days for background

--- gstlal-spiir/gst/cuda/cohfar/background_stats_utils.h
+++ gstlal-spiir/gst/cuda/cohfar/background_stats_utils.h
@@ -34,7 +34,7 @@
 #include <postcohtable.h>
 
 #ifndef MIN_BACKGROUND_NEVENT
-#define MIN_BACKGROUND_NEVENT 2000000000 // 2 billion instead of 1 million, about 20hrs
+#define MIN_BACKGROUND_NEVENT 1000000000 // 2 billion instead of 1 million, about 20hrs
 #endif
 	
 Bins1D *
