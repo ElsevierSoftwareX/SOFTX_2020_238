7b68e2bb4262bd21e4e78db3ce197e17f01b573c
Qi Chu <qi.chu@ligo.org>
2019-05-09 01:25:25 -0700

    gstlal_inspiral_postcohspiir_online and postcoh_finalsink.py Apply a simple cut of triggers before uploading them to GraceDB. In implementation, check if any event has a FAR less than 1e-6Hz. If so, examine if its coherent network SNR is less than a defined value (default is 8). If so, do not upload it to GraceDB.

--- gstlal-spiir/python/pipemodules/postcoh_finalsink.py
+++ gstlal-spiir/python/pipemodules/postcoh_finalsink.py
@@ -326,7 +326,7 @@ class FAPUpdater(object):
 					collected_fnames.append("%s/%s" % (self.path, one_bank_fname))
 
 class FinalSink(object):
-	def __init__(self, channel_dict, process_params, pipeline, need_online_perform, path, output_prefix, output_name, far_factor, cluster_window = 0.5, snapshot_interval = None, fapupdater_interval = None, cohfar_accumbackground_output_prefix = None, cohfar_accumbackground_output_name = None, fapupdater_output_fname = None, fapupdater_collect_walltime_string = None, singlefar_veto_thresh = 0.01, chisq_ratio_veto_thresh = 8.0, gracedb_far_threshold = None, gracedb_group = "Test", gracedb_search = "LowMass", gracedb_pipeline = "spiir", gracedb_service_url = "https://gracedb.ligo.org/api/", gracedb_offline_annote = None, output_skymap = 0, superevent_thresh = 3.8e-7, verbose = False):
+	def __init__(self, channel_dict, process_params, pipeline, need_online_perform, path, output_prefix, output_name, far_factor, cluster_window = 0.5, snapshot_interval = None, fapupdater_interval = None, cohfar_accumbackground_output_prefix = None, cohfar_accumbackground_output_name = None, fapupdater_output_fname = None, fapupdater_collect_walltime_string = None, singlefar_veto_thresh = 0.01, chisq_ratio_veto_thresh = 8.0, gracedb_far_threshold = None, gracedb_group = "Test", gracedb_search = "LowMass", gracedb_pipeline = "spiir", gracedb_service_url = "https://gracedb.ligo.org/api/", gracedb_offline_annote = None, output_skymap = 0, superevent_thresh = 3.8e-7, opa_cohsnr_thresh = 8, verbose = False):
 		#
 		# initialize
 		#
@@ -344,9 +344,15 @@ class FinalSink(object):
 		self.cluster_boundary = None
 		self.need_candidate_check = False
 		self.cur_event_table = lsctables.New(postcoh_table_def.PostcohInspiralTable)
-		# FIXME: hard-coded chisq_ratio_thresh to veto 
 		self.chisq_ratio_thresh = chisq_ratio_veto_thresh
 		self.superevent_thresh = superevent_thresh
+		# FIXME: hard-coded the opa_thresh that all triggers less than this thresh will be tested
+		# if their cohsnr SNRs are smaller than the given opa_cohsnr_snr
+		# if smaller, no uploading.
+		# opa_thresh is chosen as 1e-6 as to not launch lalinference jobs
+		self.opa_thresh = 1e-6
+		self.opa_cohsnr_thresh = opa_cohsnr_thresh
+
 		self.nevent_clustered = 0
 		self.singlefar_veto_thresh = singlefar_veto_thresh
 
@@ -429,6 +435,10 @@ class FinalSink(object):
 		# just submit it if is a low-significance trigger
 		if self.candidate.far < self.gracedb_far_threshold and self.candidate.far > self.superevent_thresh:
 			return True
+
+		if self.candidate.far < self.opa_thresh and self.candidate.cohsnr < self.opa_cohsnr_thresh:
+			# print "suppressed", self.candidate.cohsnr, self.candidate.far
+			return False
 
 		# FIXME: any two of the sngl fars need to be < singlefar_veto_thresh 
 		# single far veto for high-significance trigger
