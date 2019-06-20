4df67305f5970c441cb005ad76377dcabeab13f6
Patrick Godwin <patrick.godwin@ligo.org>
2019-06-19 19:00:23 -0700

    p_astro_gstlal.py: add json import to doctests to avoid unit test failures

--- gstlal-inspiral/python/p_astro_gstlal.py
+++ gstlal-inspiral/python/p_astro_gstlal.py
@@ -331,6 +331,7 @@ def compute_p_astro(event_ln_likelihood_ratio,
 
     Example
     -------
+    >>> import json
     >>> p_astros = json.loads(compute_p_astro(files))
     >>> p_astros
     {'BNS': 0.999, 'BBH': 0.0, 'NSBH': 0.0, 'Terrestrial': 0.001}
@@ -394,6 +395,7 @@ def compute_p_astro_approx(snr, far, mass1, mass2, livetime, mean_values_dict):
 
     Example
     -------
+    >>> import json
     >>> p_astros = json.loads(compute_p_astro(files))
     >>> p_astros
     {'BNS': 0.999, 'BBH': 0.0, 'NSBH': 0.0, 'Terrestrial': 0.001}
