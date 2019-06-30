a7f7c08317147de3ba043815a4155dd4b4f09039
Patrick Godwin <patrick.godwin@ligo.org>
2019-06-30 08:55:40 -0700

    update doc theme, tidy up front page

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,6 +25,9 @@ sys.path.insert(0, os.path.abspath('../../gstlal-burst/python'))
 sys.path.insert(0, os.path.abspath('../../gstlal-calibration/python'))
 sys.path.insert(0, os.path.abspath('../../gstlal-ugly/python'))
 
+#  on_rtd is whether we are on readthedocs.org, this line of code grabbed
+#  from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # -- General configuration ------------------------------------------------
 
@@ -35,7 +38,8 @@ sys.path.insert(0, os.path.abspath('../../gstlal-ugly/python'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -98,28 +102,29 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'#'classic'
-html_logo = "gstlal_small.png"
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'fixed_sidebar': 'true',
-    'sidebar_width': '200px',
-    'page_width': '95%',
-    'show_powered_by': 'false',
-    'logo_name': 'true',
-}
+#html_theme_options = {}
+
+def setup(app):
+    app.add_stylesheet('css/my_theme.css')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = { '**': ['navigation.html', 'relations.html', 'searchbox.html'] }
-html_last_updated_fmt = None
+#html_sidebars = { '**': ['navigation.html', 'relations.html', 'searchbox.html'] }
+#html_last_updated_fmt = None
 
 # -- Options for HTMLHelp output ------------------------------------------
 
