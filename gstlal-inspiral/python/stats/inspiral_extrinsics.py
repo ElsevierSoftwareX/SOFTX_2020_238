d2a6b2ac38262a7ef40c41641e5088f81de6c8de
Leo Tsukada <leo.tsukada@ligo.org>
2019-08-01 16:23:32 -0700

    inspiral_extrinsics.py : make the initialization of TimePhaseSNR class comparible with HLVK case in an ad-hoc way. ( load a new covmat.h5 and construct tree data from scratch.)

--- gstlal-inspiral/python/stats/inspiral_extrinsics.py
+++ gstlal-inspiral/python/stats/inspiral_extrinsics.py
@@ -1227,7 +1227,7 @@ class TimePhaseSNR(object):
 		self.tree_data = tree_data
 		self.margsky = margsky
 
-		if self.tree_data is None:
+		if self.tree_data is None or numpy.shape(self.tree_data)[1] != 1 + max(sum(self.slices.values(),[])):
 			time, phase, deff = TimePhaseSNR.tile(verbose = verbose)
 			self.tree_data = self.dtdphideffpoints(time, phase, deff, self.slices)
 
@@ -1290,13 +1290,24 @@ class TimePhaseSNR(object):
 		Initialize one of these from a file instead of computing it from scratch
 		"""
 		f = h5py.File(fname, "r")
-		# These *have* to be here
-		transtt = dict((frozenset(k.split(",")), numpy.array(f["transtt"][k])) for k in f["transtt"])
-		transtp = dict((frozenset(k.split(",")), numpy.array(f["transtp"][k])) for k in f["transtp"])
-		transpt = dict((frozenset(k.split(",")), numpy.array(f["transpt"][k])) for k in f["transpt"])
-		transpp = dict((frozenset(k.split(",")), numpy.array(f["transpp"][k])) for k in f["transpp"])
-		transdd = dict((frozenset(k.split(",")), numpy.array(f["transdd"][k])) for k in f["transdd"])
-		norm = dict((frozenset(k.split(",")), numpy.array(f["norm"][k])) for k in f["norm"])
+		if os.path.join(gstlal_config_paths["pkgdatadir"], "covmat.h5") in other_fnames:
+			# These *have* to be here
+			f_covmat = h5py.File(os.path.join(gstlal_config_paths["pkgdatadir"], "covmat.h5"))
+			other_fnames.remove(os.path.join(gstlal_config_paths["pkgdatadir"], "covmat.h5"))
+			transtt = dict((frozenset(k.split(",")), numpy.array(f_covmat["transtt"][k])) for k in f_covmat["transtt"])
+			transtp = dict((frozenset(k.split(",")), numpy.array(f_covmat["transtp"][k])) for k in f_covmat["transtp"])
+			transpt = dict((frozenset(k.split(",")), numpy.array(f_covmat["transpt"][k])) for k in f_covmat["transpt"])
+			transpp = dict((frozenset(k.split(",")), numpy.array(f_covmat["transpp"][k])) for k in f_covmat["transpp"])
+			transdd = dict((frozenset(k.split(",")), numpy.array(f_covmat["transdd"][k])) for k in f_covmat["transdd"])
+			norm = dict((frozenset(k.split(",")), numpy.array(f_covmat["norm"][k])) for k in f_covmat["norm"])
+		else:
+			# These *have* to be here
+			transtt = dict((frozenset(k.split(",")), numpy.array(f["transtt"][k])) for k in f["transtt"])
+			transtp = dict((frozenset(k.split(",")), numpy.array(f["transtp"][k])) for k in f["transtp"])
+			transpt = dict((frozenset(k.split(",")), numpy.array(f["transpt"][k])) for k in f["transpt"])
+			transpp = dict((frozenset(k.split(",")), numpy.array(f["transpp"][k])) for k in f["transpp"])
+			transdd = dict((frozenset(k.split(",")), numpy.array(f["transdd"][k])) for k in f["transdd"])
+			norm = dict((frozenset(k.split(",")), numpy.array(f["norm"][k])) for k in f["norm"])
 
 		try:
 			dgrp = f["gstlal_extparams"]
@@ -1909,7 +1920,7 @@ class InspiralExtrinsics(object):
 	* :py:class:`TimePhaseSNR`
 	* :py:class:`p_of_instruments_given_horizons`
 	"""
-	time_phase_snr = TimePhaseSNR.from_hdf5(os.path.join(gstlal_config_paths["pkgdatadir"], "inspiral_dtdphi_pdf.h5"))
+	time_phase_snr = TimePhaseSNR.from_hdf5(os.path.join(gstlal_config_paths["pkgdatadir"], "inspiral_dtdphi_pdf.h5"), other_fnames=[os.path.join(gstlal_config_paths["pkgdatadir"], "covmat.h5")])
 	p_of_ifos = {}
 	# FIXME add Kagra
 	p_of_ifos[("H1", "L1", "V1",)] = p_of_instruments_given_horizons.from_hdf5(os.path.join(gstlal_config_paths["pkgdatadir"], "H1L1V1_p_of_instruments_given_H_d.h5"))
