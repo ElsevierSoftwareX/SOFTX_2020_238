5ce2c1f3dbd3316e12aa6622b01f6a2c9196ced2
cal <cal@ldas-pcdev6.ligo-wa.caltech.edu>
2019-08-19 12:36:08 -0700

    Edits to plotting scripts for calibration checks

--- gstlal-calibration/tests/check_calibration/timeserieskappas.py
+++ gstlal-calibration/tests/check_calibration/timeserieskappas.py
@@ -13,6 +13,9 @@ parser.add_option("--ifo", metavar = "name", help = "Interferometer to perform t
 parser.add_option("--frame-cache", metavar = "name", help = "Filename for frame cache to be analyzed.")
 parser.add_option("--channel-list", metavar = "list", help = "List of channels to be plotted.")
 parser.add_option("--front-end-channel-list", metavar = "list", help = "List of channels to be plotted from the front-end.")
+parser.add_option("--analyze-additional-hoft", action = "store_true", help = "Set this to analyze an additional h(t) channel.")
+parser.add_option("--additional-hoft-frames-cache", metavar = "name", help = "If desired, provide an additional frame cache for a secondary h(t) data stream to be analyzed.")
+parser.add_option("--additional-channel-list", metavar = "list", help = "List of channels to be plotted from additional h(t) frames.")
 
 options, filenames = parser.parse_args()
 
@@ -28,6 +31,17 @@ if options.channel_list is not None:
 else:
 	raise ValueError('Channel list option must be set.')
 
+if options.analyze_additional_hoft:
+	additional_channel_list = []
+	if options.additional_channel_list is not None:
+		additional_channels = options.additional_channel_list.split(',')
+		for channel in additional_channels:
+			additional_channel_list.append((ifo, channel))
+		plot_additional_hoft = True
+	else:
+		plot_additional_hoft = False
+
+
 front_end_channel_list = []
 if options.front_end_channel_list is not None:
 	front_end_channels = options.front_end_channel_list.split(',')
@@ -40,6 +54,8 @@ else:
 data = TimeSeriesDict.read(options.frame_cache, map("%s:%s".__mod__, channel_list), start = start, end = end)
 if plot_front_end:
 	front_end_data = TimeSeriesDict.fetch(map("%s:%s".__mod__, front_end_channel_list), start = start, end = end)
+if plot_additional_hoft:
+	additional_hoft_data = TimeSeriesDict.read(options.additional_hoft_frames_cache, map("%s:%s".__mod__, additional_channel_list), start = start, end = end)
 
 print(map("%s:%s".__mod__, front_end_channel_list))
 
@@ -50,6 +66,8 @@ for n, channel in enumerate(channels):
 	ax = plot.gca()
 	if plot_front_end:
 		ax.plot(front_end_data["%s:%s" % (ifo, front_end_channels[n])])
+	if plot_additional_hoft:
+		ax.plot(additional_hoft_data["%s:%s" % (ifo, additional_channels[n])])
 	ax.set_ylabel('Correction value')
 	plot.gca().legend()
 	#title = item
