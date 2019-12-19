f55ded41b7b71d3d781834cf933263409a640d0d
Madeline Wade <madeline.wade@ligo.org>
2019-12-19 12:23:08 -0600

    gstlal_compute_strain: Added in resampler so metrics are dumped at lower sample rate calibhandler.py: Enclosed more of metrics monitoring into if statement so nothing is done if kafka server isn't set

--- gstlal-calibration/python/calibhandler.py
+++ gstlal-calibration/python/calibhandler.py
@@ -67,11 +67,11 @@ class Handler(simplehandler.Handler):
 		#
 		# initialize
 		#
-		self.lock = threading.Lock()
 		self.pipeline = pipeline
 		self.verbose = verbose
 		self.kafka_server = kafka_server
 		if self.kafka_server is not None:
+			self.lock = threading.Lock()
 			from kafka import KafkaProducer
 			from kafka import errors
 			try:
@@ -80,34 +80,62 @@ class Handler(simplehandler.Handler):
 						key_serializer = lambda m: json.dumps(m).encode('utf-8'),
 						value_serializer = lambda m: json.dumps(m).encode('utf-8'),
 					)
-				print("Kafka server established.")
+				if self.verbose:
+					print("Kafka server established.")
 			except errors.NoBrokersAvailable:
 				self.producer = None
-				print("No brokers available for kafka. Defaulting to not pushing to kafka.")
+				if self.verbose:
+					print("No brokers available for kafka. Defaulting to not pushing to kafka.")
 
 	def appsink_statevector_new_buffer(self, elem, ifo, bitmaskdict):
-		with self.lock:
-			# retrieve data from appsink buffer
-			buf = elem.emit("pull-sample").get_buffer()
-			result, mapinfo = buf.map(Gst.MapFlags.READ)
-			buf_timestamp = LIGOTimeGPS(0, buf.pts)
-			if mapinfo.data:
-				s = StringIO.StringIO(mapinfo.data)
-				time, state = s.getvalue().split('\n')[0].split()
-				state = int(state)
-				buf.unmap(mapinfo)
-				monitor_dict = {}
-				monitor_dict['time'] = float(buf_timestamp)
-				for key, bitmask in bitmaskdict.items():
-					all_bits_on = bitmask & bitmask
-					monitor = state & bitmask
-					if monitor == all_bits_on:
-						monitor_dict[key] = 1
+		if self.kafka_server is not None:
+			with self.lock:
+				# retrieve data from appsink buffer
+				buf = elem.emit("pull-sample").get_buffer()
+				result, mapinfo = buf.map(Gst.MapFlags.READ)
+				buf_timestamp = LIGOTimeGPS(0, buf.pts)
+				if mapinfo.data:
+					s = StringIO.StringIO(mapinfo.data)
+					time, state = s.getvalue().split('\n')[0].split()
+					state = int(state)
+					buf.unmap(mapinfo)
+					monitor_dict = {}
+					monitor_dict['time'] = float(buf_timestamp)
+					for key, bitmask in bitmaskdict.items():
+						all_bits_on = bitmask & bitmask
+						monitor = state & bitmask
+						if monitor == all_bits_on:
+							monitor_dict[key] = 1
+						else:
+							monitor_dict[key] = 0
+					# Check if kafka server is now available if it's supposed to be used
+					if self.producer is None:
+						from kafka import KafkaProducer
+						from kafka import errors
+						try:
+							self.producer = KafkaProducer(
+									bootstrap_servers = [self.kafka_server],
+									key_serializer = lambda m: json.dumps(m).encode('utf-8'),
+									value_serializer = lambda m: json.dumps(m).encode('utf-8'),
+								)
+						except errors.NoBrokersAvailable:
+							self.producer = None
+							if self.verbose:
+								print("No brokers available for kafka. Defaulting to not pushing to kafka.")
 					else:
-						monitor_dict[key] = 0
+						self.producer.send("%s_statevector_bit_check" % ifo, value = monitor_dict) 
+			return Gst.FlowReturn.OK	
+
+	def latency_new_buffer(self, elem, param):
+		if self.kafka_server is not None:
+			with self.lock:
+				latency = elem.get_property("current-latency")
+				name = elem.get_property("name")
+				time = elem.get_property("timestamp")
 				# Check if kafka server is now available if it's supposed to be used
-				if self.kafka_server is not None and self.producer is None:
+				if self.producer is None:
 					from kafka import KafkaProducer
+					from kafka import errors
 					try:
 						self.producer = KafkaProducer(
 								bootstrap_servers = [self.kafka_server],
@@ -116,29 +144,9 @@ class Handler(simplehandler.Handler):
 							)
 					except errors.NoBrokersAvailable:
 						self.producer = None
-						print("No brokers available for kafka. Defaulting to not pushing to kafka.")
-				if self.kafka_server is not None and self.producer is not None:
-					self.producer.send("%s_statevector_bit_check" % ifo, value = monitor_dict) 
-			return Gst.FlowReturn.OK	
-
-	def latency_new_buffer(self, elem, param):
-		with self.lock:
-			latency = elem.get_property("current-latency")
-			name = elem.get_property("name")
-			time = elem.get_property("timestamp")
-			# Check if kafka server is now available if it's supposed to be used
-			if self.kafka_server is not None and self.producer is None:
-				from kafka import KafkaProducer
-				try:
-					self.producer = KafkaProducer(
-							bootstrap_servers = [self.kafka_server],
-							key_serializer = lambda m: json.dumps(m).encode('utf-8'),
-							value_serializer = lambda m: json.dumps(m).encode('utf-8'),
-						)
-				except errors.NoBrokersAvailable:
-					self.producer = None
-					print("No brokers available for kafka. Defaulting to not pushing to kafka.")
-			if self.kafka_server is not None and self.producer is not None:
-				self.producer.send("%s_latency" % (name.split("_")[0]), value = {"time": time, name: latency})
+						if self.verbose:
+							print("No brokers available for kafka. Defaulting to not pushing to kafka.")
+				else:
+					self.producer.send("%s_latency" % (name.split("_")[0]), value = {"time": time, name: latency})
 			return Gst.FlowReturn.OK
 
