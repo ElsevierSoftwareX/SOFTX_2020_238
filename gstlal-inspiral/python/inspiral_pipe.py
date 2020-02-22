a102c7897d94df8f22d0f4ea775f6cbc3b5e1b2b
Ryan Magee <ryan.magee@ligo.org>
2020-02-22 12:07:23 -0800

    gstlal_ll_inspiral_pipe, inspiral_pipe.py: exposed new aggregator options

--- gstlal-inspiral/python/inspiral_pipe.py
+++ gstlal-inspiral/python/inspiral_pipe.py
@@ -221,6 +221,8 @@ def event_upload_layer(dag, jobs, options, job_tags):
 		"gracedb-service-url": options.gracedb_service_url,
 		"far-threshold": options.event_aggregator_far_threshold,
 		"far-trials-factor": options.event_aggregator_far_trials_factor,
+		"upload-cadence-type": options.event_aggregator_upload_cadence_type,
+		"upload-cadence-factor": options.event_aggregator_upload_cadence_factor,
 		"num-jobs": len(job_tags),
 		"input-topic": "events",
 		"rootdir": "event_uploader",
