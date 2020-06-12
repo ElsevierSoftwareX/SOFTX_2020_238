d43bcfd6096ac4fab33114848b2d5f9ffaf6ca86
Cody Messick <cody.messick@ligo.org>
2020-06-12 14:01:38 -0700

    Added GEO600 support ("G1") to itacac

--- gstlal-inspiral/python/snglinspiraltable.py
+++ gstlal-inspiral/python/snglinspiraltable.py
@@ -30,6 +30,33 @@ class GSTLALSnglInspiral(_snglinspiraltable.GSTLALSnglInspiral):
 	spin2 = lsctables.SnglInspiral.spin2
 
 	@property
+	def G1_snr_time_series(self):
+		try:
+			name = self._G1_snr_name
+		except ValueError:
+			# C interface raises ValueError if the internal snr
+			# pointer is NULL
+			return None
+		series = lal.CreateCOMPLEX8TimeSeries(
+			name,
+			lal.LIGOTimeGPS(self._G1_snr_epoch_gpsSeconds, self._G1_snr_epoch_gpsNanoSeconds),
+			self._G1_snr_f0,
+			self._G1_snr_deltaT,
+			lal.Unit(self._G1_snr_sampleUnits),
+			self._G1_snr_data_length
+		)
+		# we want to be able to keep the table row object in memory
+		# for an extended period of time so we need to be able to
+		# release the memory used by the SNR time series when we no
+		# longer need it, and so we copy the data here instead of
+		# holding a reference to the original memory.  if we
+		# allowed references to the original memory to leak out
+		# into Python land we could never know if it's safe to free
+		# it
+		series.data.data[:] = self._G1_snr_data
+		return series
+
+	@property
 	def H1_snr_time_series(self):
 		try:
 			name = self._H1_snr_name
@@ -136,6 +163,10 @@ class GSTLALSnglInspiral(_snglinspiraltable.GSTLALSnglInspiral):
 		# it
 		series.data.data[:] = self._K1_snr_data
 		return series
+
+	@G1_snr_time_series.deleter
+	def G1_snr_time_series(self):
+		self._G1_snr_time_series_deleter()
 
 	@H1_snr_time_series.deleter
 	def H1_snr_time_series(self):
