92d69eeacd60dc52f69ceeef07b2feaf085e5f4b
Patrick Godwin <patrick.godwin@ligo.org>
2021-02-03 12:02:39 -0800

    tests/framecpp_test_01.sh: update for python3

--- gstlal-ugly/tests/framecpp_test_01.sh
+++ gstlal-ugly/tests/framecpp_test_01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CMP_NXYDUMPS=`/usr/bin/env python -c "import gstlal.cmp_nxydumps ; print gstlal.cmp_nxydumps.__file__.replace('.pyc', '.py')"`
+CMP_NXYDUMPS=`/usr/bin/env python3 -c "import gstlal.cmp_nxydumps ; print(gstlal.cmp_nxydumps.__file__.replace('.pyc', '.py'))"`
 
 gst-launch-1.0 audiotestsrc wave=9 samplesperbuffer=89 num-buffers=3000 ! audio/x-raw, format=F32LE, rate=1024 ! tee name="src" ! lal_nxydump ! filesink sync=false async=false location="framecpp_test_01_in.dump" src. ! .H2:FAKE-DATA framecpp_channelmux ! framecpp_channeldemux .H2:FAKE-DATA ! lal_nxydump ! filesink sync=false async=false location="framecpp_test_01_out.dump"
 
-/usr/bin/env python $CMP_NXYDUMPS framecpp_test_01_in.dump framecpp_test_01_out.dump
+/usr/bin/env python3 $CMP_NXYDUMPS framecpp_test_01_in.dump framecpp_test_01_out.dump
